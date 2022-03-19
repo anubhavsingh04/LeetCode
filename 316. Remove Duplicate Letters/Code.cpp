575e50ac7b49bc582425d49c060a4825b097c9c7
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-03-19 10:36:17 +0530

    Add files via upload

---
+++ 316. Remove Duplicate Letters/Code.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int>cnt(26,0),vis(26,0);
+        string res="";
+        for(int i=0;i<s.size();i++)
+        {
+            cnt[s[i]-'a']++;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            cnt[s[i]-'a']--;
+            if(!vis[s[i]-'a'])
+            {
+                while(res.size()>0&&s[i]<res.back()&&cnt[res.back()-'a']>0)
+                {
+                    vis[res.back()-'a']=0;
+                    res.pop_back();
+                }
+                res+=s[i];
+                vis[s[i]-'a']=1;
+            }
+        }
+        return res;
+    }
+};
