9db8a058aefe1db67414575f76772c48fb0f827e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-03-21 19:24:35 +0530

    Add files via upload

---
+++ 763. Partition Labels/Code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>ans,lastindex(26,0);
+        for(int i=0;i<s.size();i++)
+        lastindex[s[i]-'a']=i;
+        int start=0,end=0;
+        for(int i=0;i<s.size();i++)
+        {
+            end=max(end,lastindex[s[i]-'a']);
+            if(i==end)
+            {
+                ans.push_back(i-start+1);
+                start=i+1;
+            }
+        }
+        return ans;
+    }
+};
