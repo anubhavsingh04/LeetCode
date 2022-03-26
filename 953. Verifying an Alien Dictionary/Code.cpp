ecf38229cc6aeaf872f10ace476e2279397ac95e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-03-27 01:48:27 +0530

    Add files via upload

---
+++ 953. Verifying an Alien Dictionary/Code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool compare(string& s1, string& s2, string& order) {
+        int i = 0;
+        while (i < s1.size() && s1[i] == s2[i])
+            i++;
+        if (s1.size() == i) 
+            return true;
+        else if (s2.size() == i)
+            return false;
+        return (order.find(s1[i]) < order.find(s2[i]));
+    }
+    
+    bool isAlienSorted(vector<string>& words, string order) {
+        for (int i = 0; i < words.size()-1; i++) {
+            if (!compare(words[i], words[i+1], order))
+                return false;
+        }
+        return true;
+    }
+};
