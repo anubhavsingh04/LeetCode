d84be09de89efa5f302f970355fa83ffd7e5cb6a
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-03-29 18:29:26 +0530

    Add files via upload

---
+++ 287. Find the Duplicate Number/Code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(vector<int>nums)
+    {
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+                return nums[i];
+        }
+        return -1;
+    }
+    int findDuplicate(vector<int>& nums) {
+        return solve(nums);
+    }
+};
