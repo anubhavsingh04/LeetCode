c712dd8d295dad7d85cf7b2e144dd7e9ad193103
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-03-30 17:53:31 +0530

    Add files via upload

--- 28. Implement strStr()/Code.cpp
+++ 28. Implement strStr()/Code.cpp
@@ -16,3 +16,29 @@ public:
         return -1;
     }
 };
+
+// TWO POINTER 
+
+int strStr(string haystack, string needle) 
+    {
+        if(needle=="")
+            return 0;
+        
+        int i=0,j=0;
+        while(i<haystack.size() && j<needle.size())
+        {
+            if(haystack[i]==needle[j])
+            {
+                i++;
+                j++;
+                if(j==needle.size()) 
+                    return i-needle.size();
+            }
+            else
+            { 
+                i=i-j+1;  
+                j=0; 
+            }            
+        }        
+        return -1;
+    }
