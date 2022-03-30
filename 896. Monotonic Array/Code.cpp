546bdc4ad775cb8ad5c8056d926066f84f945d62
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-03-30 17:32:11 +0530

    Add files via upload

---
+++ 896. Monotonic Array/Code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int inc=0,dec=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]<= nums[i+1])
+            inc++;
+        }
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]>= nums[i+1])
+            dec++;
+        }
+        if(inc==nums.size()-1||dec==nums.size()-1)
+        return true;
+        return false;
+    }
+};
