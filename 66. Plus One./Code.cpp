cb4807c117f671b8b90df83df6c4b0bb84280b88
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-01 22:07:04 +0530

    Add files via upload

---
+++ 66. Plus One./Code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size()-1;
+        for(int i=n;i>=0;i--)        
+        {
+            if(digits[i]==9)
+            digits[i]=0;
+            else 
+            {
+                digits[i]+=1;
+                return digits;
+            }
+        }
+        digits[0]=1;
+        digits.push_back(0);
+        return digits;
+    }
+};
