0e00ea7a7941e43dc51b186add2d795c60806b3f
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-03 14:30:45 +0530

    Add files via upload

--- 31. Next Permutation/Code.cpp
+++ 31. Next Permutation/Code.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx=-1;
-        for(int i=nums.size()-1;i>0;i--)
-        {
-            if(nums[i]>nums[i-1])
-            {
-                idx=i-1;
-                break;
-            }
-        }
-        if(idx<0)
-        {
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-            if(nums[i]>nums[idx])
-            {
-                swap(nums[i],nums[idx]);
-                reverse(nums.begin()+idx+1,nums.end());
-                break;
-            }
-        }
+        next_permutation(nums.begin(),nums.end());
     }
 };
