fe634c57831a0a55bc928a8606348dea51781dd8
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-11 22:53:20 +0530

    Add files via upload

--- 304. Range Sum Query 2D - Immutable/Code.cpp
+++ 304. Range Sum Query 2D - Immutable/Code.cpp
@@ -1,20 +1,45 @@
 class NumMatrix {
-    vector<vector<int> > prefixsum;
-public:    
-    NumMatrix(vector<vector<int>>& matrix) : prefixsum(matrix) {
-		for(auto& row : prefixsum) 
-            for(int i = 1; i < size(row); i++) 
-                row[i] += row[i - 1];                
+public: 
+    vector<vector<int>>pfs;
+    NumMatrix(vector<vector<int>>& matrix) {
+		// vector<vector<int>>pfs;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        pfs.resize(n+1,vector<int>(m+1));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                pfs[i][j]=matrix[i-1][j-1]+pfs[i][j-1]+pfs[i-1][j]-pfs[i-1][j-1];
+            }
+        }
     }    
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-		
-        for(int row = row1; row <= row2; row++) 
-            sum += prefixsum[row][col2] - (col1 ? prefixsum[row][col1 - 1] : 0);
-        return sum;
+        ++row1;++col1;++row2;++col2; //since we are using 1 based pfs 
+        return pfs[row2][col2]-pfs[row1-1][col2]-pfs[row2][col1-1]+pfs[row1-1][col1-1];
     }
 };
+
+
+
+// class NumMatrix {
+//     vector<vector<int> > prefixsum;
+// public:    
+//     NumMatrix(vector<vector<int>>& matrix) : prefixsum(matrix) {
+// 		for(auto& row : prefixsum) 
+//             for(int i = 1; i < size(row); i++) 
+//                 row[i] += row[i - 1];                
+//     }    
+    
+//     int sumRegion(int row1, int col1, int row2, int col2) {
+//         int sum = 0;
+		
+//         for(int row = row1; row <= row2; row++) 
+//             sum += prefixsum[row][col2] - (col1 ? prefixsum[row][col1 - 1] : 0);
+//         return sum;
+//     }
+// };
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
