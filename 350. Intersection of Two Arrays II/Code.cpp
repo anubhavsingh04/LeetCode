b6c0eeff510eddf3a29913b62e5d73cf9c87e432
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-14 22:52:49 +0530

    Add files via upload

---
+++ 350. Intersection of Two Arrays II/Code.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int>m;
+        vector<int>v;
+        for(int i=0;i<nums1.size();i++)
+            m[nums1[i]]++;
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(m[nums2[i]]>0)
+            {
+                m[nums2[i]]--;
+                v.push_back(nums2[i]);
+            }
+        }
+        return v;
+    }
+};
