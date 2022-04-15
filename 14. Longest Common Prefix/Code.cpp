2266c53d370b9fdd223b4f7a88d92691a6488ad1
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-15 22:39:39 +0530

    Add files via upload

---
+++ 14. Longest Common Prefix/Code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        sort(strs.begin(),strs.end());
+        string s1=strs[0];
+        string s2=strs[strs.size()-1];
+        for(int i=0;i<s1.size();i++)
+        {
+            if(s1[i]==s2[i])
+                ans+=s1[i];
+            else 
+                break;
+        }
+        return ans ;
+    }
+};
