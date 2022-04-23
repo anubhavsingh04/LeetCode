909920088f2db563e7a9e7217b57f2d826d5e1fa
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-23 10:30:26 +0530

    Add files via upload

--- 535. Encode and Decode TinyURL/Code.cpp
+++ 535. Encode and Decode TinyURL/Code.cpp
@@ -1,14 +1,32 @@
-class Solution {
-public:
+// class Solution {
+// public:
 
+//     // Encodes a URL to a shortened URL.
+//     string encode(string longUrl) {
+//         return longUrl;
+//     }
+
+//     // Decodes a shortened URL to its original URL.
+//     string decode(string shortUrl) {
+//         return shortUrl;
+//     }
+// };
+
+class Solution {
+    unordered_map<string,string>m;
+    int cnt=0;
+public:
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        string shortUrl="http://tinyurl.com/"+to_string(cnt);
+        m[shortUrl]=longUrl;
+        cnt++;
+        return shortUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return m[shortUrl];
     }
 };
 
