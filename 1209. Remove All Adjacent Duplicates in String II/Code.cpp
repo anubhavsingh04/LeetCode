f4ed7b5b204cdbf5b619707b21ff2cfeb5282b16
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-05-07 00:02:15 +0530

    Add files via upload

--- 1209. Remove All Adjacent Duplicates in String II/Code.cpp
+++ 1209. Remove All Adjacent Duplicates in String II/Code.cpp
@@ -1,17 +1,60 @@
 class Solution {
 public:
-    string removeDuplicates(string S, int K) {
-        int i, j;
-        stack<int> st;
-        st.push(0);
-        for (i = 1, j = 1; j < S.size(); i++, j++) {
-            S[i] = S[j];
-            if (i == 0 || S[i] != S[i-1]) st.push(i);
-            else if (i - st.top() + 1 == K) {
-                i = st.top() - 1;
-                st.pop();
+    string removeDuplicates(string s, int k) {
+        stack<pair<char,int>>st;
+        
+        string ans;
+        for(int i=0;i<s.size();i++)
+        {
+            if(st.empty())
+            {
+                st.push({s[i],1});
+                continue;
+            }
+            else if(s[i]==st.top().first)
+            {
+                st.push({s[i],st.top().second+1});
+                if(st.top().second==k)
+                {
+                    int x=k;
+                    while(x--)
+                    {
+                        st.pop();    
+                    }
+                }
+            }
+            else if(s[i]!=st.top().first)
+            {
+                st.push({s[i],1});
             }
         }
-        return S.substr(0, i);
+        while(!st.empty())
+        {
+            char c=st.top().first;
+            ans.push_back(c);
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
+    
 };
+
+// class Solution {
+// public:
+//     string removeDuplicates(string s, int k) {
+//         stack<int>st;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(i==0||s[i]!=s[i-1])
+//                 st.push(1);
+//             else if(++st.top()==k)
+//             {
+//                 st.pop();
+//                 s.erase(i-k+1,k);
+//                 i=i-k;
+//             }
+//         }
+//         return s;
+//     }
+// };
