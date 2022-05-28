ce0e28162ea6c45619e50d53a3655c3b925b6102
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-05-28 13:06:57 +0530

    Time: 30 ms (40.13%), Space: 18 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,17 +1,25 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int ans;
+//         int sz=nums.size();
+//         nums.resize(nums.size()+1);
+//         for(int i=0;i<nums.size()+1;i++)
+//         {
+//             if(nums[i]!=i)
+//             {
+//                 ans=i;break;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans;
-        int sz=nums.size();
-        nums.resize(nums.size()+1);
-        for(int i=0;i<nums.size()+1;i++)
-        {
-            if(nums[i]!=i)
-            {
-                ans=i;break;
-            }
-        }
-        return ans;
+        int n=nums.size();
+        return n*(n+1)/2-accumulate(nums.begin(),nums.end(),0);
     }
 };
