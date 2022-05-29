8a613f029c0680ecf054d1f1b60afe45ab7c3096
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-05-30 03:42:18 +0530

    Time: 8 ms (47.05%), Space: 9.1 MB (26.82%) - LeetHub

--- 942-di-string-match/942-di-string-match.cpp
+++ 942-di-string-match/942-di-string-match.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
-        int a=0,b=s.size();
+        // int a=0,b=s.size();
+        // vector<int>ans;
+        // for(auto &i:s)
+        // {
+        //     if(i=='I') ans.push_back(a++);
+        //     else if(i=='D') ans.push_back(b--);
+        // }
+        // if(s[s.size()-1]=='D') ans.push_back(b--);
+        // if(s[s.size()-1]=='I') ans.push_back(a++);
+        // return ans;
+        int l=0,r=s.size();
         vector<int>ans;
         for(auto &i:s)
         {
-            if(i=='I') ans.push_back(a++);
-            else if(i=='D') ans.push_back(b--);
+            if(i=='D') ans.push_back(r--);
+            else ans.push_back(l++);
         }
-        if(s[s.size()-1]=='D') ans.push_back(b--);
-        if(s[s.size()-1]=='I') ans.push_back(a++);
+        ans.push_back(l++);
         return ans;
     }
 };
