d0eb2b674fb7273969f5ea78eea0525e55b225fd
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-05-30 16:26:15 +0530

    Time: 57 ms (68.64%), Space: 41.4 MB (81.15%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -9,18 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int sumEvenGrandparent(TreeNode* root) {
+//         if(!root) return 0;
+//         int sum=0;
+//         if(!(root->val&1))
+//         {
+//             if(root->left!=NULL&&root->left->left!=NULL) sum+=root->left->left->val;
+//             if(root->left!=NULL&&root->left->right!=NULL) sum+=root->left->right->val;
+//             if(root->right!=NULL&&root->right->left!=NULL) sum+=root->right->left->val;
+//             if(root->right!=NULL&&root->right->right!=NULL) sum+=root->right->right->val;
+//         }
+//         return sum + sumEvenGrandparent(root->left)+ sumEvenGrandparent(root->right);
+//     }
+// };
+
 class Solution {
 public:
     int sumEvenGrandparent(TreeNode* root) {
-        if(!root) return 0;
+        return func(root,NULL,NULL);
+    }
+    int func(TreeNode*curr,TreeNode*p,TreeNode*gp) 
+    {
+        if(curr==NULL) return 0;
         int sum=0;
-        if(!(root->val&1))
+        if(gp!=NULL&&!(gp->val&1)) // if gp is even and curr is not null
         {
-            if(root->left!=NULL&&root->left->left!=NULL) sum+=root->left->left->val;
-            if(root->left!=NULL&&root->left->right!=NULL) sum+=root->left->right->val;
-            if(root->right!=NULL&&root->right->left!=NULL) sum+=root->right->left->val;
-            if(root->right!=NULL&&root->right->right!=NULL) sum+=root->right->right->val;
+            sum+=curr->val;
         }
-        return sum + sumEvenGrandparent(root->left)+ sumEvenGrandparent(root->right);
+        sum+=func(curr->left,curr,p);
+        sum+=func(curr->right,curr,p);
+        return sum;
     }
 };
