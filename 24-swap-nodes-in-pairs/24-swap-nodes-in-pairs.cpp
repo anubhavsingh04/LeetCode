8e78ae7c5aa8c05916a7770da60c2a9d4f99962f
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-01 03:00:30 +0530

    Time: 4 ms (49.97%), Space: 7.5 MB (91.98%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -8,22 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         if(!head||!head->next) return head;
-        
-        ListNode*dummy=new ListNode();
-        ListNode*curr=head,*prev=dummy;
-        while(curr&&curr->next)
-        {
-            prev->next=curr->next;
-            curr->next=prev->next->next;
-            prev->next->next=curr;
-            
-            prev=curr;
-            curr=curr->next;
-        }
-        return dummy->next;
+        ListNode*tmp=head->next;
+        head->next=swapPairs(head->next->next);
+        tmp->next=head;
+        return tmp;
     }
 };
+
+
+// class Solution {
+// public:
+//     ListNode* swapPairs(ListNode* head) {
+//         if(!head||!head->next) return head;
+        
+//         ListNode*dummy=new ListNode();
+//         ListNode*curr=head,*prev=dummy;
+//         while(curr&&curr->next)
+//         {
+//             prev->next=curr->next;
+//             curr->next=prev->next->next;
+//             prev->next->next=curr;
+            
+//             prev=curr;
+//             curr=curr->next;
+//         }
+//         return dummy->next;
+//     }
+// };
