835098bb4f2dd82d70d74e9224d3e9fc09b6c4c8
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-01 12:26:20 +0530

    Time: 3 ms (42.37%), Space: 5.9 MB (44.24%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,23 +1,36 @@
+// class Solution {
+// public:
+//     // if n%3==0 || n%3==1 then only n can be expressed as power of 2 
+//     bool checkPowersOfThree(int n) {
+//         int cnt=0;
+//         vector<int>power(15);
+//         while(n)
+//         {
+//             if(!(n%3)) 
+//             {
+//                 n/=3;
+//                 cnt++;
+//             }
+//             else if(n%3==1)
+//             {
+//                 n--;
+//                 power[cnt]++;
+//                 if(power[cnt]>=2) return false;
+//             }
+//             else return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     // if n%3==0 || n%3==1 then only n can be expressed as power of 2 
     bool checkPowersOfThree(int n) {
-        int cnt=0;
-        vector<int>power(15);
         while(n)
         {
-            if(!(n%3)) 
-            {
-                n/=3;
-                cnt++;
-            }
-            else if(n%3==1)
-            {
-                n--;
-                power[cnt]++;
-                if(power[cnt]>=2) return false;
-            }
-            else return false;
+            if(n%3==2) return false;
+            n/=3;
         }
         return true;
     }
