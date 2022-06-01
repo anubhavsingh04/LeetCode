fc6d9d63d8037b4cc0f519ccf7abd004fdd1ddf0
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-01 12:02:16 +0530

    Time: 38 ms (27.59%), Space: 5.8 MB (89.18%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,3 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<1) return false;
+        while(n!=1) 
+        {
+            if(n%3!=0) return false;
+            n/=3;
+        }
+        return true;
+    }
+};
+
+// Loop faster 62% tc: O(log3(n)) space O(1)
 // class Solution {
 // public:
 //     bool isPowerOfThree(int n) {
@@ -8,11 +22,11 @@
 //     }
 // };
 
-// recursive 
-class Solution {
-public:
-    bool isPowerOfThree(int n) {
-        if(n<=1) return n==1;
-        return (n%3==0&&isPowerOfThree(n/3));
-    }
-};
+// recursive TC: o(Log3(n)) space o(log3(n)) fatser 35% 
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if(n<=1) return n==1;
+//         return (n%3==0&&isPowerOfThree(n/3));
+//     }
+// };
