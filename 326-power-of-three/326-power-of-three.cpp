faba62d349c096085bc8adee7da750ad17708315
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-01 11:52:36 +0530

    Time: 35 ms (35.85%), Space: 5.9 MB (73.82%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,9 +1,18 @@
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if(!n) return false;
+//         while(n%3==0)
+//             n/=3;
+//         return n==1;
+//     }
+// };
+
+// recursive 
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(!n) return false;
-        while(n%3==0)
-            n/=3;
-        return n==1;
+        if(n<=1) return n==1;
+        return (n%3==0&&isPowerOfThree(n/3));
     }
 };
