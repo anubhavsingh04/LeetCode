a31833b4b5df68d7a48b4e6729745eba0a752119
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-02 19:54:49 +0530

    Time: 3 ms (86.77%), Space: 12.5 MB (22.55%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -1,14 +1,39 @@
+// class Solution {
+// public:
+//     int findMiddleIndex(vector<int>& nums) {
+//         int totalsum=accumulate(nums.begin(),nums.end(),0);
+//         int leftsum=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             totalsum-=nums[i];
+//             if(leftsum==totalsum)
+//                 return i;
+//             leftsum+=nums[i];
+//         }
+//         return -1;
+//     }
+// };
+
+// Prefix sum
 class Solution {
 public:
     int findMiddleIndex(vector<int>& nums) {
-        int totalsum=accumulate(nums.begin(),nums.end(),0);
-        int leftsum=0;
-        for(int i=0;i<nums.size();i++)
+        // int totalsum=accumulate(nums.begin(),nums.end(),0);
+        vector<int>prefix(nums.size(),0);
+        prefix[0]=nums[0];
+        int totalsum=nums[0];
+        
+        for(int i=1;i<nums.size();i++)
         {
-            totalsum-=nums[i];
-            if(leftsum==totalsum)
-                return i;
-            leftsum+=nums[i];
+            totalsum+=nums[i];
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        if(totalsum-prefix[0]==0) return 0;
+        else
+        for(int i=1;i<nums.size();i++)
+        {
+            if(prefix[i-1]==(totalsum-prefix[i]))
+            return i;
         }
         return -1;
     }
