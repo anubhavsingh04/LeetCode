fe5463a20231a86482fb1f83a5f6bc0f1771b643
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-03 10:20:11 +0530

    Time: 4 ms (79.36%), Space: 9.7 MB (28.66%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,40 +2,40 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         
-        // for (auto a : arr) 
-        //     if (a <= k) 
-        //         k++;
-        //     else 
-        //          break
-        // return k; 
+        for (auto a : arr) 
+            if (a <= k) 
+                k++;
+            else 
+                 break;
+        return k; 
         
-         if(arr[0]!=1)
-        {
-            int res=arr[0]-1;
-            if(res<k)
-                k-=res;
-            else
-                return k;
-        }
-        for(int i=0; i<arr.size()-1; i++)
-        {
-            int sum=arr[i+1]-arr[i];
-            if(sum>1)
-            {
-                if(sum>k)
-                {    
-                   int ans =arr[i]+k;
-                   return ans;
-                }
-                else
-                {
-                    k-=sum-1;
-                }
-            }
+//          if(arr[0]!=1)
+//         {
+//             int res=arr[0]-1;
+//             if(res<k)
+//                 k-=res;
+//             else
+//                 return k;
+//         }
+//         for(int i=0; i<arr.size()-1; i++)
+//         {
+//             int sum=arr[i+1]-arr[i];
+//             if(sum>1)
+//             {
+//                 if(sum>k)
+//                 {    
+//                    int ans =arr[i]+k;
+//                    return ans;
+//                 }
+//                 else
+//                 {
+//                     k-=sum-1;
+//                 }
+//             }
             
                 
-        }
-       return arr[arr.size()-1]+k;
+//         }
+//        return arr[arr.size()-1]+k;
     
     }
 };
