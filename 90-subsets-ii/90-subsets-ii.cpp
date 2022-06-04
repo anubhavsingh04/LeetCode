374c2f2a99db4604053501d4410fafd3ad49cfe7
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-04 18:19:40 +0530

    Time: 21 ms (6.49%), Space: 7.3 MB (96.80%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,29 +1,53 @@
+// class Solution {
+// public:
+//     vector<vector<int>>v;
+//     set<vector<int>>st;
+//     void solve(vector<int> ip,vector<int>op)
+//     {
+//         if(ip.size()==0)
+//         {
+//             sort(op.begin(),op.end());
+//             st.insert(op);
+//             return ;
+//         }
+//         vector<int>op1=op;
+//         vector<int>op2=op;
+//         op2.push_back(ip[0]);
+//         ip.erase(ip.begin()+0);
+//         solve(ip,op1);
+//         solve(ip,op2);
+//         return ;
+//     }
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         vector<int>op;
+//         solve(nums,op);
+//         for(auto &i:st){
+//             v.push_back(i);
+//         }
+//         return v;
+//     }
+// };
+
 class Solution {
 public:
-    vector<vector<int>>v;
-    set<vector<int>>st;
-    void solve(vector<int> ip,vector<int>op)
-    {
-        if(ip.size()==0)
-        {
-            sort(op.begin(),op.end());
-            st.insert(op);
-            return ;
-        }
-        vector<int>op1=op;
-        vector<int>op2=op;
-        op2.push_back(ip[0]);
-        ip.erase(ip.begin()+0);
-        solve(ip,op1);
-        solve(ip,op2);
-        return ;
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int>op;
-        solve(nums,op);
-        for(auto &i:st){
-            v.push_back(i);
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> res = {{}};
+        vector<int> curr;
+        int size;
+        
+        for (auto num : nums) {
+            size = res.size();
+            
+            for (int i = 0; i < size; i++) {
+                curr = res[i];
+                curr.push_back(num);
+                if (find(res.begin(), res.end(), curr) == res.end())
+                    res.push_back(curr);
+            }
         }
-        return v;
+        
+        return res;
     }
 };
