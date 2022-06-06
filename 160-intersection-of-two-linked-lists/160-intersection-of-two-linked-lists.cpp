bb170ef61a7f3a5b7916453f2dbbe5ced58c9841
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-06 12:09:06 +0530

    Time: 53 ms (73.19%), Space: 14.5 MB (72.44%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -6,63 +6,86 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Map soln --> Put in map and check for same node is there or not TC: O(m+n) ,SC: O(m)
+
 // class Solution {
 // public:
 //     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-//         ListNode*tmp=headA,*ptr=headB;
+//         ListNode*p=headA,*q=headB;
 //         map<ListNode*,int>m;
-//         while(tmp!=NULL)
+//         while(p!=NULL)
 //         {
-//             m[tmp]++;
-//             tmp=tmp->next;
+//             m[p]++;
+//             p=p->next;
 //         }
-//         while(ptr!=NULL)
+//         while(q!=NULL)
 //         {
-//             if(m[ptr]>0) return ptr;
-//             ptr=ptr->next;
+//             if(m[q]>0) return q;
+//             q=q->next;
 //         }
 //         return NULL;
 //     }
 // };
 
+// len diffrence soln --> make the length of both ll same and iterate until get same node 
+// TC: O(m+n), SC: O(1)
+
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         int a=0,b=0;
+//         ListNode*p=headA,*q=headB;
+//         while(p!=NULL)
+//         {
+//             a++;
+//             p=p->next;
+//         }
+//         while(q!=NULL)
+//         {
+//             b++;
+//             q=q->next;
+//         }
+//         p=headA;
+//         q=headB;
+//         int diff=abs(a-b);
+//         if(a>b)
+//         {
+//             while(diff--)
+//                 p=p->next;
+//         }
+//         else 
+//         {
+//             while(diff--)
+//                 q=q->next;
+//         }
+//         while(p!=NULL&&q!=NULL)
+//         {
+//             if(p==q)
+//                 return p;
+//             else
+//             {
+//                 p=p->next;
+//                 q=q->next;
+//             }
+//         }
+//         return NULL;
+//     }
+// };
+
+// Two Pointer Soln
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int a=0,b=0;
         ListNode*p=headA,*q=headB;
-        while(p!=NULL)
+        while(p!=q)
         {
-            a++;
-            p=p->next;
+            if(p==NULL) p=headB;
+            else  p=p->next;
+            
+            if(q==NULL) q=headA;
+            else q=q->next;
         }
-        while(q!=NULL)
-        {
-            b++;
-            q=q->next;
-        }
-        p=headA;
-        q=headB;
-        int diff=abs(a-b);
-        if(a>b)
-        {
-            while(diff--)
-                p=p->next;
-        }
-        else 
-        {
-            while(diff--)
-                q=q->next;
-        }
-        while(p!=NULL&&q!=NULL)
-        {
-            if(p==q)
-                return p;
-            else
-            {
-                p=p->next;
-                q=q->next;
-            }
-        }
-        return NULL;
+        return p;
     }
 };
