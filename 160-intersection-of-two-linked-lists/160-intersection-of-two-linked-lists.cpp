5f4bee83058882eb23b04cc0b73a04c6cbe76581
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-06 11:54:26 +0530

    Time: 49 ms (84.74%), Space: 14.6 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -6,20 +6,62 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode*tmp=headA,*ptr=headB;
+//         map<ListNode*,int>m;
+//         while(tmp!=NULL)
+//         {
+//             m[tmp]++;
+//             tmp=tmp->next;
+//         }
+//         while(ptr!=NULL)
+//         {
+//             if(m[ptr]>0) return ptr;
+//             ptr=ptr->next;
+//         }
+//         return NULL;
+//     }
+// };
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode*tmp=headA,*ptr=headB;
-        map<ListNode*,int>m;
-        while(tmp!=NULL)
+        int a=0,b=0;
+        ListNode*p=headA,*q=headB;
+        while(p!=NULL)
         {
-            m[tmp]++;
-            tmp=tmp->next;
+            a++;
+            p=p->next;
         }
-        while(ptr!=NULL)
+        while(q!=NULL)
         {
-            if(m[ptr]>0) return ptr;
-            ptr=ptr->next;
+            b++;
+            q=q->next;
+        }
+        p=headA;
+        q=headB;
+        int diff=abs(a-b);
+        if(a>b)
+        {
+            while(diff--)
+                p=p->next;
+        }
+        else 
+        {
+            while(diff--)
+                q=q->next;
+        }
+        while(p!=NULL&&q!=NULL)
+        {
+            if(p==q)
+                return p;
+            else
+            {
+                p=p->next;
+                q=q->next;
+            }
         }
         return NULL;
     }
