33840bc05a5dcc058797b62ebe7f34ce5980391b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-07 23:21:16 +0530

    Time: 21 ms (40.81%), Space: 24.2 MB (33.75%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,13 +1,39 @@
+// Josephus problem 
+// class Solution {
+// public:
+    
+//     int findTheWinner(int n, int k) {
+//         if(n==1) return 1;
+//         else
+//         {
+//             int tmp=findTheWinner(n-1,k);
+//             int ans=(tmp+k-1)%n+1;
+//             return ans;
+//         }
+//     }
+// };
+
 class Solution {
 public:
+    void solve(int k,vector<int>v,int &ans,int index){
+        if(v.size()==1){
+            ans=v[0];
+            return;
+        }
+        index=(index+k)%(v.size());
+        v.erase(v.begin()+index);
+        solve(k,v,ans,index);
+        return;
+    }
     
     int findTheWinner(int n, int k) {
-        if(n==1) return 1;
-        else
-        {
-            int tmp=findTheWinner(n-1,k);
-            int ans=(tmp+k-1)%n+1;
-            return ans;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            v[i]=i+1;
         }
+        k--;
+        int ans=-1;
+        solve(k,v,ans,0);
+        return ans;
     }
 };
