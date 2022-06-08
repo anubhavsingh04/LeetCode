6c1cba3582d4932e47790fa0b530fa22e3676f6e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-08 09:15:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,15 +1,29 @@
+// class Solution {
+// public:
+//     bool ispalindrome(string s,int start,int end)
+//     {
+//         if(start==end) return true;
+//         if(s[start]!=s[end]) return false;
+//         if(start<end) return ispalindrome(s,start+1,end-1);
+//         return true;
+//     }
+//     int removePalindromeSub(string s) {
+//         if(s=="") return 0;
+//         if(ispalindrome(s,0,s.size()-1)) return 1;
+//         else return 2;
+//     }
+// };
+
 class Solution {
 public:
-    bool ispalindrome(string s,int start,int end)
-    {
-        if(start==end) return true;
-        if(s[start]!=s[end]) return false;
-        if(start<end) return ispalindrome(s,start+1,end-1);
-        return true;
-    }
+   
     int removePalindromeSub(string s) {
-        if(s=="") return 0;
-        if(ispalindrome(s,0,s.size()-1)) return 1;
-        else return 2;
+        int i=0,j=s.size()-1;
+        while(i<j)
+        {
+            if(s[i]!=s[j]) return 2;
+            i++;j--;
+        }
+        return 1;
     }
 };
