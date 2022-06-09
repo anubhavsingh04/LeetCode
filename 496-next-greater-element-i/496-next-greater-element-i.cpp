e2fc6bbc7e190a174dca436636643e5fb9f41a3e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-09 16:32:50 +0530

    Time: 7 ms (74.99%), Space: 9 MB (31.49%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,3 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       vector<int>ans;
+        stack<int>st;
+        map<int,int>m;
+        for(int i=nums2.size()-1;i>=0;i--)
+        {
+            while(!st.empty()&&st.top()<nums2[i])
+                st.pop();
+            
+            if(st.empty())
+                m[nums2[i]]=-1;
+            else 
+                m[nums2[i]]=st.top();
+            
+            st.push(nums2[i]);   
+        }
+        for(auto &i:nums1)
+        {
+            ans.push_back(m[i]);
+        }
+        return ans ;
+    }
+};
+
 // class Solution {
 // public:
 //     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
@@ -27,28 +53,28 @@
 //     }
 // };
 
-class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        for(int i=0;i<nums1.size();i++)
-        {
-            for(int j=0;j<nums2.size();j++)
-            {
-                if(nums1[i]==nums2[j]){
-                    int tmp=-1;
-                    for(int k=j+1;k<nums2.size();k++)
-                    {
-                        if(nums2[k]>nums2[j])
-                        {
-                            tmp=nums2[k];
-                            break;
-                        }
-                    }
-                    ans.push_back(tmp);
-                }
-            }
-        }
-         return ans;
-    }
-};
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int>ans;
+//         for(int i=0;i<nums1.size();i++)
+//         {
+//             for(int j=0;j<nums2.size();j++)
+//             {
+//                 if(nums1[i]==nums2[j]){
+//                     int tmp=-1;
+//                     for(int k=j+1;k<nums2.size();k++)
+//                     {
+//                         if(nums2[k]>nums2[j])
+//                         {
+//                             tmp=nums2[k];
+//                             break;
+//                         }
+//                     }
+//                     ans.push_back(tmp);
+//                 }
+//             }
+//         }
+//          return ans;
+//     }
+// };
