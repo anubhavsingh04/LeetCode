ce5d053dfb28272b5b8a20b1c14fccb88f7acc9b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-09 08:56:20 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (16.50%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,49 +9,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-//     vector<int>v;
-// public:
-//     void inorder(TreeNode*root)
-//     {
-//         if(root==NULL)
-//             return ;
-//          inorder(root->left);
-//         v.push_back(root->val);
-//         inorder(root->right);
-//     }
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         inorder(root);
-//         return v;
-//     }
-// };
-
-// Iterative inorder traversal 
-
 class Solution {
     vector<int>v;
 public:
-    
+    void inorder(TreeNode*root)
+    {
+        if(root==NULL)
+            return ;
+         inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(!root) return ans;
-        stack<TreeNode*>st;
-        TreeNode*curr=root;
-        while(true)
-        {
-            if(curr!=NULL){
-                 st.push(curr);
-                curr=curr->left;
-            }
-            else {
-                if(st.empty()) break;
-                
-                    curr=st.top();
-                    st.pop();
-                    ans.push_back(curr->val);
-                    curr=curr->right;
-            }   
-        }
-        return ans;
+        inorder(root);
+        return v;
     }
 };
+
+// Iterative inorder traversal 
+
+// class Solution {
+//     vector<int>v;
+// public:
+    
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int>ans;
+//         if(!root) return ans;
+//         stack<TreeNode*>st;
+//         TreeNode*curr=root;
+//         while(true)
+//         {
+//             if(curr!=NULL){
+//                  st.push(curr);
+//                 curr=curr->left;
+//             }
+//             else {
+//                 if(st.empty()) break;
+                
+//                     curr=st.top();
+//                     st.pop();
+//                     ans.push_back(curr->val);
+//                     curr=curr->right;
+//             }   
+//         }
+//         return ans;
+//     }
+// };
