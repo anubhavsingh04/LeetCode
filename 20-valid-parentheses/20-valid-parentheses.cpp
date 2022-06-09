33f527cdb7c618ee1568f07470d8143759cea6df
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-09 15:25:08 +0530

    Time: 4 ms (30.69%), Space: 6.4 MB (51.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,51 +1,51 @@
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         stack<char>st;
-//         unordered_map<char,int>m={ {'(',-1},{')',1},{'{',-2},{'}',2},{'[',-3},{']',3} };
-//         for(char ch: s)
-//         {
-//             if(m[ch]<0)
-//             {
-//                 st.push(ch);
-//             }
-//             else 
-//             {
-//                 if(st.empty())
-//                     return false ;
-//                 else 
-//                 {
-//                     char c=st.top();
-//                     if(m[ch]+m[c]!=0)
-//                         return false ;
-//                     else 
-//                         st.pop();
-//                 }
-//             }
-//         }
-//         return st.empty();
-//     }
-// };
-
-
 class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        for(int i=0;i<s.size();i++)
+        unordered_map<char,int>m={ {'(',-1},{')',1},{'{',-2},{'}',2},{'[',-3},{']',3} };
+        for(char ch: s)
         {
-            if(s[i]=='('||s[i]=='{'||s[i]=='[')
-                st.push(s[i]);
-            else {
-                if(st.empty()) return false ;
-                else {
-                    char x=st.top();
-                    if(x=='('&&s[i]==')'||x=='{'&&s[i]=='}'||x=='['&&s[i]==']')
+            if(m[ch]<0)
+            {
+                st.push(ch);
+            }
+            else 
+            {
+                if(st.empty())
+                    return false ;
+                else 
+                {
+                    char c=st.top();
+                    if(m[ch]+m[c]!=0)
+                        return false ;
+                    else 
                         st.pop();
-                    else return false;
                 }
             }
         }
         return st.empty();
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char>st;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(s[i]=='('||s[i]=='{'||s[i]=='[')
+//                 st.push(s[i]);
+//             else {
+//                 if(st.empty()) return false ;
+//                 else {
+//                     char x=st.top();
+//                     if(x=='('&&s[i]==')'||x=='{'&&s[i]=='}'||x=='['&&s[i]==']')
+//                         st.pop();
+//                     else return false;
+//                 }
+//             }
+//         }
+//         return st.empty();
+//     }
+// };
