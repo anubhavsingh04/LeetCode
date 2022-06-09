980a4ebc89488e2b423a1e04041cf5e5cc86e8a4
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-09 15:24:45 +0530

    Time: 6 ms (11.08%), Space: 6.4 MB (51.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,25 +1,48 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char>st;
+//         unordered_map<char,int>m={ {'(',-1},{')',1},{'{',-2},{'}',2},{'[',-3},{']',3} };
+//         for(char ch: s)
+//         {
+//             if(m[ch]<0)
+//             {
+//                 st.push(ch);
+//             }
+//             else 
+//             {
+//                 if(st.empty())
+//                     return false ;
+//                 else 
+//                 {
+//                     char c=st.top();
+//                     if(m[ch]+m[c]!=0)
+//                         return false ;
+//                     else 
+//                         st.pop();
+//                 }
+//             }
+//         }
+//         return st.empty();
+//     }
+// };
+
+
 class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        unordered_map<char,int>m={ {'(',-1},{')',1},{'{',-2},{'}',2},{'[',-3},{']',3} };
-        for(char ch: s)
+        for(int i=0;i<s.size();i++)
         {
-            if(m[ch]<0)
-            {
-                st.push(ch);
-            }
-            else 
-            {
-                if(st.empty())
-                    return false ;
-                else 
-                {
-                    char c=st.top();
-                    if(m[ch]+m[c]!=0)
-                        return false ;
-                    else 
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+                st.push(s[i]);
+            else {
+                if(st.empty()) return false ;
+                else {
+                    char x=st.top();
+                    if(x=='('&&s[i]==')'||x=='{'&&s[i]=='}'||x=='['&&s[i]==']')
                         st.pop();
+                    else return false;
                 }
             }
         }
