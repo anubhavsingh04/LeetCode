22f9a4c22df493f74a123e55d55c55ed513fdbb9
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-10 16:39:07 +0530

    Time: 174 ms (14.45%), Space: 47 MB (8.70%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int cnt=0,n=nums.size();
-       vector<int>prefix(n);
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++)
-        {
-            prefix[i]=prefix[i-1]+nums[i];
-        }
-        // if prefix[i]-prefix[j]==k then from index j+1 to i we have sum=k 
+        int cnt=0,n=nums.size(),sum=0;
         unordered_map<int,int>m;
-        for(int i=0;i<n;i++)
+        m[0]=1;
+        for(auto &i:nums)
         {
-            if(prefix[i]==k)
-                cnt++;
-            if(m.find(prefix[i]-k)!=m.end())
-            {
-                cnt+=m[prefix[i]-k];
-            }
-            m[prefix[i]]++;
+            sum+=i;
+            cnt+=m[sum-k];
+            m[sum]++;
         }
         return cnt;
     }
 };
+
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int cnt=0,n=nums.size();
+//        vector<int>prefix(n);
+//         prefix[0]=nums[0];
+//         for(int i=1;i<n;i++)
+//         {
+//             prefix[i]=prefix[i-1]+nums[i];
+//         }
+//         // if prefix[i]-prefix[j]==k then from index j+1 to i we have sum=k 
+//         unordered_map<int,int>m;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(prefix[i]==k)
+//                 cnt++;
+//             if(m.find(prefix[i]-k)!=m.end())
+//             {
+//                 cnt+=m[prefix[i]-k];
+//             }
+//             m[prefix[i]]++;
+//         }
+//         return cnt;
+//     }
+// };
 
 
 // Brute force gives TLE
