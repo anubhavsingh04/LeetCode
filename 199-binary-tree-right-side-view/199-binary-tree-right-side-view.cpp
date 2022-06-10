eec9af95549ee14ff2d1221479a16cf0e53f107f
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-11 02:46:06 +0530

    Time: 4 ms (60.46%), Space: 11.9 MB (67.97%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -9,33 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// Har level ka last node right vieew hoga 
 class Solution {
 public:
+    void solve(TreeNode*root,vector<int>&ans,int level)
+    {
+        if(!root) return;
+        
+        if(ans.size()==level) ans.push_back(root->val);
+        
+        solve(root->right,ans,level+1);
+        solve(root->left,ans,level+1);
+    }
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
-        if(!root) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty())
-        {
-            int sz=q.size();
-            vector<int>v;
-            for(int i=0;i<sz;i++)
-            {
-                TreeNode*curr=q.front();
-                q.pop();
-                
-                if(curr->left)
-                    q.push(curr->left);
-                if(curr->right)
-                    q.push(curr->right);
-                
-                v.push_back(curr->val);
-            }
-            ans.push_back(v.back());
-        }
+        solve(root,ans,0);
         return ans;
     }
 };
+
+
+// Har level ka last node right vieew hoga 
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int>ans;
+//         if(!root) return ans;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty())
+//         {
+//             int sz=q.size();
+//             vector<int>v;
+//             for(int i=0;i<sz;i++)
+//             {
+//                 TreeNode*curr=q.front();
+//                 q.pop();
+                
+//                 if(curr->left)
+//                     q.push(curr->left);
+//                 if(curr->right)
+//                     q.push(curr->right);
+                
+//                 v.push_back(curr->val);
+//             }
+//             ans.push_back(v.back());
+//         }
+//         return ans;
+//     }
+// };
