de42c81076d13563c0ddd273444970c0af3ceee2
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-11 16:25:35 +0530

    Time: 29 ms (23.84%), Space: 21.1 MB (68.27%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -15,44 +15,44 @@
 // jis node ke corresponding left aur right subtree ki heights same hongi
 // vahi leaf ka common lca hoga 
 
-// class Solution {
-// public:
-//     int height(TreeNode*root){
-//         if(!root)
-//             return 0;
-//         int lh=height(root->left);
-//         int rh=height(root->right);
-//         return 1+max(lh,rh);
-//     }
-//     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-//         int left=height(root->left);
-//         int right=height(root->right);
-//         if(left<right)
-//             return lcaDeepestLeaves(root->right);
-//         else if(left>right)
-//             return lcaDeepestLeaves(root->left);
-//         else 
-//             return root;
-//     }
-// };
-
-
 class Solution {
 public:
-    int dfs(TreeNode*root,TreeNode*&ans,int l,int &maxdepth)
-    {
-        if(!root) return l-1;
-        maxdepth=max(maxdepth,l);
-        int left=dfs(root->left,ans,l+1,maxdepth);
-        int right=dfs(root->right,ans,l+1,maxdepth);
-        if(left==maxdepth&&right==maxdepth)
-            ans=root;
-        return max(left,right);
+    int height(TreeNode*root){
+        if(!root)
+            return 0;
+        int lh=height(root->left);
+        int rh=height(root->right);
+        return 1+max(lh,rh);
     }
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        TreeNode*ans;
-        int maxdepth=0;
-        dfs(root,ans,0,maxdepth);
-        return ans;
+        int left=height(root->left);
+        int right=height(root->right);
+        if(left<right)
+            return lcaDeepestLeaves(root->right);
+        else if(left>right)
+            return lcaDeepestLeaves(root->left);
+        else 
+            return root;
     }
 };
+
+
+// class Solution {
+// public:
+//     int dfs(TreeNode*root,TreeNode*&ans,int l,int &maxdepth)
+//     {
+//         if(!root) return l-1;
+//         maxdepth=max(maxdepth,l);
+//         int left=dfs(root->left,ans,l+1,maxdepth);
+//         int right=dfs(root->right,ans,l+1,maxdepth);
+//         if(left==maxdepth&&right==maxdepth)
+//             ans=root;
+//         return max(left,right);
+//     }
+//     TreeNode* lcaDeepestLeaves(TreeNode* root) {
+//         TreeNode*ans;
+//         int maxdepth=0;
+//         dfs(root,ans,0,maxdepth);
+//         return ans;
+//     }
+// };
