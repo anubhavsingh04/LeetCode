99d63a1ef8b4c27b6088ca5b2f7c7c13e1e80747
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-11 16:50:52 +0530

    Time: 10 ms (74.19%), Space: 17.3 MB (53.45%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -13,34 +13,72 @@ class Solution {
 public:
   
     int widthOfBinaryTree(TreeNode* root) {
+        int ans=0;
+        if(!root) return ans;
         queue<pair<TreeNode*,int>>q;
-        int ans=INT_MIN;
-        q.push({root,1});
-        while(!q.empty())
-        {
-            int sz=q.size(),maxi,mini;
-            int min_idx=q.front().second;
+        q.push({root,0}); // 0 based indexing 
+        
+        while(!q.empty()){
+            int sz=q.size();
+            int mini=q.front().second;
+            int first,last;
             for(int i=0;i<sz;i++)
             {
-                int curr_idx=q.front().second-min_idx+1;
-                
+                int curr_idx=q.front().second-mini; // to avoid overflow and to normalize index 
                 TreeNode*curr=q.front().first;
                 q.pop();
                 
-                if(i==0) {
-                    mini=curr_idx;
+                if(i==0) first=curr_idx;
+                if(i==sz-1) last=curr_idx;
+                
+                if(curr->left){
+                    q.push({curr->left,2LL*curr_idx+1});
                 }
-                if(i==sz-1){
-                    maxi=curr_idx;
+                if(curr->right){
+                    q.push({curr->right,2LL*curr_idx+2});
                 }
-                    
-                if(curr->left)
-                    q.push({curr->left,2LL*curr_idx});
-                if(curr->right)
-                    q.push({curr->right,2LL*curr_idx+1});
             }
-            ans=max(ans,maxi-mini+1);
+            ans=max(ans,last-first+1);
         }
         return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+  
+//     int widthOfBinaryTree(TreeNode* root) {
+//         queue<pair<TreeNode*,int>>q;
+//         int ans=INT_MIN;
+//         q.push({root,1});
+//         while(!q.empty())
+//         {
+//             int sz=q.size(),maxi,mini;
+//             int min_idx=q.front().second;
+//             for(int i=0;i<sz;i++)
+//             {
+//                 int curr_idx=q.front().second-min_idx+1;
+                
+//                 TreeNode*curr=q.front().first;
+//                 q.pop();
+                
+//                 if(i==0) {
+//                     mini=curr_idx;
+//                 }
+//                 if(i==sz-1){
+//                     maxi=curr_idx;
+//                 }
+                    
+//                 if(curr->left)
+//                     q.push({curr->left,2LL*curr_idx});
+//                 if(curr->right)
+//                     q.push({curr->right,2LL*curr_idx+1});
+//             }
+//             ans=max(ans,maxi-mini+1);
+//         }
+//         return ans;
+//     }
+// };
