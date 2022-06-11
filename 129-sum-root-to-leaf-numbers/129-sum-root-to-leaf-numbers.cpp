8e126c874ffdecd3f457b835d1350303ea132157
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-11 13:42:55 +0530

    Time: 3 ms (67.82%), Space: 9.2 MB (48.18%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -11,23 +11,47 @@
  */
 class Solution {
 public:
-    void solve(TreeNode*root,int tmp,int &ans)
+    int solve(TreeNode*root,int ans)
     {
-        if(!root) return ;
-        tmp=(tmp*10+root->val);
-        if(root->left==NULL&&root->right==NULL)
-        {
-            ans+=tmp;
-        }
-        
-        solve(root->left,tmp,ans);
-        solve(root->right,tmp,ans);
+        if(!root) return 0;
+        ans=ans*10+root->val;
+        if(!root->left&&!root->right)
+            return ans;
+        int lh=solve(root->left,ans);
+        int rh=solve(root->right,ans);
+        ans =lh+rh;
+        return ans;
+        // return solve(root->left,ans)+solve(root->right,ans);
     }
     int sumNumbers(TreeNode* root) {
         int ans=0;
         if(!root) return ans;
-        
-        solve(root,0,ans);
-        return ans;
+        return solve(root,ans);
+        // return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     void solve(TreeNode*root,int tmp,int &ans)
+//     {
+//         if(!root) return ;
+//         tmp=(tmp*10+root->val);
+//         if(root->left==NULL&&root->right==NULL)
+//         {
+//             ans+=tmp;
+//         }
+        
+//         solve(root->left,tmp,ans);
+//         solve(root->right,tmp,ans);
+//     }
+//     int sumNumbers(TreeNode* root) {
+//         int ans=0;
+//         if(!root) return ans;
+        
+//         solve(root,0,ans);
+//         return ans;
+//     }
+// };
