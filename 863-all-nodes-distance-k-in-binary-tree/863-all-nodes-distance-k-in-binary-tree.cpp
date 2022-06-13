dcaf692d1cd2069c2672082c233668e62652a662
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-13 16:21:57 +0530

    Time: 7 ms (74.21%), Space: 13 MB (44.91%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -7,74 +7,77 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
 class Solution {
 public:
-    void markparent(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&m)
-    {
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int sz=q.size();
-            for(int i=0;i<sz;i++)
-            {
-                TreeNode*curr=q.front(); q.pop();
-                if(curr->left)
-                {
-                    m[curr->left]=curr;
-                    q.push(curr->left);
-                }
-                if(curr->right)
-                {
-                    m[curr->right]=curr;
-                    q.push(curr->right);
-                }
-            }
-        }
-    }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        
-        unordered_map<TreeNode*,TreeNode*>m; // to keep trak of parents 
-        markparent(root,m);
-        unordered_map<TreeNode*,bool>vis;
-        int level=0;
-        queue<TreeNode*>q;
-        q.push(target);
-        vis[target]=true;
-        while(!q.empty())
-        {
-            int sz=q.size();
-            if(level==k)
-                break;
-            else 
-                level++;
-            for(int i=0;i<sz;i++)
-            {
-                TreeNode*curr=q.front();
-                q.pop();
-                if(curr->left&&!vis[curr->left])
-                {
-                    q.push(curr->left);
-                    vis[curr->left]=true;
-                }
-                if(curr->right&&!vis[curr->right])
-                {
-                    q.push(curr->right);
-                    vis[curr->right]=true;
-                }
-                if(m[curr]&&!vis[m[curr]])
-                {
-                    q.push(m[curr]);
-                    vis[m[curr]]=true;
-                }
-            }
-        }
-        vector<int>ans;
-        while(!q.empty())
-        {
-            TreeNode*curr=q.front();
-            q.pop();
-            ans.push_back(curr->val);
-        }
-        return ans ;
-    }
+	void markparent(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&m)
+	{
+		queue<TreeNode*>q;
+		q.push(root);
+		while(!q.empty()){
+			int sz=q.size();
+			for(int i=0;i<sz;i++)
+			{
+				TreeNode*curr=q.front(); q.pop();
+				if(curr->left)
+				{
+					m[curr->left]=curr;
+					q.push(curr->left);
+				}
+				if(curr->right)
+				{
+					m[curr->right]=curr;
+					q.push(curr->right);
+				}
+			}
+		}
+	}
+	vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+
+		unordered_map<TreeNode*,TreeNode*>m; // to keep track of parents 
+		markparent(root,m);
+		unordered_map<TreeNode*,bool>vis;
+		int level=0;
+		queue<TreeNode*>q;
+		q.push(target);
+		vis[target]=true;
+		while(!q.empty())
+		{
+			int sz=q.size();
+
+			if(level==k)  // level is distance from target node 
+				break;
+			else 
+				level++; // for each level increase the level
+
+			for(int i=0;i<sz;i++)
+			{
+				TreeNode*curr=q.front();
+				q.pop();
+				if(curr->left&&!vis[curr->left]) // if left exist and not visited 
+				{
+					q.push(curr->left);
+					vis[curr->left]=true;   // mark as visited 
+				}
+				if(curr->right&&!vis[curr->right]) // if right child exist and not visited 
+				{
+					q.push(curr->right);
+					vis[curr->right]=true;  // mark as visited 
+				}
+				if(m[curr]&&!vis[m[curr]]) // if parent of curr node exist and not visited 
+				{
+					q.push(m[curr]);
+					vis[m[curr]]=true;   // mark as visited 
+				}
+			}
+		}
+		vector<int>ans;
+		while(!q.empty())
+		{
+			TreeNode*curr=q.front();
+			q.pop();
+			ans.push_back(curr->val);
+		}
+		return ans;
+	}
 };
