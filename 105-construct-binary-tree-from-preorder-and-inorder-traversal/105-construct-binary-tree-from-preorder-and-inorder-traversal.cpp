a6e76e10439dd063e719db3c19d24b5f8f8f865e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-16 00:11:09 +0530

    Time: 86 ms (10.28%), Space: 25.8 MB (97.56%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,26 +9,93 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    int preInd=0;
+    int find(vector<int>&inorder,int element)
+    {
+        for(int i=0;i<inorder.size();i++)
+        {
+            if(inorder[i]==element)
+            return i;
+        }
+        return -1;
+    }
+     TreeNode* solve(vector<int>&preorder,vector<int>&inorder,int &preindex,int instart,
+                     int inend,int n)      {
+         if(instart>inend||preindex>n){
+             return NULL;
+         }
+         int curr=preorder[preindex++];
+         TreeNode*root=new TreeNode(curr);
+         int pos=find(inorder,curr);
+         root->left=solve(preorder,inorder,preindex,instart,pos-1,n);
+         root->right=solve(preorder,inorder,preindex,pos+1,inend,n);
+         return root;
+     }
     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-        return createTree(preorder,inorder,0,inorder.size() - 1);
+        
+        int preindex=0;
+        return solve(preorder,inorder,preindex,0,inorder.size()-1,preorder.size()-1);
     }
-    TreeNode* createTree(vector<int>& preorder, vector<int>& inorder,int start, int end){
-        if(start > end){
-            return NULL;
-        }
-        TreeNode* node=new TreeNode(preorder[preInd++]);
-        int pos;
-        for(int i=start;i<=end;i++){
-            if(inorder[i]==node->val){
-                pos=i;
-                break;
-            }
-        }
-        node->left =createTree(preorder, inorder,start,pos-1);
-        node->right =createTree(preorder, inorder, pos+1,end);
-        return node;
-    }
+    
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int preInd=0;
+//     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+//         return createTree(preorder,inorder,0,inorder.size() - 1);
+//     }
+//     TreeNode* createTree(vector<int>& preorder, vector<int>& inorder,int start, int end){
+//         if(start > end){
+//             return NULL;
+//         }
+//         TreeNode* node=new TreeNode(preorder[preInd++]);
+//         int pos;
+//         for(int i=start;i<=end;i++){
+//             if(inorder[i]==node->val){
+//                 pos=i;
+//                 break;
+//             }
+//         }
+//         node->left =createTree(preorder, inorder,start,pos-1);
+//         node->right =createTree(preorder, inorder, pos+1,end);
+//         return node;
+//     }
+// };
+
+
+
+
+
+// class Solution {
+// public:
+//     TreeNode* solve(vector<int>&inorder,int instart,int inend,
+//                     vector<int>&preorder,int prestart,int preend,map<int,int>inMap)
+//     {
+//         if(instart>inend||prestart>preend)
+//             return nullptr;
+        
+//         TreeNode*root=new TreeNode(preorder[prestart]);
+//         int pos=inMap[root->val]; // this position indicates root ki val inorder me kaha lie kr rhi h
+//         int numleft=pos-instart;
+//         root->left=solve(inorder,instart,pos-1,preorder,prestart+1,prestart+numleft,inMap);
+//         root->right=solve(inorder,pos+1,inend,preorder,prestart+numleft+1,preend,inMap);
+//         return root;
+//     }
+        
+//     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+//         map<int,int>inMap;
+//         for(int i=0;i<inorder.size();i++)
+//         {
+//             inMap[inorder[i]]=i;
+//         }
+//         return solve(inorder,0,inorder.size()-1,preorder,0,preorder.size()-1,inMap);
+//     }
+    
+// };
