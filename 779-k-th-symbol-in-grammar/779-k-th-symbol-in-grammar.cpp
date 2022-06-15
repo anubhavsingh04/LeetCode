74c4313521ad5eb31be6f0d4d80060cce3571496
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-15 22:54:37 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.52%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
@@ -1,3 +1,10 @@
+// Recursion (basically hame 0 ka 0 1 and 1 ka 1 0 banana hai)
+//              0 
+//              0 1 |
+//              0 1 | 1 0 
+// if we carefully observe first half of each next iteration is same as previous
+// and second half is complemented corresponding to first half of prev   
+                  
 class Solution {
 public:
     int kthGrammar(int n, int k) {
