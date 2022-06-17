9760f6f0c227046e8e498d5c9e2c8f5792aa2bd7
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-18 01:44:27 +0530

    Time: 9 ms (56.80%), Space: 12.7 MB (39.49%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -9,23 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// using stack TC: O(n) SC: O(n)
+
+// class Solution {
+// public:
+//     void flatten(TreeNode* root) {
+//         stack<TreeNode*>st;
+//         if(!root) return;
+//         st.push(root);
+//         while(!st.empty())
+//         {
+//             TreeNode*curr=st.top();
+//             st.pop();
+//             if(curr->right)
+//                 st.push(curr->right);
+//             if(curr->left)
+//                 st.push(curr->left);
+//             if(!st.empty())
+//             curr->right=st.top();
+//             curr->left=NULL;
+//         }
+//     }
+// };
+
+// using morris traversal TC: O(n) space: O(1)
+
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        stack<TreeNode*>st;
-        if(!root) return;
-        st.push(root);
-        while(!st.empty())
+        TreeNode*curr=root;
+        while(curr!=NULL)
         {
-            TreeNode*curr=st.top();
-            st.pop();
-            if(curr->right)
-                st.push(curr->right);
             if(curr->left)
-                st.push(curr->left);
-            if(!st.empty())
-            curr->right=st.top();
-            curr->left=NULL;
+            {
+                TreeNode*prev=curr->left;
+                while(prev->right)
+                    prev=prev->right;
+                prev->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            curr=curr->right;
         }
     }
 };
