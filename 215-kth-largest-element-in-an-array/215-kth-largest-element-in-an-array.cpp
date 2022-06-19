627cc8c50635e41a52a972311e43a3377edc6cda
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-19 12:02:49 +0530

    Time: 14 ms (44.98%), Space: 10.1 MB (61.70%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,10 @@
+// Kth largest element is (n-k)th smallest 
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return nums[n-k];
+    }
+};
