ac666fe44cc911fe2b860d1018a14e861211fd98
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-19 13:12:09 +0530

    Time: 24 ms (63.77%), Space: 25.1 MB (76.76%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -9,46 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     int ans=INT_MAX; 
-//     TreeNode*prev=NULL;
-//     void inorder(TreeNode*root)
-//     {
-//         if(!root) return;
-//         inorder(root->left);
-//         if(prev)
-//         {
-//             ans=min(ans,abs(prev->val-root->val));
-//         }
-//         prev=root;
-//         inorder(root->right);
-//     }
-//     int getMinimumDifference(TreeNode* root) {
-//         inorder(root);
-//         return ans;
-//     }
-// };
 
-
-
+// TC: O(n) SC: O(h) if we dont consider stack space then SC: O(1)
 class Solution {
 public:
-    vector<int>v;
+    int ans=INT_MAX; 
+    TreeNode*prev=NULL;
     void inorder(TreeNode*root)
     {
         if(!root) return;
         inorder(root->left);
-        v.push_back(root->val);
+        if(prev)
+        {
+            ans=min(ans,abs(prev->val-root->val));
+        }
+        prev=root;
         inorder(root->right);
     }
     int getMinimumDifference(TreeNode* root) {
         inorder(root);
-        int ans=INT_MAX;
-        for(int i=1;i<v.size();i++)
-        {
-            ans=min(ans,abs(v[i]-v[i-1]));
-        }
         return ans;
     }
 };
+
+
+// TC: O(n) SC: O(n)
+// class Solution {
+// public:
+//     vector<int>v;
+//     void inorder(TreeNode*root)
+//     {
+//         if(!root) return;
+//         inorder(root->left);
+//         v.push_back(root->val);
+//         inorder(root->right);
+//     }
+//     int getMinimumDifference(TreeNode* root) {
+//         inorder(root);
+//         int ans=INT_MAX;
+//         for(int i=1;i<v.size();i++)
+//         {
+//             ans=min(ans,abs(v[i]-v[i-1]));
+//         }
+//         return ans;
+//     }
+// };
