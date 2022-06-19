88c969bb59606c5495cf477775a741a7ac7c1dce
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-19 13:10:53 +0530

    Time: 27 ms (53.66%), Space: 25.6 MB (20.69%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -9,23 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int ans=INT_MAX; 
+//     TreeNode*prev=NULL;
+//     void inorder(TreeNode*root)
+//     {
+//         if(!root) return;
+//         inorder(root->left);
+//         if(prev)
+//         {
+//             ans=min(ans,abs(prev->val-root->val));
+//         }
+//         prev=root;
+//         inorder(root->right);
+//     }
+//     int getMinimumDifference(TreeNode* root) {
+//         inorder(root);
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int ans=INT_MAX; 
-    TreeNode*prev=NULL;
+    vector<int>v;
     void inorder(TreeNode*root)
     {
         if(!root) return;
         inorder(root->left);
-        if(prev)
-        {
-            ans=min(ans,abs(prev->val-root->val));
-        }
-        prev=root;
+        v.push_back(root->val);
         inorder(root->right);
     }
     int getMinimumDifference(TreeNode* root) {
         inorder(root);
+        int ans=INT_MAX;
+        for(int i=1;i<v.size();i++)
+        {
+            ans=min(ans,abs(v[i]-v[i-1]));
+        }
         return ans;
     }
 };
