d4d6fcb8b7973513892ed90d33b75fc1482deb3e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-20 21:33:35 +0530

    Time: 27 ms (15.70%), Space: 21.7 MB (70.53%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,28 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// bst valid tab hoga jab uska inorder travresal sorted array dega 
-
+// valid bst will 
 class Solution {
 public:
-   void inorder(TreeNode*root,vector<int>&ans)
-   {
-       if(!root) return;
-       inorder(root->left,ans);
-       ans.push_back(root->val);
-       inorder(root->right,ans);
-   }
+   
     bool isValidBST(TreeNode* root) {
-        vector<int>v;
-        inorder(root,v);
-        for(int i=1;i<v.size();i++)
-        {
-            if(v[i]<=v[i-1]) // agar vector me agla element chota hai means sorted order me nhi h elemen
-                return false;
-        }
-        return true;
+            return isvalid(root,LLONG_MIN,LLONG_MAX);
+    }
+    bool isvalid(TreeNode*root,long long int mini,long long int maxi)
+    {
+        if(root==NULL)
+            return true;
+        if(root->val>=maxi|| root->val<=mini) 
+            return false;
+        return isvalid(root->left,mini,root->val)&&isvalid(root->right,root->val,maxi);
     }
 };
+
+
+
+// bst valid tab hoga jab uska inorder travresal sorted array dega TC: O(n) SC: O(n)
+// class Solution {
+// public:
+//    void inorder(TreeNode*root,vector<int>&ans)
+//    {
+//        if(!root) return;
+//        inorder(root->left,ans);
+//        ans.push_back(root->val);
+//        inorder(root->right,ans);
+//    }
+//     bool isValidBST(TreeNode* root) {
+//         vector<int>v;
+//         inorder(root,v);
+//         for(int i=1;i<v.size();i++)
+//         {
+//             if(v[i]<=v[i-1]) // if not in order  
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
 
 
 
