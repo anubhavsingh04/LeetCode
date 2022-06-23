a741645f9a515e56cbd49325f2caa12753892901
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 18:05:18 +0530

    Time: 14 ms (68.25%), Space: 12.6 MB (8.64%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,14 +1,42 @@
+
+// Brute force 
+// class Solution {
+// public:
+//     int countPairs(vector<int>& nums, int k) {
+//         int cnt=0;
+//         for(int i=0;i<nums.size()-1;i++)
+//         {
+//             for(int j=i+1;j<nums.size();j++)
+//             {
+//                 if(nums[i]==nums[j] && (i*j)%k==0)
+//                     cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+// TC: O(n^2) Sc: O(m)
+
+
+// Hashing force 
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
         int cnt=0;
-        for(int i=0;i<nums.size()-1;i++)
+        unordered_map<int,vector<int>>m;
+        for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
+            if(m.find(nums[i])!=m.end())
             {
-                if(nums[i]==nums[j] && (i*j)%k==0)
-                    cnt++;
+                for(auto &a:m[nums[i]])
+                {
+                    if((i*a)%k==0)
+                        cnt++;
+                }
             }
+            m[nums[i]].push_back(i);
         }
         return cnt;
     }
