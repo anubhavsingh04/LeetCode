bfa4c05915822b3da48def7f61f163ddae2a3b9d
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 19:13:08 +0530

    Time: 7 ms (67.20%), Space: 11.7 MB (15.32%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,12 +1,36 @@
+// class Solution {
+// public:
+//     vector<int> targetIndices(vector<int>& nums, int target) {
+//         sort(nums.begin(),nums.end());
+//         vector<int>ans;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]==target)
+//                 ans.push_back(i);
+//         }
+//         return ans;
+//     }
+// };
+
+
+// count kr lo kitne element target se chote hai 
+// and count the freq of target element
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
         vector<int>ans;
+        int k=0;
+        int small=0,freq_of_target=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]==target)
-                ans.push_back(i);
+           if(nums[i]<target)
+               small++;
+            else if(nums[i]==target)
+                freq_of_target++;
+        }
+        for(int i=small;i<small+freq_of_target;i++)
+        {
+            ans.push_back(i);
         }
         return ans;
     }
