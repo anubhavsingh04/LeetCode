a1bd2bd7b0a321b67f5fb9db677b83c2422936dc
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 23:56:29 +0530

    Time: 47 ms (31.69%), Space: 27 MB (54.05%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -7,7 +7,7 @@ public:
             if(nums[i]<nums[i-1])
             {
                 cnt++;
-                if(cnt>1||(i>1 && i<nums.size()-1 && nums[i-2]>nums[i] && nums[i-1]>nums[i+1]))
+                if(cnt>1 ||(i>1 && i<nums.size()-1 && nums[i-2]>nums[i] && nums[i-1]>nums[i+1]))
                     return false;
             }
         }
