26104c16e9da768f855098facc0279034fc1b3ee
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 20:07:25 +0530

    Time: 3 ms (49.48%), Space: 6.8 MB (8.97%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -1,19 +1,37 @@
+// floor(x/2) val gives less or equal to x
 class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int>ans;
-        if(n&1)
+        if(n&1) ans.push_back(0);
+        for(int i=1;i<=floor(n/2);i++)
         {
-            ans.push_back(0);
-            n--;
-        }
-        
-        while(n)
-        {
-            ans.push_back(n);
-            ans.push_back(-1*n);
-            n-=2;
+            ans.push_back(i);
+            ans.push_back(-i);
         }
         return ans;
     }
 };
+
+
+
+// if n is odd then push 0 now n becomes even start pushing n ,-n 
+// class Solution {
+// public:
+//     vector<int> sumZero(int n) {
+//         vector<int>ans;
+//         if(n&1)
+//         {
+//             ans.push_back(0);
+//             n--;
+//         }
+        
+//         while(n)
+//         {
+//             ans.push_back(n);
+//             ans.push_back(-1*n);
+//             n-=2;
+//         }
+//         return ans;
+//     }
+// };
