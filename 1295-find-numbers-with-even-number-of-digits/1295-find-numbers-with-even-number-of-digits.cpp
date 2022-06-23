523805f2476a4e38812a8a68a1ed756679b069a7
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 19:45:58 +0530

    Time: 7 ms (73.13%), Space: 10.1 MB (8.25%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -1,11 +1,29 @@
+// even no ke lsb pe zero hota hai aur 1 ke lsb pe 1 hota h
+
+// class Solution {
+// public:
+//     int findNumbers(vector<int>& nums) {
+//         int ans=0;
+//         for(auto &i:nums)
+//         {
+//             string s=to_string(i);
+//             if((s.size()&1)==0) 
+//                 ans++;
+//         }
+//         return ans;
+//     }
+// };
+
+
+// if we take log of any no on base 10 it will give no of digit in that no -1
+// means agr log ans odd de rha hai then no of digits even rha hoga us no me 
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int ans=0;
         for(auto &i:nums)
         {
-            string s=to_string(i);
-            if((s.size()&1)==0)
+            if((int)log10(i)&1)
                 ans++;
         }
         return ans;
