f06fe83f5cdf98a7f54ab60bd37e8391db64249f
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 16:59:33 +0530

    Time: 5 ms (26.80%), Space: 6.4 MB (58.21%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     string truncateSentence(string s, int k) {
+//         string ans;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(s[i]==' ')
+//             {
+//                 k--;
+//                 if(k==0) 
+//                     break;
+//             }
+//             ans+=s[i];
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-        string ans;
         for(int i=0;i<s.size();i++)
         {
-            if(s[i]==' ')
-            {
-                k--;
-                if(k==0) 
-                    break;
-            }
-            ans+=s[i];
+            if(s[i]==' ' && --k==0)
+                return s.substr(0,i);
         }
-        return ans;
+        return s;
     }
 };
