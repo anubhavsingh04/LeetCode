d491ef7b4443e653e5d613be59afa05034c71df0
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 20:25:39 +0530

    Time: 30 ms (23.50%), Space: 12.9 MB (97.37%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -1,15 +1,33 @@
+// agr kisi element ki freq odd hai then pair nahi ban skta
+// class Solution {
+// public:
+//     bool divideArray(vector<int>& nums) {
+//         unordered_map<int,int>m;
+//         for(auto &i:nums)
+//         {
+//             m[i]++;
+//         }
+//         for(auto &i:nums)
+//         {
+//             if(m[i]&1)
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int,int>m;
-        for(auto &i:nums)
+        sort(nums.begin(),nums.end());
+        
+        for(int i=1;i<nums.size();)
         {
-            m[i]++;
-        }
-        for(auto &i:nums)
-        {
-            if(m[i]&1)
+            if(nums[i]!=nums[i-1])
                 return false;
+            i+=2;
         }
         return true;
     }
