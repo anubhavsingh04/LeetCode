3d73ceb35bca6bb4476370f0f610bd045820449f
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 19:34:00 +0530

    Time: 23 ms (19.46%), Space: 9.9 MB (90.85%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,13 +1,25 @@
+// class Solution {
+// public:
+//     int prefixCount(vector<string>& words, string pref) {
+//         int cnt=0,k=0;
+//         for(auto &word: words)
+//         {
+//             int n=pref.size();
+//             if(word.substr(0,n)==pref)
+//             cnt++;
+//         }
+//         return cnt;
+//     }
+// };
+
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int cnt=0,k=0;
-        for(auto &word: words)
-        {
-            int n=pref.size();
-            if(word.substr(0,n)==pref)
-            cnt++;
-        }
-        return cnt;
+        int ans=0;
+        for(auto & word:words)
+            if(word.find(pref)==0) // agr pref 0 index pe exist kr rhi h
+                ans++;
+        return ans;
     }
 };
