c7be1aaa0029678005607df7a2535b9031f9faa4
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 19:52:12 +0530

    Time: 8 ms (59.48%), Space: 9.8 MB (84.20%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -17,14 +17,35 @@
 
 // if we take log of any no on base 10 it will give no of digit in that no -1
 // means agr log ans odd de rha hai then no of digits even rha hoga us no me 
+// class Solution {
+// public:
+//     int findNumbers(vector<int>& nums) {
+//         int ans=0;
+//         for(auto &i:nums)
+//         {
+//             if((int)log10(i)&1)
+//                 ans++;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+// we can also use if else to check
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int ans=0;
         for(auto &i:nums)
         {
-            if((int)log10(i)&1)
+            if(i>9 && i<1e2)
                 ans++;
+            else if(i>999 && i<1e4)
+                ans++;
+            else if(i>99999 && i<1e6)
+                ans++;   
         }
         return ans;
     }
