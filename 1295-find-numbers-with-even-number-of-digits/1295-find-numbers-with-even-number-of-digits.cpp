c241eff737db6b756c902069d01128b4a992b390
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-23 19:52:48 +0530

    Time: 28 ms (5.47%), Space: 9.9 MB (46.15%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -1,18 +1,18 @@
 // even no ke lsb pe zero hota hai aur 1 ke lsb pe 1 hota h
 
-// class Solution {
-// public:
-//     int findNumbers(vector<int>& nums) {
-//         int ans=0;
-//         for(auto &i:nums)
-//         {
-//             string s=to_string(i);
-//             if((s.size()&1)==0) 
-//                 ans++;
-//         }
-//         return ans;
-//     }
-// };
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int ans=0;
+        for(auto &i:nums)
+        {
+            string s=to_string(i);
+            if((s.size()&1)==0) 
+                ans++;
+        }
+        return ans;
+    }
+};
 
 
 // if we take log of any no on base 10 it will give no of digit in that no -1
@@ -34,19 +34,19 @@
 
 
 // we can also use if else to check
-class Solution {
-public:
-    int findNumbers(vector<int>& nums) {
-        int ans=0;
-        for(auto &i:nums)
-        {
-            if(i>9 && i<1e2)
-                ans++;
-            else if(i>999 && i<1e4)
-                ans++;
-            else if(i>99999 && i<1e6)
-                ans++;   
-        }
-        return ans;
-    }
-};
+// class Solution {
+// public:
+//     int findNumbers(vector<int>& nums) {
+//         int ans=0;
+//         for(auto &i:nums)
+//         {
+//             if(i>9 && i<1e2)
+//                 ans++;
+//             else if(i>999 && i<1e4)
+//                 ans++;
+//             else if(i>99999 && i<1e6)
+//                 ans++;   
+//         }
+//         return ans;
+//     }
+// };
