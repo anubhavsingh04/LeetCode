4e8554eceb398edabe3eaf37fa28508e05ce2c77
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-24 14:47:08 +0530

    Time: 3 ms (92.90%), Space: 9.9 MB (83.06%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,37 +2,61 @@
 // first find out the array for next smaller on the right 
 // then subtract price[i]-v[i]
 
+// class Solution {
+// public:
+//     vector<int> finalPrices(vector<int>& prices) {
+//         vector<int>v;
+//         stack<int>st;
+//         for(int i=prices.size()-1;i>=0;i--)
+//         {
+//             if(st.empty())
+//                 v.push_back(-1);
+//             else if(st.top()<prices[i])
+//                 v.push_back(st.top());
+//             else if(st.size()>0 && st.top()>=prices[i])
+//             {
+//                 while(st.size()>0 && st.top()>prices[i]) // yaha pe equal allowed hai 
+//                     st.pop();
+//                 if(st.empty())
+//                     v.push_back(-1);
+//                 else 
+//                     v.push_back(st.top());
+//             }
+//             st.push(prices[i]);
+//         }
+//         reverse(v.begin(),v.end());
+//         for(int i=0;i<prices.size();i++)
+//         {
+//             if(v[i]!=-1)
+//                 prices[i]-=v[i];
+//         }
+//         return prices;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int>v;
+        int n=prices.size();
+        vector<int>v(n);
         stack<int>st;
-        for(int i=prices.size()-1;i>=0;i--)
+        // v[n-1]=prices[n-1];
+        for(int i=n-1;i>=0;i--)
         {
+            while(!st.empty()&&st.top()>prices[i])
+            st.pop();
             if(st.empty())
-                v.push_back(-1);
-            else if(st.top()<prices[i])
-                v.push_back(st.top());
-            else if(st.size()>0 && st.top()>=prices[i])
-            {
-                while(st.size()>0 && st.top()>prices[i])
-                    st.pop();
-                if(st.empty())
-                    v.push_back(-1);
-                else 
-                    v.push_back(st.top());
-            }
+                v[i]=prices[i];
+            else 
+                v[i]=prices[i]-st.top();
             st.push(prices[i]);
         }
-        reverse(v.begin(),v.end());
-        for(int i=0;i<prices.size();i++)
-        {
-            if(v[i]!=-1)
-                prices[i]-=v[i];
-        }
-        return prices;
+        return v;
     }
 };
+
+
 
 
 // next smaller to left
