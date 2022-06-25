7832f81bceb0946a0e6958e0f5d58d7757ba8d8b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-25 13:56:22 +0530

    Time: 4 ms (70.31%), Space: 11 MB (74.78%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,41 @@
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-//         int s=0,e=nums.size()-1;
-//         while(s<=e)
-//         {
-//             int mid=(s+e)/2;
-//             if(nums[mid]==target)
-//                 return mid;
-//             if(nums[s]<=nums[mid])
-//             {
-//                 if(target<=nums[mid]&&target>=nums[s])
-//                     e=mid-1;
-//                 else
-//                     s=mid+1;
-//             }
-//             else
-//             {
-//                 if(target>=nums[mid]&&target<=nums[e])
-//                     s=mid+1;
-//                 else
-//                     e=mid-1;
-//             }
-//         }
-//         return -1;
-//     }
-// };
-
-
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++)
+        int s=0,e=nums.size()-1;
+        while(s<=e)
         {
-            if(nums[i]==target)
-                return i;
+            int mid=(s+e)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[s]<=nums[mid])
+            {
+                if(target<=nums[mid]&&target>=nums[s])
+                    e=mid-1;
+                else
+                    s=mid+1;
+            }
+            else
+            {
+                if(target>=nums[mid]&&target<=nums[e])
+                    s=mid+1;
+                else
+                    e=mid-1;
+            }
         }
         return -1;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]==target)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// };
