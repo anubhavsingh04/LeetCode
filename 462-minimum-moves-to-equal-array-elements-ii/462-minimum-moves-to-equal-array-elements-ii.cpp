b35123a31e305eb1c02bfe2177a59794fa160316
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-30 12:32:27 +0530

    Time: 15 ms (63.23%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,22 +1,26 @@
-
 // more optimised approach O(n)
+// nth_element(nums.begin(),nums.begin()+n/2,num.end()) this function will place the median element at 
+// its correct position 
 
-// class Solution {
-// public:
-//     int minMoves2(vector<int>& nums) {
-//         int n=nums.size();
+// basically it rearranges the list in such a way such that the element at the nth position is the one which should be at that position if we sort the list.
+// means nth element ko uski sahi position pr rkh dega using quick select method 
+
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n=nums.size();
         
-//         sort(nums.begin(),nums.end());
-//         int median=nums[n/2];
+        nth_element(nums.begin(),nums.begin()+n/2,nums.end());
+        int median=nums[n/2];
         
-//         int ans=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             ans+=abs(nums[i]-median);
-//         }
-//         return ans;
-//     }
-// };
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=abs(nums[i]-median);
+        }
+        return ans;
+    }
+};
 
 
 
@@ -43,21 +47,19 @@
 
  // Now for any value x in the range the total steps needed to reach x for both the numbers is (x - nums[i]) + (nums[n-1-i] - x) = nums[n-1-i] - nums[i]
 
-
-
-class Solution {
-public:
-    int minMoves2(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int ans=0;
-        for(int i=0;i<n/2;i++)
-        {
-            ans+=nums[n-i-1]-nums[i];  
-        }
-        return ans;
-    }
-};
+// class Solution {
+// public:
+//     int minMoves2(vector<int>& nums) {
+//         int n=nums.size();
+//         sort(nums.begin(),nums.end());
+//         int ans=0;
+//         for(int i=0;i<n/2;i++)
+//         {
+//             ans+=nums[n-i-1]-nums[i];  
+//         }
+//         return ans;
+//     }
+// };
 
 
 
