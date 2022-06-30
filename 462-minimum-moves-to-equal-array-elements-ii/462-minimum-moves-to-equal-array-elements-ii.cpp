18bce84c8cbe14d5fcf1aea852b084df3d4c1789
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-06-30 12:24:32 +0530

    Time: 13 ms (75.41%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,21 +1,63 @@
 
+// more optimised approach O(n)
+
+// class Solution {
+// public:
+//     int minMoves2(vector<int>& nums) {
+//         int n=nums.size();
+        
+//         sort(nums.begin(),nums.end());
+//         int median=nums[n/2];
+        
+//         int ans=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             ans+=abs(nums[i]-median);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+// O(n)+O(nlog(n))
+
+// class Solution {
+// public:
+//     int minMoves2(vector<int>& nums) {
+//         int n=nums.size();
+        
+//         sort(nums.begin(),nums.end());
+//         int median=nums[n/2];
+        
+//         int ans=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             ans+=abs(nums[i]-median);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+ // Now for any value x in the range the total steps needed to reach x for both the numbers is (x - nums[i]) + (nums[n-1-i] - x) = nums[n-1-i] - nums[i]
+
+
+
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         int n=nums.size();
-        
         sort(nums.begin(),nums.end());
-        int median=nums[n/2];
-        
         int ans=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n/2;i++)
         {
-            ans+=abs(nums[i]-median);
+            ans+=nums[n-i-1]-nums[i];  
         }
         return ans;
     }
 };
-
 
 
 
