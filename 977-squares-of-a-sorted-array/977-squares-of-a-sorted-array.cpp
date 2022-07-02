043197c0278b9c06e5e21c244b7f3a801a7d9ccb
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-02 23:18:03 +0530

    Time: 64 ms (26.61%), Space: 25.8 MB (94.67%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,11 +1,30 @@
+// Just iterate from right and put max of abs(nums[right]), abs(nums[left])
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++)
+        vector<int>ans(nums.size());
+        int left=0,right=nums.size()-1;
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            nums[i]=nums[i]*nums[i];
+            if(abs(nums[right])>abs(nums[left]))  
+                ans[i]=nums[right]*nums[right--];
+            else 
+                ans[i]=nums[left]*nums[left++];
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+        return ans;
     }
 };
+
+// TC: O(n) + o(nlog(n))=O(nlog(n))
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             nums[i]=nums[i]*nums[i];
+//         }
+//         sort(nums.begin(),nums.end());
+//         return nums;
+//     }
+// };
