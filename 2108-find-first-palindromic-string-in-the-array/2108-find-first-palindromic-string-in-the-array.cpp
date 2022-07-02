d7482672d4fec5e2b802051fa74d7884f0a30ab2
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-02 23:06:08 +0530

    Time: 73 ms (64.03%), Space: 22.9 MB (57.23%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -5,10 +5,9 @@ public:
         int i=0,j=s.size()-1;
         while(i<j)
         {
-            if(s[i]==s[j])
-                i++,j--;
-            else 
+            if(s[i]!=s[j])
                 return false;
+                i++,j--;
         }
         return true;
     }
