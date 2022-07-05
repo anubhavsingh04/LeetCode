a3f87b80c35b443e835eb3a62f91600ea81cca9e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-05 11:53:22 +0530

    Time: 1124 ms (11.23%), Space: 45.7 MB (66.36%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int longest=0;
+        if(nums.size()==0)
+            return 0;
         unordered_set<int>s(nums.begin(),nums.end());
-        
-        for(auto &num: nums)
+        int longest=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(s.count(num-1))
-                continue;
-            int j=1;
-            while(s.count(num+j))
-                j++;
-            
-            longest=max(longest,j);
+            if(s.find(nums[i]-1)==s.end())
+            {
+                int len=1,curr=nums[i];
+                while(s.find(curr+1)!=s.end())
+                {
+                    curr++;
+                    len++;
+                }
+                longest=max(longest,len);
+            }
         }
         return longest;
     }
@@ -21,6 +25,29 @@ public:
 
 
 
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         int longest=0;
+//         unordered_set<int>s(nums.begin(),nums.end());
+        
+//         for(auto &num: nums)
+//         {
+//             if(s.count(num-1))
+//                 continue;
+//             int j=1;
+//             while(s.count(num+j))
+//                 j++;
+            
+//             longest=max(longest,j);
+//         }
+//         return longest;
+//     }
+// };
+
+
+
+// EASY
 // class Solution {
 // public:
 //     int longestConsecutive(vector<int>& nums) {
