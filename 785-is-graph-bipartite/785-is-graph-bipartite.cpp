fb85aa02dff721a9c6bd047b37e9cf013bbd6665
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-11 21:08:20 +0530

    Time: 54 ms (20.84%), Space: 13.5 MB (83.47%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,24 +1,61 @@
+// class Solution {
+// public:
+//     bool bfs(int src,vector<vector<int>>&graph, vector<int>&color)
+//     {
+//         color[src]=1;
+//         queue<int>q;
+//         q.push(src);
+//         while(!q.empty())
+//         {
+//             int curr=q.front();
+//             q.pop();
+//             for(auto &i:graph[curr])
+//             {
+//                 if(color[i]==-1)
+//                 {
+//                     color[i]=1-color[curr];
+//                     q.push(i);
+//                 }
+//                 else if(color[i]==color[curr])
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+    
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int n=graph.size();
+//         vector<int>color(n,-1);
+//         for(int i=0;i<n;i++)
+//         {
+//             if(color[i]==-1)
+//             {
+//                 if(!bfs(i,graph,color))
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+// Using dfs
+
 class Solution {
 public:
-    bool bfs(int src,vector<vector<int>>&graph, vector<int>&color)
+    bool dfs(int src,vector<vector<int>>&graph, vector<int>&color)
     {
-        color[src]=1;
-        queue<int>q;
-        q.push(src);
-        while(!q.empty())
+        if(color[src]==-1)
+            color[src]=1;
+        for(auto &i:graph[src])
         {
-            int curr=q.front();
-            q.pop();
-            for(auto &i:graph[curr])
+            if(color[i]==-1)
             {
-                if(color[i]==-1)
-                {
-                    color[i]=1-color[curr];
-                    q.push(i);
-                }
-                else if(color[i]==color[curr])
+                color[i]=1-color[src];
+                if(!dfs(i,graph,color))
                     return false;
             }
+            else if(color[i]==color[src])
+                return false;
         }
         return true;
     }
@@ -30,7 +67,7 @@ public:
         {
             if(color[i]==-1)
             {
-                if(!bfs(i,graph,color))
+                if(!dfs(i,graph,color))
                     return false;
             }
         }
