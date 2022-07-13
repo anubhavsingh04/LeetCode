2cb03d5348594fc165918a5ff92a8ff3e25fdd6c
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-13 23:33:49 +0530

    Time: 22 ms (8.93%), Space: 10.9 MB (10.19%) - LeetHub

--- 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<int> minSubsequence(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        vector<int>ans;
-        int sum=0;
-        int s2=accumulate(nums.begin(),nums.end(),0)/2;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(sum>s2) break;
-            sum+=nums[i];
-            ans.push_back(nums[i]);
+        vector<int> res;
+        auto sub_sum = 0, half_sum = accumulate(begin(nums), end(nums), 0) / 2;
+        priority_queue<int> pq(begin(nums), end(nums));
+        while (sub_sum <= half_sum) {
+            res.push_back(pq.top());
+            sub_sum += res.back();
+            pq.pop();
         }
-        return ans;
+        return res;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> minSubsequence(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int n=nums.size();
+//         vector<int>ans;
+//         int sum=0;
+//         int s2=accumulate(nums.begin(),nums.end(),0)/2;
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             if(sum>s2) break;
+//             sum+=nums[i];
+//             ans.push_back(nums[i]);
+//         }
+//         return ans;
+//     }
+// };
