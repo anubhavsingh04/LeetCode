1f6632a06ca2dae31d3c5e945cca79850b702e45
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-15 14:10:22 +0530

    Time: 11 ms (89.78%), Space: 19.7 MB (36.89%) - LeetHub

--- 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.cpp
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.cpp
@@ -9,31 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    string s;
-    void help(TreeNode* root ,vector<string>&v)
+    string ans="";
+    void help(TreeNode* root ,string s)
     {
         
-    if(!root)
-        return;
-        s.push_back(root->val+97);
+    if(!root) return;
+        s=(char)(root->val+97)+s;
         if(root->left==NULL and root->right==NULL )
         {
-            string tmp=s;
-            reverse(tmp.begin(),tmp.end());
-            v.push_back(tmp);
+            if(ans.empty()) 
+                ans=s;
+            else 
+                ans=min(ans,s);
         }
         
-        help(root->left,v);
-        help(root->right,v);
+        help(root->left,s);
+        help(root->right,s);
         s.pop_back();
     }
     string smallestFromLeaf(TreeNode* root) {
-        vector<string>v;
-        help(root,v);
-        sort(v.begin(),v.end());
-        return v[0];
-      
+        help(root,"");
+        return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     string s;
+//     void help(TreeNode* root ,vector<string>&v)
+//     {
+        
+//     if(!root)
+//         return;
+//         s.push_back(root->val+97);
+//         if(root->left==NULL and root->right==NULL )
+//         {
+//             string tmp=s;
+//             reverse(tmp.begin(),tmp.end());
+//             v.push_back(tmp);
+//         }
+        
+//         help(root->left,v);
+//         help(root->right,v);
+//         s.pop_back();
+//     }
+//     string smallestFromLeaf(TreeNode* root) {
+//         vector<string>v;
+//         help(root,v);
+//         sort(v.begin(),v.end());
+//         return v[0];
+      
+//     }
+// };
