cdb9dbf8573f107890f3dc76e1424d088aab9a9a
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-18 23:20:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        long num = abs(n);
-        
-        double pow = 1;
-        
-        while(num){ // equivalent to while(num != 0)
-            if(num & 1) { // equivalent to if((num & 1) != 0)
-                pow *= x;
-                num--;
-            }
-            x *= x;
-            num >>= 1;
+        if(n<0)
+        {
+            x=1/x;
+            n=abs(n);
         }
-        
-        return pow;
+        double ans=1;
+        while(n>0)
+        {
+            if(n&1)
+            {
+                ans*=x;
+                n--;
+            }
+            else
+            {
+                x*=x;
+                n/=2;
+            }
+        }
+        return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+        
+//         if(n < 0) {
+//             x = 1 / x;
+//         } 
+        
+//         long num = abs(n);
+        
+//         double pow = 1;
+        
+//         while(num){ // equivalent to while(num != 0)
+//             if(num & 1) { // equivalent to if((num & 1) != 0)
+//                 pow *= x;
+//                 num--;
+//             }
+//             x *= x;
+//             num >>= 1;
+//         }
+        
+//         return pow;
+//     }
+// };
