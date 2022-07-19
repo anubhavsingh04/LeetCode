97baa5ea1d4c27a7a0b121493f91ea91752dcf38
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-19 11:53:52 +0530

    Time: 281 ms (41.79%), Space: 82.9 MB (77.72%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -0,0 +1,20 @@
+// Put each element at its right position like 5 will be at positon 4 
+// if nums[i]==5 then it should be at position nums[nums[i]-1]
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i])
+                swap(nums[nums[i]-1],nums[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        return n+1;
+    }
+};
