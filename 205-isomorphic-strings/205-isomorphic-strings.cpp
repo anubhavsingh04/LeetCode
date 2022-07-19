401d05e2c83d4b48ae3ed2fb9e89b3b6fcf40741
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-19 13:51:10 +0530

    Time: 5 ms (85.75%), Space: 7.1 MB (46.04%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int>a(256,-1),b(256,-1); // 256 ascii character
+        for(int i=0;i<s.size();i++)
+        {
+            if(a[s[i]]!=b[t[i]]) return false;
+            a[s[i]]=i+1;
+            b[t[i]]=i+1;
+        }
+        return true;
+    }
+};
