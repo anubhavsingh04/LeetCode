5fd05f2affdfdbdec69fe99ce70cfefd7ca9ca92
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-20 13:44:52 +0530

    Time: 156 ms (71.64%), Space: 61.2 MB (91.01%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,3 +1,27 @@
+// Most optimal
+// turtoise is slow 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0],fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        
+        slow=nums[0];
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+};
+
+
+
+
 // class Solution {
 // public:
 //     int findDuplicate(vector<int>& nums) {
@@ -13,18 +37,18 @@
 
 
 
-class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        unordered_set<int>s;
-        for(auto &i:nums)
-        {
-            if(s.find(i)!=s.end()) return i;
-            s.insert(i);
-        }
-        return -1;
-    }
-};
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         unordered_set<int>s;
+//         for(auto &i:nums)
+//         {
+//             if(s.find(i)!=s.end()) return i;
+//             s.insert(i);
+//         }
+//         return -1;
+//     }
+// };
 
 
 
