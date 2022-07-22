ed461d2eeace4423e93c390d44440a66c37cdf88
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-22 12:57:03 +0530

    Time: 13 ms (23.02%), Space: 10.9 MB (11.89%) - LeetHub

---
+++ 86-partition-list/86-partition-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        queue<ListNode*>qsmall,qlarge;
+        ListNode*tmp=head;
+        while(tmp)
+        {
+            if(tmp->val>=x)
+            {
+                qlarge.push(tmp);
+            }
+            else 
+            {
+                qsmall.push(tmp);
+            }
+            tmp=tmp->next;
+        }
+        ListNode*dummy= new ListNode(0);
+        tmp=dummy;
+        while(!qsmall.empty())
+        {
+            auto curr=qsmall.front();
+            qsmall.pop();
+            ListNode*newnode=new ListNode(curr->val);
+            tmp->next=newnode;
+            tmp=newnode;
+        }
+        while(!qlarge.empty())
+        {
+            auto curr=qlarge.front();
+            qlarge.pop();
+            ListNode* newnode=new ListNode(curr->val);
+            tmp->next=newnode;
+            tmp=newnode;
+        }
+        tmp->next=NULL;
+        return dummy->next;
+    }
+};
