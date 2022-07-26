dd4f5a49c63da3f1ac249a36736286b2bee99a01
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-26 10:34:27 +0530

    Time: 23 ms (58.51%), Space: 14.3 MB (25.30%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 // agar kisi point pe hame p ya q milega to p or return karna hai if not found return null
 // in case agr kisi node pe hame p aur q dono return ho rhe hai us case me root ki hi value return 
 // kro and that value will also be lowest common ancestor
@@ -23,7 +24,7 @@ public:
             return rt;
         else if(rt==NULL)
             return lft;
-            else // is case me p aur q dono return ho rhe hain and none of them are nULL
+            else // is case me p aur q dono return ho rhe hain and none of them are NULL
                 return root;
     }
 };
