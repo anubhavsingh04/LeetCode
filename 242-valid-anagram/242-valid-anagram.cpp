fb679e548ed63837b7077315541e21d4a4f7fad7
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-28 09:52:32 +0530

    Time: 19 ms (36.65%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,11 +1,33 @@
+// TC: O(n) space: O(1)
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.size()!=t.size()) return false;
+        
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+        
+//         return s==t;            
+//     }
+// };
+
+
+// Using map 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size()!=t.size()) return false;
-        
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        return s==t;            
+        if(s.size()!=t.size()) return false;
+        map<char,int>m;
+        for(int i=0;i<s.size();i++)
+        {
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+        for(auto &x:m)
+        {
+            if(x.second!=0) return false;
+        }
+        return true;
     }
 };
