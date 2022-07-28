ecce1f88e3ad87c221fa8ed5537f7b3422d76c1e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-07-28 10:03:31 +0530

    Time: 119 ms (5.61%), Space: 23.6 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -36,16 +36,30 @@
 
 // TC: O(n) space O(n)
 
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.size()!=t.size()) return false;
+//         map<char,int>m1,m2;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             m1[s[i]]++;
+//             m2[t[i]]++;
+//         }
+//         return m1==m2;
+//     }
+// };
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        map<char,int>m1,m2;
+        multiset<char>s_set,t_set;
         for(int i=0;i<s.size();i++)
         {
-            m1[s[i]]++;
-            m2[t[i]]++;
+            s_set.insert(s[i]);
+            t_set.insert(t[i]);
         }
-        return m1==m2;
+        return s_set==t_set;
     }
 };
