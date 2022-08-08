fe4b9e869cb337e796a785bd3df532b9c3238f9d
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-09 00:30:09 +0530

    Time: 3 ms (39.24%), Space: 6.5 MB (71.27%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,17 +1,34 @@
-// ith ow have i+1 elements 
+// ith row have i+1 elements 
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int rows) {
+//         vector<vector<int>>ans;
+//         for(int i=0;i<rows;i++)
+//         {
+//             vector<int>row(i+1,1);
+//             for(int j=1;j<i;j++)
+//             {
+//                 row[j]=ans[i-1][j-1]+ans[i-1][j];
+//             }
+//             ans.push_back(row);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> generate(int rows) {
-        vector<vector<int>>ans;
-        for(int i=0;i<rows;i++)
-        {
-            vector<int>row(i+1,1);
-            for(int j=1;j<i;j++)
-            {
-                row[j]=ans[i-1][j-1]+ans[i-1][j];
-            }
-            ans.push_back(row);
-        }
+        vector<vector<int >>ans;
+       for(int i=0;i<rows;i++)
+       {
+           vector<int>row(i+1,1);
+           for(int j=1;j<i;j++)
+           {
+               row[j]=ans[i-1][j-1]+ans[i-1][j];
+           }
+           ans.push_back(row);
+       }
         return ans;
     }
 };
