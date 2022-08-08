ac1e2f43c1427c626ae8c1a4e2255fca60c1bfb3
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-09 01:16:18 +0530

    Time: 2 ms (66.41%), Space: 8.3 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,4 +1,42 @@
-// Butch national flag DNF sort
+// Dutch national flag DNF sort
+// Take low and mid as 0 and high as n-1
+// traverse until mid<=high because each time nums[mid] is compared
+// if nums[mid] is 0 then swap with nums[low] increase mid and low 
+// if nums[mid] is 1 then just update the mid value as we want 1 in thre mid 
+// lastly if nums[mid]==2 then swap with nums[high] and decrease high
+
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n=nums.size();
+//         int low=0,mid=0,high=n-1;
+//         while(mid<=high)
+//         {
+//             if(nums[mid]==0)
+//             {
+//                 swap(nums[low],nums[mid]);
+//                 low++,mid++;
+//             }
+//             else if(nums[mid]==1)
+//                 mid++;
+//             else 
+//             {
+//                 swap(nums[mid],nums[high]);
+//                 high--;
+//             }
+//         }
+//     }
+// };
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
@@ -7,17 +45,10 @@ public:
         while(mid<=high)
         {
             if(nums[mid]==0)
-            {
-                swap(nums[mid],nums[low]);
-                low++,mid++;
-            }
-            else if(nums[mid]==1)
-                mid++;
-            else 
-            {
-                swap(nums[mid],nums[high]);
-                high--;
-            }
+            swap(nums[low],nums[mid]),mid++,low++;
+            else if(nums[mid]==1) mid++;
+            else
+                swap(nums[mid],nums[high]),high--;
         }
     }
 };
