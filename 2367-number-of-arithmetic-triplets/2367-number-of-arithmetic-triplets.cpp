da3a954a6b616488c69c7cffe4b86219b05be13b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-10 01:13:14 +0530

    Time: 11 ms (75.00%), Space: 9.3 MB (25.00%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -0,0 +1,46 @@
+// Brute force 
+// we will check condition for k only if nums[j]-nums[i]==diff
+// class Solution {
+// public:
+//     int arithmeticTriplets(vector<int>& nums, int diff) {
+//         int ans=0;
+//         for(int i=0;i<nums.size()-2;i++)
+//         {
+//             for(int j=i+1;j<nums.size()-1;j++)
+//             {
+//                 if(nums[j]-nums[i]==diff)
+//                 for(int k=j+1;k<nums.size();k++)
+//                 {
+//                     if(nums[k]-nums[j]==diff)
+//                         ans++;
+//                 }
+//             }
+//         }
+//         return ans ;
+//     }
+// };
+
+// O(n) Solution 
+//nums[j] and nums[k] can be written as nums[j]=nums[i]+diff --(1)
+// and nums[k]=nums[j]+diff 
+// nums[k]=nums[i]+2*diff from equation (1)
+// create a map to store element of array
+// check two conditon if(nums[i]+diff) is in map and nums[i]+2*diff is in map
+
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int ans=0;
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        for(auto &i:nums)
+        {
+            if(i-diff>=0 && m[i-diff] && m[i-2*diff])
+                ans++;
+        }
+        return ans;
+    }
+};
