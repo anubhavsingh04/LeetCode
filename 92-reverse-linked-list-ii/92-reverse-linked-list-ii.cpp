d5c4d1056b549e5ef46ca7c359ad412e0c6eaa78
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-09 23:30:55 +0530

    Time: 6 ms (31.96%), Space: 7.4 MB (64.02%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -8,22 +8,63 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// reversal logic is nxt=curr->next; curr->next=prev; prev=curr; curr =nxt;
+
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode*dummy=new ListNode(0);
+       ListNode*dummy=new ListNode(-1);
         dummy->next=head;
-        ListNode*prev=dummy;
-        for(int i=0;i<left-1;i++)
-            prev=prev->next;
-        ListNode*curr=prev->next;
-        for(int i=0;i<right-left;i++)
+        ListNode*prev=NULL;
+        ListNode*curr=dummy;
+        
+        for(int i=0;i<left;i++)
         {
-            ListNode*forw=curr->next;
-            curr->next=forw->next;
-            forw->next=prev->next;
-            prev->next=forw;
+            prev=curr;
+            curr=curr->next;
         }
+        ListNode*curr1=curr,*prev1=prev,*nxt; 
+        
+        // reversing 
+        for(int i=left;i<=right;i++)
+        {
+            nxt=curr1->next;
+            curr1->next=prev1;
+            prev1=curr1;
+            curr1=nxt;
+        }
+        // connecting 
+        curr->next=curr1;
+        prev->next=prev1;
         return dummy->next;
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     ListNode* reverseBetween(ListNode* head, int left, int right) {
+//         ListNode*dummy=new ListNode(0);
+//         dummy->next=head;
+        
+//         ListNode*prev=dummy;
+        
+//         for(int i=0;i<left-1;i++)
+//             prev=prev->next;
+        
+//         ListNode*curr=prev->next;
+//         for(int i=0;i<right-left;i++)
+//         {
+//             ListNode*forw=curr->next;
+//             curr->next=forw->next;
+//             forw->next=prev->next;
+//             prev->next=forw;
+//         }
+//         return dummy->next;
+//     }
+// };
