22a7d20064ef1c3e97c35c0b50c07b60e4b7478a
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-09 23:41:41 +0530

    Time: 23 ms (61.17%), Space: 11.5 MB (29.96%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -8,26 +8,65 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// class Solution {
+// public:
+//     ListNode* reverseKGroup(ListNode* head, int k) {
+//         ListNode*tmp=head;
+//         for(int i=0;i<k;i++)
+//         {
+//             if(tmp==NULL) return head;
+//             tmp=tmp->next;
+//         }
+//         ListNode*prev=NULL;
+//         ListNode*nxt=NULL;
+//         ListNode*curr=head;
+//         for(int i=0;i<k;i++)
+//         {
+//             nxt=curr->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=nxt;
+//         }
+//         head->next=reverseKGroup(curr,k);
+//         return prev;
+//     }
+// };
+
+
+
+
+// here we will be reversing pair of k nodes each time 
+// reverse function will return newhead 
+// after first k nodes will be reversed recersive fn is callled in which new head is tmp
+
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode*tmp=head;
-        for(int i=0;i<k;i++)
-        {
-            if(tmp==NULL) return head;
-            tmp=tmp->next;
-        }
+    ListNode* reverse(ListNode*head,ListNode* tail)
+    {
+        ListNode*curr=head;
         ListNode*prev=NULL;
         ListNode*nxt=NULL;
-        ListNode*curr=head;
-        for(int i=0;i<k;i++)
+        while(curr!=tail)
         {
             nxt=curr->next;
             curr->next=prev;
             prev=curr;
             curr=nxt;
         }
-        head->next=reverseKGroup(curr,k);
         return prev;
     }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode*tmp=head;
+        for(int i=0;i<k;i++)
+        {
+            if(!tmp) return head;
+            tmp=tmp->next;
+        }
+        ListNode*newhead=reverse(head,tmp);
+        head->next=reverseKGroup(tmp,k);
+        return newhead;
+    }
 };
+
+
