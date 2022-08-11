4d4d85dedaad67f4ac8e13e124b681ecccb34044
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-12 02:15:26 +0530

    Time: 8 ms (27.80%), Space: 12.1 MB (76.11%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -16,7 +17,7 @@ public:
         if(!root) return ans;
         queue<TreeNode*>q;
         q.push(root); 
-        bool flag=1; // means left to right  ans flag 0 means right to left
+        bool lefttoright=1; 
         while(!q.empty())
         {
             int sz=q.size();
@@ -25,14 +26,45 @@ public:
             {
                 TreeNode*curr=q.front();
                 q.pop();
-                int index=(flag==1?i:sz-1-i);
+                int index=(lefttoright ? i : sz-i-1);
+                
                 level[index]=curr->val;
                 if(curr->left) q.push(curr->left);
                 if(curr->right) q.push(curr->right);
             }
             ans.push_back(level);
-            flag=!flag;
+            lefttoright=!lefttoright;
         }
         return ans ;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+//         vector<vector<int>>ans;
+//         if(!root) return ans;
+//         queue<TreeNode*>q;
+//         q.push(root); 
+//         bool flag=1; // means left to right  ans flag 0 means right to left
+//         while(!q.empty())
+//         {
+//             int sz=q.size();
+//             vector<int>level(sz);
+//             for(int i=0;i<sz;i++)
+//             {
+//                 TreeNode*curr=q.front();
+//                 q.pop();
+//                 int index=(flag==1?i:sz-1-i);
+//                 level[index]=curr->val;
+//                 if(curr->left) q.push(curr->left);
+//                 if(curr->right) q.push(curr->right);
+//             }
+//             ans.push_back(level);
+//             flag=!flag;
+//         }
+//         return ans ;
+//     }
+// };
