24b51fa964acaaa43d629eb049a96f5a3d22faad
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-11 15:33:11 +0530

    Time: 37 ms (51.63%), Space: 18 MB (74.07%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,22 +3,56 @@
 // so return false otherwise insert it into row ,col and block 
 
 
+// class Solution {
+// public:
+//     bool isValidSudoku(vector<vector<char>>& board) {
+//         vector<set<char>>row(9),col(9),block(9);
+//         for(int i=0;i<9;i++)
+//         {
+//             for(int j=0;j<9;j++)
+//             {
+//                 if(board[i][j]=='.') continue;
+                
+//                 char ch=board[i][j];
+//                 if(row[i].count(ch) || col[j].count(ch) || block[3*(i/3)+j/3].count(ch))
+//                     return false;
+//                 row[i].insert(ch);
+//                 col[j].insert(ch);
+//                 block[3*(i/3)+j/3].insert(ch);
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+// here for each position we will check the curr character is valid or not 
+// almost the similar logic but without using extra space 
+
 class Solution {
 public:
+    bool isvalid(vector<vector<char>>&board,char ch,int row,int col)
+    {
+        for(int i=0;i<9;i++)
+        {
+            if(board[row][i]==ch) return false;
+            if(board[i][col]==ch) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==ch) return false;
+        }
+        return true;
+    }
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<set<char>>row(9),col(9),block(9);
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
                 if(board[i][j]=='.') continue;
-                
                 char ch=board[i][j];
-                if(row[i].count(ch) || col[j].count(ch) || block[3*(i/3)+j/3].count(ch))
-                    return false;
-                row[i].insert(ch);
-                col[j].insert(ch);
-                block[3*(i/3)+j/3].insert(ch);
+                board[i][j]='.';
+                if(!isvalid(board,ch,i,j)) return false;
+                else {
+                    board[i][j]=ch;
+                }
             }
         }
         return true;
