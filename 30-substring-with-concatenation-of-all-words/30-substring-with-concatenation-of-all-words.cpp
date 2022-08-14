abc8cbfbb415811a6cf9cc7e7dcff55bcf3a7612
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-14 14:06:44 +0530

    Time: 1795 ms (12.74%), Space: 302.3 MB (18.36%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,38 +1,75 @@
 class Solution {
 public:
-    
-    bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
-        for(int j=0; j<s.size(); j+=wordLen) {
-            string w = s.substr(j, wordLen);
-            if(wordCount.find(w) != wordCount.end()) {
-                if(--wordCount[w] == -1) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> res;
-        int wordLen = words[0].size();
-        int sLen = s.size();
-        int wordsWindow = words.size() * wordLen;
-        
-        unordered_map<string, int> wordCount;
-        for(int i=0; i<words.size(); i++) {
-            wordCount[words[i]]++;
-        }
-        
-        int i = 0;
-        while(i + wordsWindow <= sLen) {
-            if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
-                res.push_back(i);
+        int n=words.size();
+        int m=words[0].size();
+        vector<int>ans;
+        if(m*n>s.size()) return ans;
+        for(int i=0;i<=s.size()-m*n;i++)
+        {
+            unordered_map<string,int>mp;
+            for(int j=0;j<n;j++)
+            {
+                mp[words[j]]++;
             }
-            i++;
+            int t;
+            for(t=0;t<n;t++)
+            {
+                string str = s.substr(i+t*m,m);
+                
+                if(mp.count(str)==0) 
+                    break;
+                else
+                {
+                    if(mp[str]!=0)
+                        mp[str]--;
+                    else 
+                        break;
+                }
+            }
+            if(t==n)
+                ans.push_back(i);
         }
-        return res;
+        return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
+//         for(int j=0; j<s.size(); j+=wordLen) {
+//             string w = s.substr(j, wordLen);
+//             if(wordCount.find(w) != wordCount.end()) {
+//                 if(--wordCount[w] == -1) {
+//                     return false;
+//                 }
+//             } else {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     vector<int> findSubstring(string s, vector<string>& words) {
+//         vector<int> res;
+//         int wordLen = words[0].size();
+//         int sLen = s.size();
+//         int wordsWindow = words.size() * wordLen;
+        
+//         unordered_map<string, int> wordCount;
+//         for(int i=0; i<words.size(); i++) {
+//             wordCount[words[i]]++;
+//         }
+        
+//         int i = 0;
+//         while(i + wordsWindow <= sLen) {
+//             if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
+//                 res.push_back(i);
+//             }
+//             i++;
+//         }
+//         return res;
+//     }
+// };
