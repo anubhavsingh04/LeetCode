f2fec22d3887151c09ec3c01dbb95c1a4011aae7
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-14 15:09:53 +0530

    Time: 7 ms (71.90%), Space: 9.1 MB (25.88%) - LeetHub

--- 942-di-string-match/942-di-string-match.cpp
+++ 942-di-string-match/942-di-string-match.cpp
@@ -1,24 +1,52 @@
-class Solution {
+// In for loop we have pushed n integer 1 integer is remaining 
+// after coming out of the for loop value of  start and end will be equal so push any of them
+
+class Solution{
 public:
-    vector<int> diStringMatch(string s) {
-        // int a=0,b=s.size();
-        // vector<int>ans;
-        // for(auto &i:s)
-        // {
-        //     if(i=='I') ans.push_back(a++);
-        //     else if(i=='D') ans.push_back(b--);
-        // }
-        // if(s[s.size()-1]=='D') ans.push_back(b--);
-        // if(s[s.size()-1]=='I') ans.push_back(a++);
-        // return ans;
-        int l=0,r=s.size();
+    vector<int> diStringMatch(string s){
         vector<int>ans;
-        for(auto &i:s)
+        int start=0,end=s.size();
+        for(int i=0;i<s.size();i++)
         {
-            if(i=='D') ans.push_back(r--);
-            else ans.push_back(l++);
+            if(s[i]=='I')
+                ans.push_back(start++);
+            else 
+                ans.push_back(end--);
         }
-        ans.push_back(l++);
+        ans.push_back(start++);
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> diStringMatch(string s) {
+//         int l=0,r=s.size();
+//         vector<int>ans;
+//         for(auto &i:s)
+//         {
+//             if(i=='D') ans.push_back(r--);
+//             else ans.push_back(l++);
+//         }
+//         ans.push_back(l++);
+//         return ans;
+//     }
+// };
+
+// class Solution{
+// public:
+//     vector<int> diStringMatch(string s){
+//         int a=0,b=s.size();
+//         vector<int>ans;
+//         for(auto &i:s)
+//         {
+//             if(i=='I') ans.push_back(a++);
+//             else if(i=='D') ans.push_back(b--);
+//         }
+//         if(s[s.size()-1]=='D') ans.push_back(b--);
+//         if(s[s.size()-1]=='I') ans.push_back(a++);
+//         return ans;    
+//     }
+// };
