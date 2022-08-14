78f16fd0bab5702d908646427b1afd6551c37bd0
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-14 22:20:50 +0530

    Time: 642 ms (44.44%), Space: 176.8 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-    
+    int dfs(int src,vector<int>adj[],vector<int>&vis,unordered_set<int>&s)
+    {
+        int cnt=0;
+        if(!vis[src])
+        {
+            vis[src]=1;
+            cnt++;
+        }
+        for(auto &i:adj[src])
+        if(!vis[i] && !s.count(i)) 
+            cnt+=dfs(i,adj,vis,s);
+        return cnt;
+    }
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         unordered_set<int>s(restricted.begin(),restricted.end());
         vector<int>vis(n,0);
-        int ans=0;
         vector<int>adj[n];
         for(int i=0;i<edges.size();i++)
         {
@@ -13,24 +24,44 @@ public:
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        queue<int>q;
-        q.push(0);
-        vis[0]=true;
-        while(!q.empty())
-        {
-            int curr=q.front();
-            q.pop();
-            if(s.count(curr)) continue;
-            ans++;
-            for(auto &i:adj[curr])
-            {
-                if(!vis[i])
-                {
-                    vis[i]=true;
-                    q.push(i);
-                }
-            }
-        }
+        int ans=dfs(0,adj,vis,s);
         return ans;
     }
 };
+
+// Using bfs
+// class Solution {
+// public:
+//     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+//         unordered_set<int>s(restricted.begin(),restricted.end());
+//         vector<int>vis(n,0);
+//         int ans=0;
+//         vector<int>adj[n];
+//         for(int i=0;i<edges.size();i++)
+//         {
+//             int u=edges[i][0];
+//             int v=edges[i][1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+//         queue<int>q;
+//         q.push(0);
+//         vis[0]=true;
+//         while(!q.empty())
+//         {
+//             int curr=q.front();
+//             q.pop();
+//             if(s.count(curr)) continue;
+//             ans++;
+//             for(auto &i:adj[curr])
+//             {
+//                 if(!vis[i])
+//                 {
+//                     vis[i]=true;
+//                     q.push(i);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
