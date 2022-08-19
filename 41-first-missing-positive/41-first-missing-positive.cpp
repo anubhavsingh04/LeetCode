ea5cd373f11122df8facb276c7b8b6e2ac1cb959
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-19 23:06:22 +0530

    Time: 50 ms (99.93%), Space: 21.6 MB (99.93%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,9 +1,11 @@
 // Put each element at its right position like 5 will be at positon 4 
 // if nums[i]==5 then it should be at position nums[nums[i]-1]
+// TC: O(n) SC: O(1)
 
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        
         int n=nums.size();
         for(int i=0;i<n;i++)
         {
@@ -16,5 +18,6 @@ public:
                 return i+1;
         }
         return n+1;
+        
     }
 };
