00edab98e688cd2ddb9f4f7fa1843c1d17fa6fa1
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-19 22:21:47 +0530

    Time: 75 ms (83.18%), Space: 23.4 MB (67.40%) - LeetHub

--- 491-increasing-subsequences/491-increasing-subsequences.cpp
+++ 491-increasing-subsequences/491-increasing-subsequences.cpp
@@ -1,29 +1,58 @@
 class Solution {
 public:
-    void solve(int cnt,vector<int>&nums,vector<int>&v,set<vector<int>>&st)
+    void solve(vector<int>&nums,vector<int>&v,set<vector<int>>&st,int idx,int n)
     {
-        if(cnt==nums.size()) {
-            if(v.size()>1)
-            {
-                st.insert(v);
-            }
-            return;
-        }
-        if(v.empty()||nums[cnt]>=v.back())
+        if(idx>nums.size()) return;
+        if(v.size()>1) st.insert(v);
+        for(int i=idx;i<n;i++)
         {
-            v.push_back(nums[cnt]);
-            solve(cnt+1,nums,v,st);
-            v.pop_back();
+            if(v.empty()||nums[i]>=v.back())
+            {
+                v.push_back(nums[i]);
+                solve(nums,v,st,i+1,n);
+                v.pop_back();
+            } 
         }
-        solve(cnt+1,nums,v,st);
     }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         set<vector<int>>st;
         vector<vector<int>>ans;
         vector<int>v;
-        solve(0,nums,v,st);
+        solve(nums,v,st,0,nums.size());
         for(auto &i:st)
             ans.push_back(i);
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     void solve(int cnt,vector<int>&nums,vector<int>&v,set<vector<int>>&st)
+//     {
+//         if(cnt==nums.size()) {
+//         if(v.size()>1)
+//         {
+//             st.insert(v);
+//         }
+//             return;
+//         }
+//         if(v.empty()||nums[cnt]>=v.back())
+//         {
+//             v.push_back(nums[cnt]);
+//             solve(cnt+1,nums,v,st);
+//             v.pop_back();
+//         }
+//         solve(cnt+1,nums,v,st);
+//     }
+//     vector<vector<int>> findSubsequences(vector<int>& nums) {
+//         set<vector<int>>st;
+//         vector<vector<int>>ans;
+//         vector<int>v;
+//         solve(0,nums,v,st);
+//         for(auto &i:st)
+//             ans.push_back(i);
+//         return ans;
+//     }
+// };
