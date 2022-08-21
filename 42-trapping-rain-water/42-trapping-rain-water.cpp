a21910d6f390611fbf57ed45b0b6cd3ea28878fd
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-21 21:40:37 +0530

    Time: 22 ms (66.93%), Space: 20.6 MB (11.55%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -8,25 +8,49 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>maxm_left(n),maxm_right(n);
-        
-        maxm_left[0]=height[0];
+        vector<int>maxleft(n),maxright(n);
+        maxleft[0]=height[0];
+        maxright[n-1]=height[n-1];
         for(int i=1;i<n;i++)
         {
-            maxm_left[i]=max(maxm_left[i-1],height[i]);
+            maxleft[i]=max(maxleft[i-1],height[i]);
         }
-        
-        maxm_right[n-1]=height[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            maxm_right[i]=max(maxm_right[i+1],height[i]);
+            maxright[i]=max(maxright[i+1],height[i]);
         }
-        
-        int water=0;
+        long long water=0;
         for(int i=0;i<n;i++)
         {
-            water+=(min(maxm_left[i],maxm_right[i])-height[i]);
+            water+=(min(maxright[i],maxleft[i])-height[i]);
         }
         return water;
     }
 };
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n=height.size();
+//         vector<int>maxm_left(n),maxm_right(n);
+        
+//         maxm_left[0]=height[0];
+//         for(int i=1;i<n;i++)
+//         {
+//             maxm_left[i]=max(maxm_left[i-1],height[i]);
+//         }
+        
+//         maxm_right[n-1]=height[n-1];
+//         for(int i=n-2;i>=0;i--)
+//         {
+//             maxm_right[i]=max(maxm_right[i+1],height[i]);
+//         }
+        
+//         int water=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             water+=(min(maxm_left[i],maxm_right[i])-height[i]);
+//         }
+//         return water;
+//     }
+// };
