dad0ce6d334e0f6262c946d19ad7c7f2f131294b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-22 11:08:08 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,18 +1,21 @@
-// (num & 0xAAAAAAAA) == 0? 0xAAAAAAAA is 10101010‭10101010101010101010101010101010‬ in binary. ANDing a 32-bit number with 0xAAAAAAAA means setting all even bits to 0. After clearing even bits, if the number is larger than 0, it means the 1 bit is in an odd position and num is just a power of 2. Otherwise num is a power of 4 (or a zero).
-
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if((n>0)&&(n&n-1)==0&&(n & 0xAAAAAAAA) == 0) return true;
-        return false ;
+        if(n<0)return false;
+       long long x=abs(n);
+        if(((x)&(x-1))==0)
+        {
+            int cnt=0;
+            while(x)
+            {
+                x=x>>1;
+                cnt++;
+                
+            }
+            
+            if(cnt%2)return true;
+            return false;
+        }
+        return false;
     }
 };
-
-
-// class Solution {
-// public:
-//     bool isPowerOfFour(int n) {
-//         if((n>0)&&(n&n-1)==0&&(n-1)%3==0) return true;
-//         return false ;
-//     }
-// };
