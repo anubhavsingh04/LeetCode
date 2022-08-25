45075106e1f269cafa17282c4a83b649377026a0
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-25 07:10:23 +0530

    Time: 19 ms (72.14%), Space: 8.9 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -8,16 +8,34 @@ public:
         }
         for(int i=0;i<ransomNote.size();i++)
         {
-            if(m.find(ransomNote[i])!=m.end())
-            {
-                if(m[ransomNote[i]]>0)
-                m[ransomNote[i]]--;
-                else 
-                    return false;
-            }
-            else 
-                return false;
+            if(m[ransomNote[i]]>0)
+            m[ransomNote[i]]--;
+            else return false;
         }
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool canConstruct(string ransomNote, string magazine) {
+//         unordered_map<char,int>m;
+//         for(auto &i:magazine)
+//         {
+//             m[i]++;
+//         }
+//         for(int i=0;i<ransomNote.size();i++)
+//         {
+//             if(m.find(ransomNote[i])!=m.end())
+//             {
+//                 if(m[ransomNote[i]]>0)
+//                 m[ransomNote[i]]--;
+//                 else return false;
+//             }
+//             else 
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
