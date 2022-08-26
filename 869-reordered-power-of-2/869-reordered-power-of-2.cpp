ab11fc92d3058619b7e6ca8946cc578667aff94c
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-26 09:20:16 +0530

    Time: 5 ms (53.95%), Space: 5.9 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,25 +1,50 @@
+// two maps will be equal if all the digits and freq are equal
+// store the digits of n in m
+// check for the digits in all powers of 2 
+// class Solution {
+// public:
+//     bool isequal(int n,map<int,int>m)
+//     {
+//         map<int,int>m2;
+//         while(n)
+//         {
+//             m2[n%10]++;
+//             n/=10;
+//         }
+//         return m2==m; 
+//     }
+//     bool reorderedPowerOf2(int n) {
+//         map<int,int>digits;
+//         while(n)
+//         {
+//             digits[n%10]++;
+//             n/=10;
+//         }
+//         for(int i=0;i<32;i++)
+//         {
+//             if(isequal((1<<i),digits))
+//                 return true;
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    bool isequal(int n,map<int,int>m)
+    string sort_(int n)
     {
-        map<int,int>m2;
-        while(n)
-        {
-            m2[n%10]++;
-            n/=10;
-        }
-        return m2==m;
+        string s=to_string(n);
+        sort(s.begin(),s.end());
+        return s;
     }
     bool reorderedPowerOf2(int n) {
-        map<int,int>digits;
-        while(n)
-        {
-            digits[n%10]++;
-            n/=10;
-        }
+        string str=sort_(n);
         for(int i=0;i<32;i++)
         {
-            if(isequal((1<<i),digits))
+            if(sort_(1<<i)==str)
                 return true;
         }
         return false;
