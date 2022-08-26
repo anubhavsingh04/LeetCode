b76595892fb3e59533a5f643c12f23aad4618c28
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-26 09:33:58 +0530

    Time: 12 ms (15.28%), Space: 9.4 MB (95.23%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,44 +1,16 @@
-// class Solution {
-// public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         for(int i  = 0; i<matrix.size(); ++i)
-//         {
-//             for(int j = 0; j<matrix[0].size(); ++j)
-//             {
-//                 if(matrix[i][j] == target)
-//                     return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-// BInary search implementation
-// A n * m matrix converted into an array: matrix[x][y] : a[x * m + y]
-// An array can be converted into n * m matrix: a[x] : matrix[x / m][x % m]
-
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        if(n == 0 || m == 0)
-            return false;
-        
-// matrix converted into array---> [0..n*m]
-        int start = 0, end = m*n - 1;    
-        while(start <= end)
+        int n=matrix.size(),m=matrix[0].size();
+        int low=0,high=m*n-1;
+        while(low<=high)
         {
-            int mid = start + (end - start) / 2;
-			// a[x] : matrix[x / m][x % m] formulae
-            int ind = matrix[mid/m][mid%m];
-            if (target == ind)
-                return true;
-            else if(target < ind)
-                end = mid - 1;
-            else
-                start = mid + 1;       
+            int mid=(low+(high-low)/2);
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
         }
         return false;
     }
