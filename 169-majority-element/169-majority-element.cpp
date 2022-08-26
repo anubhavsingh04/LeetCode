46cd33d8e4e3ee2f27b8fbd66ae1540b3a7b9fa6
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-26 11:54:16 +0530

    Time: 27 ms (67.20%), Space: 19.5 MB (92.21%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -14,10 +14,19 @@
 //     }
 // };
 
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         return nums[nums.size()/2];
+//     }
+// };
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        nth_element(nums.begin(),nums.begin()+nums.size()/2,nums.end());
         return nums[nums.size()/2];
     }
 };
