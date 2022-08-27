0eacff6b1aef637c769ef98b608c0ebf8855bdd3
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-28 00:48:20 +0530

    Time: 28 ms (51.49%), Space: 14.5 MB (68.44%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,15 +1,30 @@
+// class Solution {
+// public:
+//     vector<int> replaceElements(vector<int>& nums) {
+//         int n=nums.size();
+//         int maxi=nums[n-1];
+//         vector<int>ans(n);
+//         ans[n-1]=-1;
+//         for(int i=n-2;i>=0;i--)
+//         {
+//             maxi=max(maxi,nums[i+1]);
+//             ans[i]=maxi;
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& nums) {
-        int n=nums.size();
-        int maxi=nums[n-1];
-        vector<int>ans(n);
-        ans[n-1]=-1;
-        for(int i=n-2;i>=0;i--)
+        int n=nums.size(),maxi=-1,curr;
+        for(int i=n-1;i>=0;i--)
         {
-            maxi=max(maxi,nums[i+1]);
-            ans[i]=maxi;
+            curr=nums[i];
+            nums[i]=maxi;
+            maxi=max(maxi,curr);
         }
-        return ans;
+        return nums;
     }
 };
