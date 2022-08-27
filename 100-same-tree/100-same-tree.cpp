51960ec99bd41ed422fbd3caac7a187a2222586c
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-28 03:02:59 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (45.02%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -10,14 +10,27 @@
  * };
  */
 
+// class Solution {
+// public:
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         if(p==NULL||q==NULL) return p==q;
+//         return p->val==q->val&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL||q==NULL) return p==q;
-        // inorder trversal 
-        return p->val==q->val&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+        if(!p||!q) return p==q;
+        if(p->val!=q->val) return false;
+        return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
     }
 };
+
+
+
 
 // class Solution {
 // public:
