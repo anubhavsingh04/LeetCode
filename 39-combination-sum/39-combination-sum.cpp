1c0d3597887958ff4028ad23ec5fdc5a8efea4a8
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-29 21:22:11 +0530

    Time: 157 ms (18.98%), Space: 26.6 MB (25.79%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,58 @@
+// class Solution {
+// public:
+//     void solve(vector<int>& nums,vector<int>op,int idx,int target,vector<vector<int>>&ans)
+//     {
+//         if(target<0) return;
+//         if(target==0)
+//         {
+//             ans.push_back(op);
+//             return;
+//         }
+//         for(int i=idx;i<nums.size();i++)
+//         {
+//             op.push_back(nums[i]);
+//             solve(nums,op,i,target-nums[i],ans);
+//             op.pop_back();
+//         }
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         sort(candidates.begin(),candidates.end());
+//         vector<int>op;
+//         vector<vector<int>>ans;
+//         solve(candidates,op,0,target,ans);
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void solve(vector<int>& nums,vector<int>op,int idx,int target,vector<vector<int>>&ans)
+    void solve(vector<int>& nums,vector<int>op,int idx,int sum,int target,vector<vector<int>>&ans)
     {
-        if(target<0) return;
-        if(target==0)
+        if(sum>target) return;
+        if(sum==target)
         {
             ans.push_back(op);
             return;
         }
         for(int i=idx;i<nums.size();i++)
         {
+            sum+=nums[i];
             op.push_back(nums[i]);
-            solve(nums,op,i,target-nums[i],ans);
+            solve(nums,op,i,sum,target,ans);
             op.pop_back();
+            sum-=nums[i];
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
         vector<int>op;
         vector<vector<int>>ans;
-        solve(candidates,op,0,target,ans);
+        solve(candidates,op,0,0,target,ans);
         return ans;
     }
 };
-
 
 
 // class Solution {
