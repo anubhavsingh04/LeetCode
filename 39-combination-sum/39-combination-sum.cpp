eb176c35ab027ac13e4423d3ec380c61d531e23b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-29 21:15:21 +0530

    Time: 81 ms (27.15%), Space: 26.4 MB (26.09%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,53 +1,52 @@
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<int>op,int idx,int target,vector<vector<int>>&ans)
+    {
+        if(target<0) return;
+        if(target==0)
+        {
+            ans.push_back(op);
+            return;
+        }
+        for(int i=idx;i<nums.size();i++)
+        {
+            op.push_back(nums[i]);
+            solve(nums,op,i,target-nums[i],ans);
+            op.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int>op;
+        vector<vector<int>>ans;
+        solve(candidates,op,0,target,ans);
+        return ans;
+    }
+};
+
+
+
 // class Solution {
 // public:
-//     void solve(vector<int>& nums,vector<int>op,int idx,int target,vector<vector<int>>&ans)
+//     void solve(vector<int>& nums,vector<int>op,int idx,int sum,int target,vector<vector<int>>&ans)
 //     {
-//         if(idx==nums.size())
-//         {
-//             if(target==0){
-//                 ans.push_back(op);
-//             }
+//         if(sum>target || idx>=nums.size()) return;
+//         if(sum==target){
+//             ans.push_back(op); 
 //             return;
 //         }
-//         if(nums[idx]<=target)
-//         {
-//             op.push_back(nums[idx]);
-//             solve(nums,op,idx,target-nums[idx],ans);
-//             op.pop_back(); 
-//         }
-//         solve(nums,op,idx+1,target,ans);
+//         solve(nums,op,idx+1,sum,target,ans);
+        
+//         op.push_back(nums[idx]);
+//         sum+=nums[idx];
+//         solve(nums,op,idx,sum,target,ans);
+//         sum-=nums[idx];
+//         op.pop_back();
 //     }
 //     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 //         vector<int>op;
 //         vector<vector<int>>ans;
-//         solve(candidates,op,0,target,ans);
+//         solve(candidates,op,0,0,target,ans);
 //         return ans;
 //     }
 // };
-
-
-
-class Solution {
-public:
-    void solve(vector<int>& nums,vector<int>op,int idx,int sum,int target,vector<vector<int>>&ans)
-    {
-        if(sum>target || idx>=nums.size()) return;
-        if(sum==target){
-            ans.push_back(op); 
-            return;
-        }
-        solve(nums,op,idx+1,sum,target,ans);
-        
-        op.push_back(nums[idx]);
-        sum+=nums[idx];
-        solve(nums,op,idx,sum,target,ans);
-        sum-=nums[idx];
-        op.pop_back();
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>op;
-        vector<vector<int>>ans;
-        solve(candidates,op,0,0,target,ans);
-        return ans;
-    }
-};
