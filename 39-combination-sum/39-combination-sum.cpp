363098eca2f53290a86837ba378893b87b76236d
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-29 21:04:04 +0530

    Time: 362 ms (7.36%), Space: 85.3 MB (11.51%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,30 +1,53 @@
+// class Solution {
+// public:
+//     void solve(vector<int>& nums,vector<int>op,int idx,int target,vector<vector<int>>&ans)
+//     {
+//         if(idx==nums.size())
+//         {
+//             if(target==0){
+//                 ans.push_back(op);
+//             }
+//             return;
+//         }
+//         if(nums[idx]<=target)
+//         {
+//             op.push_back(nums[idx]);
+//             solve(nums,op,idx,target-nums[idx],ans);
+//             op.pop_back(); 
+//         }
+//         solve(nums,op,idx+1,target,ans);
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<int>op;
+//         vector<vector<int>>ans;
+//         solve(candidates,op,0,target,ans);
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void findcombination(vector<int>& nums,int idx,int target,vector<int>&v,vector<vector<int>>&ans)
+    void solve(vector<int>& nums,vector<int>op,int idx,int sum,int target,vector<vector<int>>&ans)
     {
-        if(idx==nums.size())
-        {
-            if(target==0)
-            {
-                ans.push_back(v);
-            }
+        if(sum>target || idx>=nums.size()) return;
+        if(sum==target){
+            ans.push_back(op); 
             return;
         }
-        // pick (if(nums[idx]<=target))
-        if(nums[idx]<=target)
-        {
-            v.push_back(nums[idx]);
-            findcombination(nums,idx,target-nums[idx],v,ans);
-            v.pop_back(); // backtrack
-        }
-        // not pick 
-        findcombination(nums,idx+1,target,v,ans);
+        solve(nums,op,idx+1,sum,target,ans);
         
+        op.push_back(nums[idx]);
+        sum+=nums[idx];
+        solve(nums,op,idx,sum,target,ans);
+        sum-=nums[idx];
+        op.pop_back();
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>op;
         vector<vector<int>>ans;
-        vector<int>v;
-        findcombination(candidates,0,target,v,ans);
+        solve(candidates,op,0,0,target,ans);
         return ans;
     }
 };
