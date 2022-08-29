79c72b6cf9f11e355ce1839f218be27efa72f381
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-29 21:41:36 +0530

    Time: 8 ms (41.25%), Space: 7.9 MB (47.68%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<int>op,int idx,set<vector<int>>&st)
+    void solve(vector<int>&nums,vector<int>op,int idx,vector<vector<int>>&ans)
     {
-        if(idx==nums.size()){
-            sort(op.begin(),op.end());
-            st.insert(op);
-            return;
+        ans.push_back(op);
+        for(int i=idx;i<nums.size();i++)
+        {
+            if(i>idx && nums[i]==nums[i-1]) continue;
+            op.push_back(nums[i]);
+            solve(nums,op,i+1,ans);
+            op.pop_back();
         }
-        solve(nums,op,idx+1,st);
-        op.push_back(nums[idx]);
-        solve(nums,op,idx+1,st);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>>st;
+        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>op;
-        solve(nums,op,0,st);
-        for(auto i:st) ans.push_back(i);
+        solve(nums,op,0,ans);
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     void solve(vector<int>&nums,vector<int>op,int idx,set<vector<int>>&st)
+//     {
+//         if(idx==nums.size()){
+//             sort(op.begin(),op.end());
+//             st.insert(op);
+//             return;
+//         }
+//         solve(nums,op,idx+1,st);
+//         op.push_back(nums[idx]);
+//         solve(nums,op,idx+1,st);
+//     }
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         set<vector<int>>st;
+//         vector<vector<int>>ans;
+//         vector<int>op;
+//         solve(nums,op,0,st);
+//         for(auto i:st) ans.push_back(i);
+//         return ans;
+//     }
+// };
