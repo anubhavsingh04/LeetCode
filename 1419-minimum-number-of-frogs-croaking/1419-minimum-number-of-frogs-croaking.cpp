b31d0be739dc86a8fff9ef3ae4b81995bb35be6e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-30 17:17:45 +0530

    Time: 139 ms (25.51%), Space: 9.2 MB (44.47%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool is_valid_sound(map<char,int>& mp) {
+    bool is_valid_sound(unordered_map<char,int>& mp) {
         if(mp['c']>=mp['r'] && mp['r']>=mp['o'] && mp['o']>=mp['a'] && mp['a']>=mp['k']) {
             return true;
         }
@@ -8,9 +8,9 @@ public:
     }
     int minNumberOfFrogs(string croakOfFrogs) {
         int ans=0;
-        map<char,int>freq;
+        unordered_map<char,int>freq;
         int croaking_frogs=0;
-        for(auto i:croakOfFrogs) {
+        for(auto &i:croakOfFrogs) {
             freq[i]++;
             if(not is_valid_sound(freq)) {
             return -1;
