23d726052efaf10fb8727cc4c8a323850012f6cc
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-30 14:00:21 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.50%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,30 @@
+ // for 90 degree: Take Transpose then swap columns 
+ // for 180 degree: Swap columns then swap rows
+ // for 270 degree: Take transpose then swap rows
+ // for taking transpose upper loop 0 to mat.size
+ // inner loop will run from i to mat.size then swap(mat[i][j],mat[j][i])
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n=mat.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        int m=mat[0].size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int start=0,end=m-1;
+            while(start<end)
+            {
+                swap(mat[i][start],mat[i][end]);
+                start++,end--;
+            }
+        }
+    }
+};
