3ebf83d341be68893a308acc1eb6367babf34804
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-30 14:53:15 +0530

    Time: 27 ms (41.64%), Space: 14.3 MB (57.19%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,24 +7,14 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
-// agar kisi point pe hame p ya q milega to p or return karna hai if not found return null
-// in case agr kisi node pe hame p aur q dono return ho rhe hai us case me root ki hi value return 
-// kro and that value will also be lowest common ancestor
-
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL||root==p||root==q)
-            return root;
+        if(!root || root==p || root==q) return root;
         TreeNode*lft=lowestCommonAncestor(root->left,p,q);
         TreeNode*rt=lowestCommonAncestor(root->right,p,q);
-        
-        if(lft==NULL)
-            return rt;
-        else if(rt==NULL)
-            return lft;
-            else // is case me p aur q dono return ho rhe hain and none of them are NULL
-                return root;
+        if(lft !=NULL && rt!=NULL) return root;
+        else if(lft==NULL) return rt;
+        else return lft;
     }
 };
