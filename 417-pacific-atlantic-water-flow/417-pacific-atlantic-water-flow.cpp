644da50c8e88f106e196fe65be07461bb73aafa1
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-08-31 14:18:25 +0530

    Time: 55 ms (77.73%), Space: 19.4 MB (42.09%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,27 +1,60 @@
 class Solution {
 public:
-    int m, n;
-	// denotes cells reachable starting from atlantic and pacific edged cells respectively
-    vector<vector<bool> > atlantic, pacific;
-	vector<vector<int> > ans;    
-    vector<vector<int> > pacificAtlantic(vector<vector<int>>& mat) {
-        if(!size(mat)) return ans;
-        m = size(mat), n = size(mat[0]);
-        atlantic = pacific = vector<vector<bool> >(m, vector<bool>(n, false));
-		// perform dfs from all edge cells (ocean-connected cells)
-        for(int i = 0; i < m; i++) dfs(mat, pacific, i, 0), dfs(mat, atlantic, i, n - 1);
-        for(int i = 0; i < n; i++) dfs(mat, pacific, 0, i), dfs(mat, atlantic, m - 1, i);             
+    void dfs(vector<vector<int>>& heights,vector<vector<int>> & ocean,int x,int y,int m, int n) {
+        if(x<0 or y<0 or x>=heights.size() or y>=heights[0].size()) {
+            return ;
+        }
+        if(not ocean[x][y]){
+            ocean[x][y]=1;
+            int k=heights[x][y];
+            if(y+1<m and heights[x][y+1]>=k) {
+                dfs(heights,ocean,x,y+1,m,n);
+            }
+            if(y-1>=0 and heights[x][y-1]>=k) {
+                dfs(heights,ocean,x,y-1,m,n);
+            }
+            if(x-1>=0 and heights[x-1][y]>=k) {
+                dfs(heights,ocean,x-1,y,m,n);
+            }
+            if(x+1 <n and heights[x+1][y]>=k) {
+                dfs(heights,ocean,x+1,y,m,n);
+            }
+        }
+    } 
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n=heights.size();
+        int m=heights[0].size();
+        vector<vector<int>> atlantic(n,vector<int>(m,0)),pacific(n,vector<int>(m,0));
+        //0 means not visited
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(i==0 or j==0) {
+                    if(not pacific[i][j]) {
+                        dfs(heights,pacific,i,j,m,n);
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(i==n-1 or j==m-1) {
+                    if(not atlantic[i][j]) {
+                        dfs(heights,atlantic,i,j,m,n);
+                    }
+                }
+            }
+        }
+        
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                // cout<<atlantic[i][j]<<"  ";
+                if(atlantic[i][j] and pacific[i][j]) {
+                    ans.push_back(vector<int>{i,j});
+                }
+            }
+            // cout<<endl;
+        }
         return ans;
-    }
-    void dfs(vector<vector<int> >& mat, vector<vector<bool> >& visited, int i, int j){        
-        if(visited[i][j]) return;
-        visited[i][j] = true;
-		// if cell reachable from both the oceans, insert into final answer vector
-        if(atlantic[i][j] && pacific[i][j]) ans.push_back(vector<int>{i, j});    
-		// dfs from current cell only if height of next cell is greater
-/*⬇️*/  if(i + 1 <  m && mat[i + 1][j] >= mat[i][j]) dfs(mat, visited, i + 1, j); 
-/*⬆️*/  if(i - 1 >= 0 && mat[i - 1][j] >= mat[i][j]) dfs(mat, visited, i - 1, j);
-/*➡️*/  if(j + 1 <  n && mat[i][j + 1] >= mat[i][j]) dfs(mat, visited, i, j + 1); 
-/*⬅️*/  if(j - 1 >= 0 && mat[i][j - 1] >= mat[i][j]) dfs(mat, visited, i, j - 1);
     }
 };
