74b6962f747c6447bdada57deebbdabe31a4a43f
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-01 15:17:51 +0530

    Time: 130 ms (99.06%), Space: 86.2 MB (91.25%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -9,40 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     int ans=0;
-//     void preorder(TreeNode*root,int maxi)
-//     {
-//         if(!root)return;
-//         if(root->val>=maxi) {
-//             ans++;
-//             maxi=root->val;
-//         }
-//         preorder(root->left,maxi);
-//         preorder(root->right,maxi);
-//     }
-//     int goodNodes(TreeNode* root) {
-//         preorder(root,root->val);
-//         return ans;
-//     }
-// };
-
-
-
-
 class Solution {
 public:
-    int preorder(TreeNode*root,int maxi)
+    // int ans=0;
+    void preorder(TreeNode*root,int maxi,int &ans)
     {
-        if(!root)return 0;
-        int ans=0;
-        if(root->val>=maxi) ans++,maxi=root->val;
-        ans+=preorder(root->left,maxi);
-        ans+=preorder(root->right,maxi);
-        return ans;
+        if(!root)return;
+        if(root->val>=maxi) {
+            ans++;
+            maxi=root->val;
+        }
+        preorder(root->left,maxi,ans);
+        preorder(root->right,maxi,ans);
     }
     int goodNodes(TreeNode* root) {
-        return preorder(root,root->val);
+        int ans=0;
+        preorder(root,root->val,ans);
+        return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int preorder(TreeNode*root,int maxi)
+//     {
+//         if(!root)return 0;
+//         int ans=0;
+//         if(root->val>=maxi) ans++,maxi=root->val;
+//         ans+=preorder(root->left,maxi);
+//         ans+=preorder(root->right,maxi);
+//         return ans;
+//     }
+//     int goodNodes(TreeNode* root) {
+//         return preorder(root,root->val);
+//     }
+// };
