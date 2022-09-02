83a8e38d516780009a3b36558a44a86ee504c487
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-02 18:43:15 +0530

    Time: 4 ms (45.74%), Space: 5.9 MB (51.49%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    bool is_kthbit_set(int i,int n)
-    {
-        if((1<<i)&n) return true;
-        else return false;
-    }
     uint32_t reverseBits(uint32_t n) {
         int ans=0;
-        for(int i=0;i<32;i++)
+        for(int i=31;i>=0;i--)
         {
-            if(is_kthbit_set(i,n))
+            if(((1<<i)&n))
             {
-                ans=ans|(1<<(31-i));
+                ans|=(1<<(31-i));
             }
         }
         return ans;
