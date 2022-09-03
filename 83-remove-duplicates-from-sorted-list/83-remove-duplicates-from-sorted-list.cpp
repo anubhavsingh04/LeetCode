fb38598b37a9261d6e695f156e9eb230c8715581
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-03 18:58:36 +0530

    Time: 20 ms (35.44%), Space: 11.6 MB (79.00%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -8,46 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// class Solution {
-// public:
-//     ListNode* deleteDuplicates(ListNode* head) {
-//         if(!head) return NULL;
-//         ListNode*curr=head;
-//         while(curr->next)
-//         {
-//             if(curr->val==curr->next->val)
-//                 curr->next=curr->next->next;
-//             else
-//                 curr=curr->next;
-//         }
-//         return head;
-//     }
-// };
-
-
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode*curr=head,*agla=head->next;
-        while(agla!=NULL)
+        if(!head) return NULL;
+        ListNode*curr=head;
+        while(curr->next)
         {
-            if(curr->val!=agla->val)
-            {
+            if(curr->val==curr->next->val)
+                curr->next=curr->next->next;
+            else
                 curr=curr->next;
-                agla=agla->next;
-            }
-            else {
-                while(agla && agla->val==curr->val)
-                {
-                    agla=agla->next;
-                }
-                curr->next=agla;
-                curr=agla;
-                if(agla)
-                agla=agla->next;
-            }
         }
         return head;
     }
 };
+
+
+// class Solution {
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         if(!head || !head->next) return head;
+//         ListNode*curr=head,*agla=head->next;
+//         while(agla!=NULL)
+//         {
+//             if(curr->val!=agla->val)
+//             {
+//                 curr=curr->next;
+//                 agla=agla->next;
+//             }
+//             else {
+//                 while(agla && agla->val==curr->val)
+//                 {
+//                     agla=agla->next;
+//                 }
+//                 curr->next=agla;
+//                 curr=agla;
+//                 if(agla)
+//                 agla=agla->next;
+//             }
+//         }
+//         return head;
+//     }
+// };
