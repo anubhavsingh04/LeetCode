520e79d1b6298f39c01b13590cfd575096cb6d0e
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-09 11:49:55 +0530

    Time: 1626 ms (6.20%), Space: 125.1 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     static bool cmp(vector<int>&a,vector<int>&b)
     {
-        if(a[0]==b[0]) return a[1]<b[1];
+        if(a[0]==b[0])return a[1]<b[1];
         return a[0]>b[0];
     }
-    int numberOfWeakCharacters(vector<vector<int>>& v) {
-        sort(v.begin(),v.end(),cmp);
-        int ans=0;
+    int numberOfWeakCharacters(vector<vector<int>>& nums) {
+        sort(nums.begin(),nums.end(),cmp);
         int maxdefence=INT_MIN;
-        for(int i=0;i<v.size();i++)
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(maxdefence>v[i][1])
+            if(nums[i][1]<maxdefence)
             {
                 ans++;
             }
             else 
             {
-                maxdefence=v[i][1];
+                maxdefence=nums[i][1];
             }
         }
         return ans;
