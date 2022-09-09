dea6c8dec11a8c4f1a489e33741785e77d72aef3
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-09 12:01:02 +0530

    Time: 79 ms (14.92%), Space: 71.4 MB (82.29%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -8,83 +8,31 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-
-
-
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode*start=new ListNode();
-        ListNode*tmp=start;
-        
+        ListNode*dummy=new ListNode(0);
+        ListNode*tmp=dummy;
         int carry=0;
-        while(l1!=NULL || l2!=NULL || carry)
+        while(l1||l2||carry)
         {
             int sum=0;
-            if(l1!=NULL)
+            if(l1)
             {
                 sum+=l1->val;
                 l1=l1->next;
             }
-            if(l2!=NULL)
+            if(l2)
             {
                 sum+=l2->val;
                 l2=l2->next;
             }
             sum+=carry;
+            ListNode*head=new ListNode(sum%10);
+            tmp->next=head;
+            tmp=head;
             carry=sum/10;
-            sum%=10;
-            ListNode*n=new ListNode(sum);
-            tmp->next=n;
-            tmp=tmp->next;
         }
-        return start->next;
+        return dummy->next;
     }
 };
-
-
-
-
-
-
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//         ListNode *ptr = new ListNode();     //new list;
-//         ListNode *temp = ptr;
-        
-//         int c = 0;
-// 		//traversing both list till one of the list not reaches NULL
-//         while (l1 != NULL ||  l2 != NULL || c)
-//         {
-//             int sum = 0;
-// 			// if l1  is not null
-// 			// add l1-> value to sum
-//             if(l1 != NULL)
-//             {
-//                 sum += l1->val;
-//                 l1 = l1 -> next;
-//             }
-            
-// 			// if l2  is not null
-// 			// add l2-> value to sum
-//             if(l2 != NULL)
-//             {
-//                 sum += l2->val;
-//                 l2 = l2 -> next;
-//             }
-            
-// 			// add carry to sum
-//             sum += c;
-// 			// carry is updated by sum/10 because for 18 , 
-// 			// 18 / 10 is 1 which is the carry
-//             c = sum/10;
-// 			// add sum% 10 to new node as it containg the sum
-//             ListNode *node = new ListNode(sum%10);
-//             temp -> next = node;
-//             temp = temp -> next;
-//         }
-//         return ptr -> next;
-//     }
-// };
