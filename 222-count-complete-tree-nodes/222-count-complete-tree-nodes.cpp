5bc8dce92076ab03d8afb9e665e0e0e113fd15ae
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-13 21:23:18 +0530

    Time: 79 ms (6.78%), Space: 30.8 MB (93.23%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,88 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if (!root) return 0;
-        int lh=0,rh=0;
-        TreeNode*curr=root;
-        while(curr)
-        {
-            lh++;
-            curr=curr->left;
-        }
-        curr=root;
-        while(curr)
-        {
-            rh++;
-            curr=curr->right;
-        }
-        
-        if (lh == rh) return pow(2, lh) - 1; // for complete binary tree no of node=2^h-1
-        return 1 + countNodes(root->left) + countNodes(root->right);
+        return root == NULL ? 0 : countNodes(root->left) + countNodes(root->right) + 1;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     int countNodes(TreeNode* root) {
-//         if (!root) return 0;
-//         int lh=leftheight(root->left);
-//         int rh=rightheight(root->right);
-//         return 1+countNodes(root->left)+countNodes(root->right);
-//     }
-    
-//     int leftheight(TreeNode*root)
-//     {
-//         int x=1;
-//         while(root){
-//             x++;
-//           root=root->left;
-//         }
-//         return x;
-//     }
-//     int rightheight(TreeNode*root)
-//     {
-//         int x=1;
-//         while(root){
-//             x++;
-//             root=root->right;
-//         }
-//         return x;
-//     }
-// };
-
-
-
-
-// class Solution {
-// public:
-//     int countNodes(TreeNode* root) {
-//         if (!root) return 0;
-//         return 1+countNodes(root->left)+countNodes(root->right);
-//     }
-// };
-
-
-
-
-// class Solution {
-// public:
-//     int countNodes(TreeNode* root) {
-//         if (!root) return 0;
-//         int lh=height(root->left);
-//         int rh=height(root->right);
-//         return 1+countNodes(root->left)+countNodes(root->right);
-//     }
-//     int height(TreeNode*root)
-//     {
-//         if(!root) return 0;
-//         int lh=height(root->left);
-//         int rh=height(root->right);
-//         return 1+max(lh,rh);
-//     }
-// };
