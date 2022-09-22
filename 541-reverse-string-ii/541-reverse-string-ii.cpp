20c7cbfaff8e4b51f8d6e182d9ac4bd765947e61
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-22 15:31:47 +0530

    Time: 12 ms (19.03%), Space: 7.2 MB (70.10%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.cpp
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -1,11 +1,49 @@
+// class Solution {
+// public:
+//     string reverseStr(string s, int k) {
+//         int n=s.size();
+//         string ans="";
+//         int l=0;
+//         while(l<n) {
+//             string st="";
+//             for(int i=0;i<k;i++) {
+//                 if(l==n) {
+//                     break;
+//                 }
+//                 st+=s[l];
+//                 l+=1;
+//             }
+//             cout<<st<<"  "<<l<<endl;
+//             reverse(st.begin(),st.end());
+//             ans+=st;
+//             for(int j=l;j<min(l+k,n);j++) {
+//                 ans+=s[l];
+//                 l++;
+//             }
+//             cout<<l<<endl;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string reverseStr(string s, int k) {
+        int n=s.size();
         int i=0;
-        while(i<s.size())
+        while( i<s.size())
         {
-            reverse(s.begin()+i,min(s.begin()+i+k,s.end()));
+            if(i+k<n)
+            {
+                 reverse(s.begin()+i,s.begin()+i+k);
+            }
+            else 
+            {
+                reverse(s.begin()+i,s.end());
+            }
+            // reverse(s.begin()+i,s.begin()+i+k);
             i+=2*k;
+            
         }
         return s;
     }
