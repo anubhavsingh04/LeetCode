942af6c1e8025c9a8f8ab033901c542ad77ccb7a
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-24 20:48:16 +0530

    Time: 12 ms (98.91%), Space: 19.4 MB (33.89%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -12,19 +12,25 @@
 class Solution {
 public:
     int ans=0;
-    void dfs(TreeNode*root,int &target,long  long sum)
+    unordered_map<long long,int>m;
+    void dfs(TreeNode*root,int &target,long long sum)
     {
         if(!root) return;
         sum+=root->val;
         if(sum==target) ans++;
+        if(m.find(sum-target)!=m.end())
+        {
+            ans+=m[sum-target];
+        }
+        m[sum]++;
         dfs(root->left,target,sum);
         dfs(root->right,target,sum);
+        m[sum]--;
+        
     }
     int pathSum(TreeNode* root, int target) {
         if(!root) return 0;
         dfs(root,target,0LL);
-        pathSum(root->left,target);
-        pathSum(root->right,target);
         return ans;
     }
 };
