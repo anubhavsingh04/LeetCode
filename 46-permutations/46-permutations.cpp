e17c1d78495fa10bdd1767741012d5d93968c430
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-09-30 00:46:12 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (91.71%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,23 @@
-// let say abc is the string /array take 
-// swap a with a then a with b then a with c 
-// after this we will get three string abc,bac,cba and index will be increased by one
-// let say for string bac we are returning then we need to again swap b with a 
-// so that abc will be returned and then further call for swapping of c will be done 
+// swap with itsef or swap with next elements and increment the idx 
 class Solution {
 public:
-    void solve(vector<int>nums,int idx,vector<vector<int>>&ans)
+    void permutation(int idx,vector<int>&nums,int n,vector<vector<int>>&ans)
     {
-        if(idx>=nums.size())
-        {
+        if(idx==n-1){
             ans.push_back(nums);
             return;
         }
-        for(int j=idx;j<nums.size();j++)
+        for(int i=idx;i<n;i++)
         {
-            swap(nums[j],nums[idx]);
-            solve(nums,idx+1,ans);
-            swap(nums[j],nums[idx]);
+            swap(nums[idx],nums[i]);
+            permutation(idx+1,nums,n,ans);
+            swap(nums[idx],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
         int idx=0;
-        solve(nums,idx,ans);
+        permutation(idx,nums,nums.size(),ans);
         return ans;
     }
 };
