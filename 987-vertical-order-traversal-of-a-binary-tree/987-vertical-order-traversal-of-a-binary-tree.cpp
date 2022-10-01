8495963454fe75880a11337b95ded47d10e14c0c
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-01 15:05:42 +0530

    Time: 14 ms (21.97%), Space: 11.9 MB (92.99%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,30 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>>ans;
-        map<int,vector<int>>m; // contains elemets in sorted order y,vector of nodes 
-        queue<pair<int,TreeNode*>>q; // y,node
+        map<int,vector<int>>m; // y, node values in sorted order 
+        queue<pair<int,TreeNode*>>q; // y,Node
         q.push({0,root});
         while(!q.empty())
         {
-            int sz=q.size();
-            vector<pair<int,int>>v; // nodevalue ,y
-            for(int i=0;i<sz;i++)
+            int size=q.size();
+            vector<pair<int,int>>v; // nodevalue , y (for the purpose of pushing in map first sort then push)
+            for(int i=0;i<size;i++)
             {
-                auto curr=q.front();
+                auto curr=q.front(); 
                 q.pop();
                 int y=curr.first;
-                TreeNode* node=curr.second;
+                TreeNode*node=curr.second;
                 v.push_back({node->val,y});
-                if(node->left)
-                    q.push({y-1,node->left});
-                if(node->right)
-                    q.push({y+1,node->right});
+                if(node->left) q.push({y-1,node->left});
+                if(node->right) q.push({y+1,node->right});
             }
             sort(v.begin(),v.end());
             for(auto i:v)
@@ -40,6 +35,7 @@ public:
                 m[i.second].push_back(i.first);
             }
         }
+        vector<vector<int>>ans;
         for(auto i:m)
         {
             ans.push_back(i.second);
@@ -47,4 +43,3 @@ public:
         return ans;
     }
 };
-
