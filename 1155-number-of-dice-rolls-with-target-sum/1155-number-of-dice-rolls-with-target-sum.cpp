63c915ad4c70776a070e270eaa2761ea98a85eea
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-02 21:28:16 +0530

    Time: 56 ms (44.59%), Space: 8.5 MB (28.55%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    #define MOD 1000000007
-    int dp[32][1002];
-    
-    int solve(int d, int f, int target){
-        if(d==0 && target==0) return 1;
-        if(d<=0 || target<=0) return 0;
-        if(dp[d][target] != -1) return dp[d][target];
-     
-        int sum=0;
-        for(int i=1;i<=f;i++){
-            sum = (sum % MOD + solve(d-1,f,target-i)%MOD)%MOD;
+    #define mod 1000000007 
+    int nways(int dice,int face,int target,vector<vector<int>>&dp)
+    {
+        if(target<0) return 0;
+        if(dice==0 && target==0) return 1;
+        if(dice==0 && target!=0) return 0;
+        if(dice!=0 && target==0) return 0;
+        
+        // if(dice==0 && target==0) return 1;
+        // if(dice<0 || target<0) return 0;
+        
+        if(dp[dice][target]!=-1) return dp[dice][target];
+        
+        int ans=0;
+        for(int i=1;i<=face;i++)
+        {
+            ans=(ans%mod + nways(dice-1,face,target-i,dp)%mod)%mod;
         }
-        dp[d][target] = sum;
-        return dp[d][target];
+        return dp[dice][target]=ans;
     }
-    
-    int numRollsToTarget(int d, int f, int target) {
-        memset(dp,-1,sizeof(dp));
-        return solve(d,f,target);
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        return nways(n,k,target,dp);
     }
 };
