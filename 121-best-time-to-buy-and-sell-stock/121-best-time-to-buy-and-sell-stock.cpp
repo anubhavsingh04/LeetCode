904c6065850703b7437cccd8f1c4370e316afc29
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-03 23:09:30 +0530

    Time: 380 ms (6.22%), Space: 93.4 MB (50.90%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,42 +1,15 @@
-// Asked in google interview (imp)
-
-
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
+    int maxProfit(vector<int>& nums) {
         int maxprofit=0;
+        int n=nums.size();
         int mini=INT_MAX;
         for(int i=0;i<n;i++)
         {
-           mini=min(mini,prices[i]);
-            if(prices[i]>mini)
-            {
-                maxprofit=max(maxprofit,prices[i]-mini);
-            }
+            mini=min(mini,nums[i]);
+            if(nums[i]>mini)
+                maxprofit=max(maxprofit,nums[i]-mini);
         }
         return maxprofit;
     }
 };
-
-
-
-// Naive approach TLE
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int n=prices.size();
-//         int maxprofit=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             for(int j=i+1;j<n;j++)
-//             {
-//                 if(prices[j]>prices[i])
-//                 {
-//                     maxprofit=max(maxprofit,prices[j]-prices[i]);
-//                 }
-//             }
-//         }
-//         return maxprofit;
-//     }
-// };
