4dece7f8e27e8bb948b81daf9c60e8725319348d
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-09 13:56:03 +0530

    Time: 96 ms (24.34%), Space: 39 MB (24.07%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,51 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// using set if(k-root->val) exist in set then we will have our two sum
 class Solution {
 public:
-   unordered_set<int>s;
-    bool findTarget(TreeNode* root, int k) {
+    bool dfs(TreeNode*root,int k,map<int,int>&m)
+    {
         if(!root) return false;
-        if(s.find(k-root->val)!=s.end())
+        if(m.find(k-root->val)!=m.end())
+        {
             return true;
-        s.insert(root->val);
-        return findTarget(root->left,k)||findTarget(root->right,k);
+        }
+        m[root->val]++;
+        return dfs(root->left,k,m)||dfs(root->right,k,m);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        map<int,int>m;
+        return dfs(root,k,m);
     }
 };
-
-
-
-// using inorder traversal 
-// store the value int the vector which will be sorted in case of bst
-// then just apply two pointer like two sum problem 
-
-// class Solution {
-// public:
-//     vector<int>v;
-//     void inorder(TreeNode*root)
-//     {
-//         if(!root) return;
-//         inorder(root->left);
-//         v.push_back(root->val);
-//         inorder(root->right);
-//     }
-//     bool findTarget(TreeNode* root, int k) {
-//         inorder(root);
-//         int i=0,j=v.size()-1;
-//         while(i<j)
-//         {
-//             if((v[i]+v[j]) == k)
-//                 return true;
-//             else if((v[i]+v[j])>k)
-//                 j--;
-//             else
-//                 i++;
-//         }
-//         return false;
-//     }
-// };
-
-
-
-//  using bst iterator
