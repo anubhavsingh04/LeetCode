f0e748266c19065b629c4834bbbc4ed6e2cf959b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-13 00:25:00 +0530

    Time: 12 ms (6.66%), Space: 5.9 MB (87.26%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int ans=0;
+        for(auto &i:s)
+        {
+            ans=ans*26+(i-'A'+1);
+        }
+        return ans;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int titleToNumber(string s) {
+//         int ans=0;
+//         unordered_map<char,int>m;
+//         char ch='A';
+//         for(int i=1;i<=26;i++)
+//         {
+//             m[ch]=i;
+//             ch++;
+//         }
+//         for(auto &i:s)
+//         {
+//             ans=ans*26+m[i];
+//         }
+//         return ans;
+//     }
+// };
