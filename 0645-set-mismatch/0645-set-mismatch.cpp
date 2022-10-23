c7f6f5a4616ae385669a9494d026d40c6418416b
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-23 20:04:50 +0530

    Time: 163 ms (16.08%), Space: 30.9 MB (30.01%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        unordered_set<int>st;
+        int n=nums.size();
+        vector<int>ans(2);
+        int sum=(n*(n+1))/2;
+        for(auto &i:nums)
+        {
+            sum-=i;
+            if(st.count(i)) ans[0]=i;
+            st.insert(i);
+        }
+        ans[1]=sum+ans[0];
+        return ans;
+    }
+};
