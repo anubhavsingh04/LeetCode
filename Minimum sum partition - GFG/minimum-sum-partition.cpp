f2c67dcc335bd7d54280d7a4652e1ab9fcd6c0ea
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-25 02:35:04 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -6,13 +6,25 @@ using namespace std;
 class Solution{
 
   public:
+//   NOTE : There's a problem in this i.e It will work only for +ve intgeres 
+// (i.e All values in the given array is +ve)
+
+// ## 1. Reason why tabulation failed for negative values  !
+
+// As we encounter the negative elements in the given array 
+// So there's a possibility that our Total Sum or Range becomes negavtive
+// As soon as our range becomes negative the tabulation method fails 
+// because it is impossible to create a 2D matrix of size less than 0 i.e NEGATIVE
+// Or There's a possibilty that our sum or range becomes 0 
+// in this case too it is impossible to create the 2D matrix.
+
 	int minDifference(int nums[], int n)  { 
 	   // int n=nums.size();
         // int sum=accumulate(nums.begin(),nums.end(),0);
         int sum=0;
         for(int i=0;i<n;i++)
         sum+=nums[i];
-        vector<vector<bool>>dp(n+1,vector<bool>(sum/2+1,0));
+        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,0));
 
         for(int i=0;i<n+1;i++)
         {
@@ -21,7 +33,7 @@ class Solution{
         
         for(int i=1;i<n+1;i++)
         {
-            for(int j=1;j<sum/2+1;j++)
+            for(int j=1;j<sum+1;j++)
             {
                 if(nums[i-1]<=j)
                 {
