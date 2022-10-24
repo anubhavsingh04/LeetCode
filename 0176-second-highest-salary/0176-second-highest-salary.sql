d1e702b09b5215da3ec1d4c4eeeb58adcc7d9204
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-25 00:34:10 +0530

    Time: 262 ms (68.59%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+# Note that instead of nesting for second, third, etc largest salary, we can find nth salary using general query like in MySQL: 
+
+# SELECT salary 
+# FROM employee 
+# ORDER BY salary desc limit n-1,1
+
+select(
+    select distinct salary from Employee order by salary desc limit 1 offset 1
+) as SecondHighestSalary;
