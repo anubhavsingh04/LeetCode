634165b72bfe8aa9a68008d5d15f1f656c9b7114
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-10-25 12:42:06 +0530

    Time: 34 ms (58.35%), Space: 12.5 MB (40.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -8,14 +8,18 @@ public:
         int sum=(total+target)/2;
         int n=nums.size();
         vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
+        // for(int i=0;i<n+1;i++)
+        // {
+        //     for(int j=0;j<sum+1;j++)
+        //     {
+        //         if(i==0 && j==0) dp[i][j]=1;
+        //         else if(j==0) dp[i][j]=1;
+        //         else if(i==0) dp[i][j]=0;
+        //     }
+        // }
         for(int i=0;i<n+1;i++)
         {
-            for(int j=0;j<sum+1;j++)
-            {
-                if(i==0 && j==0) dp[i][j]=1;
-                else if(j==0) dp[i][j]=1;
-                else if(i==0) dp[i][j]=0;
-            }
+            dp[i][0]=1;
         }
         for(int i=1;i<n+1;i++)
         {
