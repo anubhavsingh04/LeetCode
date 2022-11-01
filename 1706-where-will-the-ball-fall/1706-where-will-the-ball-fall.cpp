0d7b187f04b0cfeedf71a6ad62f20171a2659456
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-11-01 08:00:38 +0530

    Time: 19 ms (99.03%), Space: 13.3 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int rec(vector<vector<int>>&grid,int curr_row,int curr_col,int n,int m)
+    int rec(vector<vector<int>>&grid,int curr_row,int curr_col,int n,int m,int dp[101][101])
     {
         if(curr_row==n) return curr_col;
+        if(dp[curr_row][curr_col]!=0) {
+            return dp[curr_row][curr_col];
+        }
         if(grid[curr_row][curr_col]==1) // left to right
         {
             if(curr_col+1>=m || grid[curr_row][curr_col+1]==-1) return -1;
-            return rec(grid,curr_row+1,curr_col+1,n,m);
+            return dp[curr_row][curr_col]=rec(grid,curr_row+1,curr_col+1,n,m,dp);
         }
         
          if(grid[curr_row][curr_col]==-1) // right to left
         {
             if(curr_col-1<0 || grid[curr_row][curr_col-1]==1) return -1;
-            return rec(grid,curr_row+1,curr_col-1,n,m);
+            return dp[curr_row][curr_col]=rec(grid,curr_row+1,curr_col-1,n,m,dp);
         }
         return -1;
     }
@@ -20,9 +23,11 @@ public:
         int row=grid.size();
         int col=grid[0].size();
         vector<int>ans;
+        int dp[101][101];
+        memset(dp,0,sizeof(dp));
         for(int j=0;j<col;j++)
         {
-            int x=rec(grid,0,j,row,col);
+            int x=rec(grid,0,j,row,col,dp);
             ans.push_back(x);
         }
         return ans;
