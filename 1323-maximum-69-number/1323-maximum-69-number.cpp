a2810ba0cadffa6c354d5ed0d66273e2363ff541
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-11-07 16:27:33 +0530

    Time: 5 ms (15.47%), Space: 6 MB (17.66%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,15 +1,33 @@
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         string s=to_string(num);
+//         for(auto &i:s)
+//         {
+//             if(i=='6')
+//             {
+//                 i='9';
+//                 break;
+//             }
+//         }
+//         return stoi(s);
+//     }
+// };
+
+// 9669 + 300=9969
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s=to_string(num);
-        for(auto &i:s)
+        int tmp=num;
+        int digit=0,right6posn=-1;
+        while(tmp)
         {
-            if(i=='6')
-            {
-                i='9';
-                break;
-            }
+            int rem=tmp%10;
+            if(rem==6) right6posn=digit;
+            digit++;
+            tmp/=10;
         }
-        return stoi(s);
+        if(right6posn==-1) return num;
+        else return num+3*pow(10,right6posn);
     }
 };
