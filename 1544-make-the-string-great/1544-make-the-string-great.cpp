ab921e0efedfad3c10ce08392e2dba3fb62a16f3
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-11-08 13:41:47 +0530

    Time: 11 ms (14.85%), Space: 6.6 MB (26.42%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
     string makeGood(string s) {
-        for(int i=s.size()-2;i>=0;i--){
-            if(abs(s[i]-s[i+1])==32)s.erase(i,2);
+        stack<char>st;
+        st.push(s[0]);
+        for(int i=1;i<s.size();i++)
+        {
+            
+            if(not st.empty() and abs(s[i]-st.top())==32)
+            {
+                st.pop();
+            }
+            else 
+            {
+                st.push(s[i]);
+            }
         }
-        return s;
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
