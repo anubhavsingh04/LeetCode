43f57b03c2013ca8f5b2caaaa8a29f698fa97ebb
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-01-16 10:24:55 +0530

    Time: 3 ms (93.93%), Space: 9.2 MB (6.62%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -30,38 +30,38 @@
 // create a map to store element of array
 // check two conditon if(nums[i]+diff) is in map and nums[i]+2*diff is in map
 
-class Solution {
-public:
-    int arithmeticTriplets(vector<int>& nums, int diff) {
-        int ans=0;
-        map<int,int>m;
-        for(int i=0;i<nums.size();i++)
-        {
-            m[nums[i]]++;
-        }
-        for(auto &i:nums)
-        {
-            if(m[i+diff] && m[i+2*diff])
-                ans++;
-        }
-        return ans;
-    }
-};
-
-
-
-// O(n) Using sets 
 // class Solution {
 // public:
 //     int arithmeticTriplets(vector<int>& nums, int diff) {
 //         int ans=0;
-//         set<int>s(nums.begin(),nums.end());
+//         map<int,int>m;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             m[nums[i]]++;
+//         }
 //         for(auto &i:nums)
 //         {
-//             if(s.find(i-diff)!=s.end() && s.find(i-2*diff)!=s.end())
+//             if(m[i+diff] && m[i+2*diff])
 //                 ans++;
 //         }
 //         return ans;
 //     }
 // };
+
+
+
+// O(n) Using sets 
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int ans=0;
+        set<int>s(nums.begin(),nums.end());
+        for(auto &i:nums)
+        {
+            if(s.find(i+diff)!=s.end() && s.find(i+2*diff)!=s.end())
+                ans++;
+        }
+        return ans;
+    }
+};
 
