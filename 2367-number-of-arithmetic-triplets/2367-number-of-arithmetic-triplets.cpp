1150a669b550d19cc3f282dbf91fe3c9bbe4eaf1
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-01-16 10:54:53 +0530

    Time: 4 ms (85.06%), Space: 9.2 MB (18.02%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -49,19 +49,41 @@
 // };
 
 
-
-// O(n) Using sets 
+// single pass if there is some number x for which x-diff and x-2*diff is present in map then there will always be a triplet let see how 
+//  we can write the given equation as nums[j]=nums[k]-diff and nums[i]=nums[k]-2*diff
+// so if we can somehow find nums[k]-diff and nums[k]-2*diff this means we have found nums[i] and nums[j] which is required triplet for some number nums[k]
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         int ans=0;
-        set<int>s(nums.begin(),nums.end());
+        map<int,int>m;
+        
         for(auto &i:nums)
         {
-            if(s.find(i+diff)!=s.end() && s.find(i+2*diff)!=s.end())
+            if(i-diff>=0 && m[i-diff] && m[i-2*diff])
                 ans++;
+            m[i]++;
         }
+        
         return ans;
     }
 };
+
+
+
+// O(n) Using sets 
+// class Solution {
+// public:
+//     int arithmeticTriplets(vector<int>& nums, int diff) {
+//         int ans=0;
+//         set<int>s(nums.begin(),nums.end());
+//         for(auto &i:nums)
+//         {
+//             if(s.find(i+diff)!=s.end() && s.find(i+2*diff)!=s.end())
+//                 ans++;
+//         }
+//         return ans;
+//     }
+// };
+
 
