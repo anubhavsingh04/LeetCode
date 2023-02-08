7af4b3ac0772b7a6ee4ba0383ad857d2b498f94c
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-02-09 00:04:14 +0530

    Time: 453 ms (26.63%), Space: 18 MB (12.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int farthest=0,curr=0,n=nums.size(),ans=0;
-        for(int i=0;i<n-1;i++) {
-            farthest=max(farthest,nums[i]+i);
-            if(i==curr) {
-                curr=farthest;
-                ans++;
-            }
+    long long rec(int curr, vector<int> &nums, int n,vector<int>&dp){
+        if(curr>=n-1){
+            return 0;
         }
-        return ans;
+        if(dp[curr]!=-1) return dp[curr];
+        long long ans=INT_MAX;
+        for(int i=1; i<=nums[curr]; i++){
+            ans=min(ans, 1+rec(curr+i,nums,n,dp));
+        }
+        return dp[curr]=ans;
+    }
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int curr=0;
+        vector<int>dp(n+1,-1);
+        return rec(0,nums,n,dp);
     }
 };
