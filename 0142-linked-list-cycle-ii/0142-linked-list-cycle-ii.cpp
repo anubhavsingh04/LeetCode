2f89e960358e540f9bc5121f475d84c3c4d6de02
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-02-15 03:19:10 +0530

    Time: 7 ms (88.05%), Space: 7.8 MB (28.82%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -19,12 +19,36 @@ public:
         }
         if(fast==NULL || fast->next==NULL) // agr loop nahi hua to
             return NULL;
-        // if(slow!=fast) return NULL;
-        while(head!=slow)
+        slow=head;
+        while(fast!=slow)
         {
-            head=head->next;
+            fast=fast->next;
             slow=slow->next;
         }
-        return head;
+        return slow;
     }
 };
+
+// loop removal
+// void removeLoop(Node* head)
+//     {
+//         if(!head) return;
+//         Node*slow=head,*fast=head;
+//         while(fast && fast->next)
+//         {
+//             slow=slow->next;
+//             fast=fast->next->next;
+//             if(slow==fast) break;
+//         }
+//         if(fast==NULL || fast->next==NULL) return; // means loop nahi hai
+//         slow=head;
+//         while(slow!=fast)
+//         {
+//             slow=slow->next;
+//             fast=fast->next;
+//         }
+//         // slow vali val pe loop hai
+//         Node*prev=slow->next;
+//         while(prev->next!=slow) prev=prev->next;
+//         prev->next=NULL;
+//     }
