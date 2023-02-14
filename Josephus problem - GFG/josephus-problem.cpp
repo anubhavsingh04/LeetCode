e2dd3e9cafe35db65e350252aef55f0d4022d1a9
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-02-15 01:58:53 +0530

    Added solution - LeetHub

--- Josephus problem - GFG/josephus-problem.cpp
+++ Josephus problem - GFG/josephus-problem.cpp
@@ -10,9 +10,19 @@ using namespace std;
 class Solution
 {
     public:
+    int rec(int idx,int k,vector<int>&v)
+    {
+        if(v.size()<=1) return v[0];
+        idx=(idx+k)%v.size();
+        v.erase(v.begin()+idx);
+        return rec(idx,k,v);
+    }
     int josephus(int n, int k)
     {
-        
+        vector<int>v;
+        for(int i=1;i<=n;i++) v.push_back(i);
+        k--;
+        return rec(0,k,v);
     }
 };
 
