97a85159db9aa8c834868e8fb67a34429beed4ca
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-03-03 21:34:47 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (29.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,44 +1,46 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        if(needle.size()==0)
-         return 0;
-        if(haystack.size()<needle.size())
-          return -1;  
-        int n=needle.size();
-        string str;
-        for(int i=0;i<=haystack.size()-n;i++)
-        {
-            str=haystack.substr(i,n);
-            if(str==needle)
-             return i;   
-        }
-        return -1;
-    }
-};
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         if(needle.size()==0)
+//          return 0;
+//         if(haystack.size()<needle.size())
+//           return -1;  
+//         int n=needle.size();
+//         string str;
+//         for(int i=0;i<=haystack.size()-n;i++)
+//         {
+//             str=haystack.substr(i,n);
+//             if(str==needle)
+//              return i;   
+//         }
+//         return -1;
+//     }
+// };
 
 // TWO POINTER 
 
-// int strStr(string haystack, string needle) 
-//     {
-//         if(needle=="")
-//             return 0;
+class Solution{
+public:
+int strStr(string haystack, string needle) 
+    {
+        if(needle=="")
+            return 0;
         
-//         int i=0,j=0;
-//         while(i<haystack.size() && j<needle.size())
-//         {
-//             if(haystack[i]==needle[j])
-//             {
-//                 i++;
-//                 j++;
-//                 if(j==needle.size()) 
-//                     return i-needle.size();
-//             }
-//             else
-//             { 
-//                 i=i-j+1;  
-//                 j=0; 
-//             }            
-//         }        
-//         return -1;
-//     }
+        int i=0,j=0;
+        while(i<haystack.size() && j<needle.size())
+        {
+            if(haystack[i]==needle[j])
+            {
+                i++;
+                j++;
+                if(j==needle.size()) return i-needle.size();
+            }
+            else
+            { 
+                i=(i+1-j);
+                j=0;
+            }            
+        }        
+        return -1;
+    }
+};
