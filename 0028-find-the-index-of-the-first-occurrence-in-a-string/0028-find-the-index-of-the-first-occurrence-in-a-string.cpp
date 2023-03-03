0dd56602544a1658cac6f052afa2942b0c5673a9
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-03-03 21:36:01 +0530

    Time: 4 ms (38.84%), Space: 6.3 MB (69.99%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -19,28 +19,38 @@
 
 // TWO POINTER 
 
+// class Solution{
+// public:
+// int strStr(string haystack, string needle) 
+//     {
+//         if(needle=="")
+//             return 0;
+        
+//         int i=0,j=0;
+//         while(i<haystack.size() && j<needle.size())
+//         {
+//             if(haystack[i]==needle[j])
+//             {
+//                 i++;
+//                 j++;
+//                 if(j==needle.size()) return i-needle.size();
+//             }
+//             else
+//             { 
+//                 i=(i+1-j);
+//                 j=0;
+//             }            
+//         }        
+//         return -1;
+//     }
+// };
+
+// using c++ stl 
+
 class Solution{
 public:
 int strStr(string haystack, string needle) 
     {
-        if(needle=="")
-            return 0;
-        
-        int i=0,j=0;
-        while(i<haystack.size() && j<needle.size())
-        {
-            if(haystack[i]==needle[j])
-            {
-                i++;
-                j++;
-                if(j==needle.size()) return i-needle.size();
-            }
-            else
-            { 
-                i=(i+1-j);
-                j=0;
-            }            
-        }        
-        return -1;
+        return haystack.find(needle);
     }
 };
