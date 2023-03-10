ddf28d15c128f24ee274a4ea850c3f97a16ded28
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-03-10 23:21:51 +0530

    Added solution - LeetHub

---
+++ Find the closest number - GFG/find-the-closest-number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int findClosest(int arr[], int n, int target) 
+    { 
+        int start=0,end=n-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target) return target;
+            else if(arr[mid]<target) start=mid+1;
+            else end=mid-1;
+        }
+        if(start>=0 && start<n && abs(arr[start]-target)<abs(arr[end]-target)) return arr[start];
+        else if(end>=0 && end<n && abs(arr[start]-target)>abs(arr[end]-target)) return arr[end];
+        else return max(arr[start],arr[end]);
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,target;
+        cin>>n>>target;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.findClosest(arr,n,target)<<endl;
+    }
+}
+
+// } Driver Code Ends
