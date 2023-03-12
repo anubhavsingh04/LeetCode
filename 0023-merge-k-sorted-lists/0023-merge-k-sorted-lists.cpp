a6c8b07ea801bc2b583c66ee3562e3984f262c4b
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-03-12 19:13:07 +0530

    Time: 268 ms (14.00%), Space: 13.3 MB (74.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -12,26 +12,16 @@ class Solution {
 public:
     ListNode* mergetwo(ListNode*l1,ListNode* l2)
     {
-        ListNode*dummy=new ListNode(0);
-        ListNode*ptr=dummy;
-        
-        while(l1 && l2)
-        {
-            if(l1->val<l2->val)
-            {
-                ptr->next=l1;
-                ptr=l1;
-                l1=l1->next;
-            }
-            else {
-                ptr->next=l2;
-                ptr=l2;
-                l2=l2->next;
-            }
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(l1->val<l2->val) {
+            l1->next=mergetwo(l1->next,l2);
+            return l1;
         }
-        if(l1) ptr->next=l1;
-        if(l2) ptr->next=l2;
-        return dummy->next;
+        else {
+            l2->next=mergetwo(l1,l2->next);
+            return l2;
+        }
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode*dummy=new ListNode(0);
