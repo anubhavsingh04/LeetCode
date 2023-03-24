2f47da335e0b536efd43cc5dcd523e543f6e3947
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-03-24 09:23:22 +0530

    Time: 352 ms (88.25%), Space: 107.2 MB (72.64%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
+    void dfs(int i,vector<int>&vis,vector<vector<int>>& adj,int &ans){
+        vis[i]=1;
+        for(auto x:adj[i]){
+            if(!vis[abs(x)]){
+                if(x>0)
+                ans++;
+                dfs(abs(x),vis,adj,ans);
+            }
+        }
+    }
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<int>>adj(n),adj2(n);
+        vector<vector<int>>adj(n);
         for(auto &i:connections){
             adj[i[0]].push_back(i[1]);
-            adj2[i[1]].push_back(i[0]);
+            adj[i[1]].push_back(-i[0]);
         }
-        queue<int>q;
-        q.push(0);
         vector<int>vis(n,0);
         int ans=0;
-        while(!q.empty())
-        {
-            int curr=q.front();
-            q.pop();
-            vis[curr]=1;
-            for(auto &i:adj[curr]){
-                if(!vis[i]){
-                    ans++;
-                    q.push(i);
-                }
-            }
-            for(auto &i:adj2[curr]){ // edges which are already towards 0 
-                if(!vis[i]){
-                    q.push(i);
-                }
-            }
-        }
+        dfs(0,vis,adj,ans);
         return ans;
     }
 };
