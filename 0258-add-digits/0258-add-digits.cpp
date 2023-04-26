d21eb496e79d6145767f34a1446359b27e8ac4be
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-04-26 17:05:12 +0530

    Time: 7 ms (16.24%), Space: 6 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,17 +1,28 @@
+// class Solution {
+// public:
+//     int rec(int num)
+//     {
+//         if(num<10) return num;
+//         int sum=0;
+//         while(num)
+//         {
+//             sum+=num%10;
+//             num/=10;
+//         }
+//         return rec(sum);
+//     }
+//     int addDigits(int num) {
+//         return rec(num);
+//     }
+// };
+
+// Check if the number is 0 or not . If num==0 , return 0. Else do the following.
+// Check if (num%9)==0 . If it is true , return 9. Else return (num%9).
+
+
 class Solution {
 public:
-    int rec(int num)
-    {
-        if(num<10) return num;
-        int sum=0;
-        while(num)
-        {
-            sum+=num%10;
-            num/=10;
-        }
-        return rec(sum);
-    }
     int addDigits(int num) {
-        return rec(num);
+        return (num%9==0)?(num!=0?9:0):num%9;
     }
 };
