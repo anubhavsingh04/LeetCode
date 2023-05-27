304353211a022e2816bc1cdb01e2103ef7e20cab
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-05-27 17:10:13 +0530

    Time: 348 ms (20.38%), Space: 148.1 MB (15.88%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -4,11 +4,13 @@ public:
     {
         if(i>=stones.size()) return 0;
         if(dp[i]!=-1) return dp[i];
-        int ans=INT_MIN;
-        ans=max(ans,stones[i]-rec(i+1,stones,dp));
-        if(i+1<stones.size()) ans=max(ans,stones[i]+stones[i+1]-rec(i+2,stones,dp));
-        if(i+2<stones.size()) ans=max(ans,stones[i]+stones[i+1]+stones[i+2]-rec(i+3,stones,dp));
-        return dp[i]=ans;
+        
+        int take1=stones[i]-rec(i+1,stones,dp);
+        int take2=INT_MIN;
+        if(i+1<stones.size()) take2= stones[i]+stones[i+1]-rec(i+2,stones,dp);
+        int take3=INT_MIN;
+        if(i+2<stones.size()) take3=stones[i]+stones[i+1]+stones[i+2]-rec(i+3,stones,dp);
+        return dp[i]=max({take1,take2,take3});
     }
     string stoneGameIII(vector<int>& stoneValue) {
         vector<int>dp(stoneValue.size()+1,-1);
