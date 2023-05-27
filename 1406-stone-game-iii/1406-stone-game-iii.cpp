ff29c5ddf8ba3bc536093587c94864aa21487c48
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-05-27 17:15:48 +0530

    Time: 293 ms (37.44%), Space: 132.4 MB (61.61%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int rec(int i,vector<int>&stones,vector<int>&dp)
-    {
-        if(i>=stones.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        
-        int take1=stones[i]-rec(i+1,stones,dp);
-        int take2=INT_MIN;
-        if(i+1<stones.size()) take2= stones[i]+stones[i+1]-rec(i+2,stones,dp);
-        int take3=INT_MIN;
-        if(i+2<stones.size()) take3=stones[i]+stones[i+1]+stones[i+2]-rec(i+3,stones,dp);
-        return dp[i]=max({take1,take2,take3});
-    }
-    string stoneGameIII(vector<int>& stoneValue) {
-        vector<int>dp(stoneValue.size()+1,-1);
-        int diff=rec(0,stoneValue,dp);
+    string stoneGameIII(vector<int>& stones) {
+        vector<int>dp(stones.size()+1,0);
+        int n=stones.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            int take1=stones[i]-dp[i+1];
+            int take2=INT_MIN;
+            if(i+1<stones.size()) take2= stones[i]+stones[i+1]-dp[i+2];
+            int take3=INT_MIN;
+            if(i+2<stones.size()) take3=stones[i]+stones[i+1]+stones[i+2]-dp[i+3];
+            dp[i]=max({take1,take2,take3});
+        }
+        int diff=dp[0];
         if(diff>0) return "Alice";
         else if(diff<0) return "Bob";
         else return "Tie";
