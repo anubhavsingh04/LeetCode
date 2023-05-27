6ff20ac7b094f08efc8019c91c59af104d27eb29
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-05-27 17:18:58 +0530

    Time: 280 ms (46.45%), Space: 126.5 MB (93.84%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     string stoneGameIII(vector<int>& stones) {
-        vector<int>dp(stones.size()+1,0);
+        vector<int>dp(3,0);
         int n=stones.size();
         for(int i=n-1;i>=0;i--)
         {
-            int take1=stones[i]-dp[i+1];
+            int take1=stones[i]-dp[(i+1)%3];
             int take2=INT_MIN;
-            if(i+1<stones.size()) take2= stones[i]+stones[i+1]-dp[i+2];
+            if(i+1<stones.size()) take2= stones[i]+stones[i+1]-dp[(i+2)%3];
             int take3=INT_MIN;
-            if(i+2<stones.size()) take3=stones[i]+stones[i+1]+stones[i+2]-dp[i+3];
-            dp[i]=max({take1,take2,take3});
+            if(i+2<stones.size()) take3=stones[i]+stones[i+1]+stones[i+2]-dp[(i+3)%3];
+            dp[i%3]=max({take1,take2,take3});
         }
         int diff=dp[0];
         if(diff>0) return "Alice";
