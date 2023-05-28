b8f90146088a962f9b22dbce450b69dd9f7ef71f
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-05-28 20:24:39 +0530

    Time: 266 ms (11.02%), Space: 144.8 MB (15.11%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     bool ispalindrome(string s,int i,int j)
     {
-        while(i<j)
+        while(i<j) 
         {
             if(s[i++]!=s[j--]) return false;
         }
         return true;
     }
-    void rec(string s,int idx,vector<string>&op,vector<vector<string>>&ans)
+    
+    void rec(int idx,string s,vector<string>v,vector<vector<string>>&ans)
     {
-        if(idx>=s.size()) {
-            ans.push_back(op);
-            return;
+        if(idx>=s.size()){
+            ans.push_back(v);
+            return ;
         }
-        for(int i=idx;i<s.size();i++){
+        for(int i=idx;i<s.size();i++)
+        {
             if(ispalindrome(s,idx,i)){
-                op.push_back(s.substr(idx,i-idx+1));
-                rec(s,i+1,op,ans);
-                op.pop_back();
+                v.push_back(s.substr(idx,i-idx+1));
+                rec(i+1,s,v,ans);
+                v.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<string>op;
         vector<vector<string>>ans;
-        int idx=0;
-        rec(s,idx,op,ans);
+        vector<string>v;
+        rec(0,s,v,ans);
         return ans;
     }
 };
