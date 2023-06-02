3a129035ebd8666a515e851128994a2c8638a267
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-02 13:46:51 +0530

    Time: 125 ms (80.10%), Space: 42.2 MB (71.73%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,80 @@
 class Solution {
 public:
-    void dfs_traversal(vector<vector<int>>& graph , vector<bool>& visited , int start){
-        visited[start]=true;
-        for(auto it : graph[start]){
-            if(!visited[it]){
-                dfs_traversal(graph , visited , it );
-            }
+    void makeset(vector<int>&parent,vector<int>&rank,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+            rank[i]=0;
         }
     }
-    
+
+    int findparent(vector<int>&parent,int node)
+    {
+        if(parent[node] == node)
+        {
+            return node;
+        }
+        return parent[node]=findparent(parent,parent[node]);
+    }
+
+    void unionset(int u,int v,vector<int>&parent,vector<int>&rank)
+    {
+        int p1 = findparent(parent,u);
+        int p2 = findparent(parent,v);
+        if(p1==p2)
+        {
+            return;
+        }
+        if(rank[p1]<rank[p2])
+        {
+            parent[p1]=p2;
+        }
+        else if(rank[p2]<rank[p1])
+        {
+            parent[p2]=p1;
+        }
+        else 
+        {
+            parent[p2]=p1;
+            rank[p1]++;
+        }
+    }
+
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(n>connections.size()+1)
-            return -1;
-        
-        vector<vector<int>> graph(n);
-        for(auto con : connections){
-            graph[con[0]].push_back(con[1]);
-            graph[con[1]].push_back(con[0]);
-        }
-        vector<bool> visited(n ,false);
-        int component=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                component++;
-                dfs_traversal(graph , visited , i);
-            }
-        }
-        return component-1;
+       vector<int>parent(n);
+       vector<int>rank(n);
+       makeset(parent,rank,n);
+       int count=0;
+       for(auto it:connections)
+       {
+           int a = it[0];
+           int b = it[1];
+           if(findparent(parent,a) == findparent(parent,b))
+           {
+               // cout<<a<<' '<<b<<endl;
+                count++;
+           }
+           else
+           {
+               unionset(a,b,parent,rank);
+           }
+       }
+       int component=0;
+       for(int i=0;i<n;i++)
+       {
+           if(parent[i]==i)
+           {
+               // cout<<i<<' '<<endl;
+               component++;
+           }
+       }
+       int ans=component-1;
+        // cout<<count<<" "<<ans;
+      if(count>=ans)
+      {
+          return ans;
+      }
+      return -1;
     }
 };
