e03246328cda4061a2f40fc259f6f6a1038516d7
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-09 19:30:44 +0530

    Time: 11 ms (62.33%), Space: 10.1 MB (46.93%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int dfs(int n,int m,vector<vector<int>>&grid,vector<vector<int>>&dp)
+    int rec(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp)
     {
-        if(n<0 || m<0) return 1e8;
-        if(n==0 && m==0) return grid[n][m];
-        if(dp[n][m]!=-1) return dp[n][m];
-        int up=dfs(n-1,m,grid,dp);
-        int left=dfs(n,m-1,grid,dp);
-        return dp[n][m]=grid[n][m]+min(up,left);
+        if(i==0 && j==0) return grid[i][j];
+        if(i<0 || j<0) return 1e9;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=grid[i][j]+rec(i-1,j,grid,dp);
+        int left=grid[i][j]+rec(i,j-1,grid,dp);
+        return dp[i][j]=min(up,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return dfs(n-1,m-1,grid,dp);
+        return rec(n-1,m-1,grid,dp);
     }
 };
