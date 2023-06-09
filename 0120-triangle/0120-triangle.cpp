88183aae53d937d48f7e9b0bd9bd7470270ea7af
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-09 20:00:18 +0530

    Time: 12 ms (15.86%), Space: 9 MB (49.66%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -12,6 +12,19 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return rec(0,0,n,triangle,dp);
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(i==n-1) {
+                    dp[i][j]=triangle[i][j];
+                    continue;
+                }
+                int down=triangle[i][j]+dp[i+1][j];
+                int diagonal=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diagonal);
+            }
+        }
+        return dp[0][0];
     }
 };
