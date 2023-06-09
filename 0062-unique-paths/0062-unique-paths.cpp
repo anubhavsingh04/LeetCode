d321e10dddda3be65e530188c533337f7c567ab0
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-09 09:51:13 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.15%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,11 +6,26 @@ public:
         if(i<0||j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int up=rec(i-1,j,dp);
-        int down=rec(i,j-1,dp);
-        return dp[i][j]=up+down;
+        int left=rec(i,j-1,dp);
+        return dp[i][j]=up+left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n+1,-1));
-        return rec(m-1,n-1,dp);
+        vector<vector<int>>dp(m,vector<int>(n));
+        // return rec(m-1,n-1,dp);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int up=0,left=0;
+                if(i==0||j==0) {
+                    dp[i][j]=1;
+                    continue;
+                }
+                if(i>0)  up=dp[i-1][j];
+                if(j>0)  left=dp[i][j-1];
+                 dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
