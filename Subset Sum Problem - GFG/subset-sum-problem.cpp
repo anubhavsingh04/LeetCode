0c549200d1f301723b8acccd793d126663e4caf5
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-10 15:23:15 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,20 +9,22 @@ using namespace std;
 
 class Solution{   
 public:
-    bool rec(vector<int>&arr,int n,int sum,vector<vector<int>>&dp)
-    {
-        if(sum==0) return 1;
-        if(n==0) return arr[0]==sum;
-        if(dp[n][sum]!=-1) return dp[n][sum];
-        int notpick=rec(arr,n-1,sum,dp);
-        int pick=0;
-        if(arr[n]<=sum) pick=rec(arr,n-1,sum-arr[n],dp);
-        return dp[n][sum]=pick||notpick;
-    }
     bool isSubsetSum(vector<int>arr, int sum){
         int n=arr.size();
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return rec(arr,n-1,sum,dp);
+        vector<vector<int>>dp(n,vector<int>(sum+1,0));
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        if(arr[0]<=sum) dp[0][arr[0]]=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                int notpick=dp[i-1][j];
+                int pick=0;
+                if(arr[i]<=j) pick=dp[i-1][j-arr[i]];
+                dp[i][j]=pick||notpick;
+            }
+        }
+        return dp[n-1][sum];
     }
 };
 
