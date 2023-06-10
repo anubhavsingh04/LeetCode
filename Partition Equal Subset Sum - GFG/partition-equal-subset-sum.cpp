3c54c73ca677e97bd5d78aeffc564b86021f0610
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-10 15:53:18 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -12,30 +12,23 @@ public:
     int equalPartition(int n, int arr[])
     {
         int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=arr[i];
-        }
-        if(sum&1) return 0;
+        for(int i=0;i<n;i++) sum+=arr[i];
+        if(sum %2==1) return 0;
         sum/=2;
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
-        for(int i=0;i<n+1;i++)
+        vector<vector<int>>dp(n,vector<int>(sum+1,0));
+        for(int i=0;i<n;i++) dp[i][0]=1; // target is 0 
+        if(arr[0]<=sum) dp[0][arr[0]]=1;
+        for(int i=1;i<n;i++)
         {
-            dp[i][0]=1;
-        }
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<sum+1;j++)
+            for(int j=0;j<=sum;j++)
             {
-                if(arr[i-1]<=j)
-                {
-                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
-                }
-                else 
-                dp[i][j]=dp[i-1][j];
+                int notpick=dp[i-1][j];
+                int pick=0;
+                if(arr[i]<=j) pick=dp[i-1][j-arr[i]];
+                dp[i][j]=pick||notpick;
             }
         }
-        return dp[n][sum];
+        return dp[n-1][sum];
     }
 };
 
