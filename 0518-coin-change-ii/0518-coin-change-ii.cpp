3310c6e8ac4b2dc4a4f03a6c85b87f9ff49686a6
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-11 22:15:10 +0530

    Time: 8 ms (93.66%), Space: 18.5 MB (48.47%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int change(int target, vector<int>& coins) {
-       int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(target+1,0));
-        for(int i=0;i<n;i++)
+    int rec(int idx,int amount,vector<int>&coins,vector<vector<int>>&dp)
+    {
+        if(idx==0) return amount%coins[0]==0;
+        
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        int notpick=rec(idx-1,amount,coins,dp);
+        int pick=0;
+        if(coins[idx]<=amount)
         {
-            for(int j=0;j<=target;j++)
-            {
-                if(i==0){ // base case 
-                    if(j%coins[0]==0) dp[0][j]=1;
-                    else dp[0][j]=0;
-                    continue;
-                }
-                int notpick=dp[i-1][j];
-                int pick=0;
-                if(coins[i]<=j)
-                {
-                    pick=dp[i][j-coins[i]];
-                }
-                dp[i][j]=pick+notpick;
-            }
+            pick=rec(idx,amount-coins[idx],coins,dp);
         }
-        return dp[n-1][target];
+        return dp[idx][amount]=(pick+notpick);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return rec(n-1,amount,coins,dp);
     }
 };
