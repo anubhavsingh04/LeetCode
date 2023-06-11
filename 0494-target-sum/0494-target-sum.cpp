12916b0c483125d7895a017a35e4d06a28e41ff4
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-11 21:32:37 +0530

    Time: 12 ms (81.42%), Space: 12.3 MB (36.90%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int rec(int i,vector<int>&nums,int target,vector<vector<int>>&dp)
     {
-        if(i==0) {
+        if(i==0)
+        {
             if(nums[0]==0 && target==0) return 2;
-            if(target==0 || nums[0]==target) return 1;
-            return 0;
+            if(target==0||nums[0]==target) return 1;
+            else return 0;
         }
         if(dp[i][target]!=-1)return dp[i][target];
         int notpick=rec(i-1,nums,target,dp);
@@ -17,7 +18,7 @@ public:
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        target=abs(target);
+        // target=abs(target);
         int total=accumulate(nums.begin(),nums.end(),0);
         if(total+target<0||(total+target)%2==1) return 0;
         target=(total+target)/2;
