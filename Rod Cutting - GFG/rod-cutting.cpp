e7325c6413369692e8cd54f6e655207ceab7157e
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-11 22:41:34 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,28 +10,24 @@ using namespace std;
 
 class Solution{
   public:
+    int rec(int i,int *price,int len,vector<vector<int>>&dp)
+    {
+        if(i==0){
+            return len*price[0];
+        }
+        if(dp[i][len]!=-1) return dp[i][len];
+        int notpick=rec(i-1,price,len,dp);
+        int pick=-1e9;
+        int rodlen=i+1;
+        if(len>=rodlen) // length badi honi chahiye cuttling lenth se tabhi kat payenge
+        {
+            pick=price[i]+rec(i,price,len-rodlen,dp);
+        }
+        return dp[i][len]=max(pick,notpick);
+    }
     int cutRod(int price[], int n) {
-        int len[n];
-        for(int i=1;i<=n;i++)
-        {
-            len[i-1]=i;
-        }
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i=1;i<n+1;i++) // len
-        {
-            for(int j=1;j<n+1;j++) // 
-            {
-                if(len[i-1]<=j)
-                {
-                    dp[i][j]=max(price[i-1]+dp[i][j-len[i-1]],dp[i-1][j]);
-                }
-                else 
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][n];
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return rec(n-1,price,n,dp);
     }
 };
 
