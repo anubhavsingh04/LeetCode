98d294b5b2e6c63eb88e95dd7dad5d1367cde390
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-12 19:32:58 +0530

    Time: 4 ms (34.63%), Space: 6.9 MB (13.50%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -3,35 +3,22 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         int n=nums.size();
         vector<string>ans;
-        if(n==0)  return ans;
-        string s=to_string(nums[0]);
-        int prev=nums[0];
-        for(int i=1;i<n;i++)
+        string s="";
+        for(int i=0;i<n;i++)
         {
-            if(prev+1!=nums[i])
-            {
-                if(to_string(prev)==s){
-                    ans.push_back(s);
-                    s=to_string(nums[i]); 
-                }  
-                else {
-                    s+="->";
-                    s+=to_string(prev);
-                    ans.push_back(s);
-                    s=to_string(nums[i]); 
-                }
+            int j=i;
+            while(j+1<n && nums[j+1]==nums[j]+1) {
+                j++;
             }
-            prev=nums[i];
-        }
-        if(to_string(prev)==s){
+            if(j>i){
+                s+=to_string(nums[i])+"->"+to_string(nums[j]);
+            }
+            else {
+                s+=to_string(nums[i]);
+            }
             ans.push_back(s);
-            s=to_string(nums[n-1]); 
-        }  
-        else {
-            s+="->";
-            s+=to_string(prev);
-            ans.push_back(s);
-            s=to_string(nums[n-1]); 
+            s="";
+            i=j;
         }
         return ans;
     }
