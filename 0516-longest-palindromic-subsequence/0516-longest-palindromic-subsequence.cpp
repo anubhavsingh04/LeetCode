40e8cd60abc505e48a5d3819cf96cec2a085c923
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-13 21:35:09 +0530

    Time: 332 ms (5.09%), Space: 73 MB (26.85%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int LCS(string &a,string &b)
+    int LCS(string s,string t)
     {
-      int m=a.size();
-      int n=b.size();
-      vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-      for(int i=1;i<m+1;i++)
-      {
-          for(int j=1;j<n+1;j++)
-          {
-              if(a[i-1]==b[j-1])
-              dp[i][j]=1+dp[i-1][j-1];
-              else 
-              dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-          }
-      }
-      return dp[m][n];
+        int n=s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][n];
     }
     int longestPalindromeSubseq(string s) {
-        // LPS of s=LCS(a,revres(a))
-        string s1=s;
-        reverse(s1.begin(),s1.end());
-        return LCS(s,s1);
+        string t=s;
+        reverse(t.begin(),t.end());
+        return LCS(s,t);
     }
 };
