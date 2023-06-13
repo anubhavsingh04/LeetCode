380bbf25b4cee0354ecd7145c8fa58681f6e6e8b
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-13 23:25:00 +0530

    Time: 81 ms (14.71%), Space: 81.5 MB (9.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,19 @@
 class Solution {
- public:
-  int minDistance(string word1, string word2) {
-    const int m = word1.length();//first word length
-    const int n = word2.length();//second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
-
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1[i - 1] == word2[j - 1])//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             //replace       //delete        //insert
-    return dp[m][n];
-  }
+public:
+    int rec(int i,int j,string s1,string s2,vector<vector<int>>&dp)
+    {
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j]=rec(i-1,j-1,s1,s2,dp);
+        else {
+            return dp[i][j]=1+min({rec(i-1,j-1,s1,s2,dp),rec(i-1,j,s1,s2,dp),rec(i,j-1,s1,s2,dp)}); 
+            // rep,del,insr
+        }
+    }
+    int minDistance(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return rec(n-1,m-1,s1,s2,dp);
+    }
 };
