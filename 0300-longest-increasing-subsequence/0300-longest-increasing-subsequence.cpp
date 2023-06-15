6537041c76d035a5ac57e6887714d9d29b2ee8b4
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-15 17:30:50 +0530

    Time: 1397 ms (18.38%), Space: 292.5 MB (7.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,20 @@
-// dont forgrt to intialize pick and not pick as 0
-// we can pick an element if prev==-1 or nums[curr]>nums[prev]
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int LIS(vector<int>&nums,int prev,int curr)
+    int rec(int prev,int curr,vector<int>&nums,vector<vector<int>>&dp)
     {
         if(curr>=nums.size()) return 0;
         if(dp[prev+1][curr]!=-1) return dp[prev+1][curr];
-        int pick=0,notpick=0;
-        if(prev==-1||nums[curr]>nums[prev]){
-            pick=1+LIS(nums,curr,curr+1);
+        int pick=0,notpick;
+        if(prev==-1||nums[curr]>nums[prev]) {
+            pick=1+rec(curr,curr+1,nums,dp);
         }
-        notpick=LIS(nums,prev,curr+1);
+        notpick= rec(prev,curr+1,nums,dp);
+        
         return dp[prev+1][curr]=max(pick,notpick);
     }
     int lengthOfLIS(vector<int>& nums) {
-        int curr=0,prev=-1;
         int n=nums.size();
-        dp.resize(n+1,vector<int>(n+1,-1));
-        return LIS(nums,prev,curr);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return rec(-1,0,nums,dp);
     }
 };
