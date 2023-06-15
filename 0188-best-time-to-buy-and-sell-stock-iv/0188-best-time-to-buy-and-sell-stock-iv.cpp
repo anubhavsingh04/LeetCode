ae6448254d08780b3295995e7d9c34b642cedb74
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-15 15:57:09 +0530

    Time: 40 ms (19.57%), Space: 14.4 MB (31.72%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
+    int rec(int i,int buy,int trans,vector<int>&prices,vector<vector<vector<int>>>&dp)
+    {
+        if(trans<=0) return 0;
+        if(i>=prices.size()) return 0;
+        
+        if(dp[i][buy][trans]!=-1) return dp[i][buy][trans];
+        if(buy)
+        {
+            return dp[i][buy][trans]=max(-prices[i]+rec(i+1,0,trans,prices,dp),
+                                         rec(i+1,1,trans,prices,dp));
+        }
+        else {
+            return dp[i][buy][trans]=max(prices[i]+rec(i+1,1,trans-1,prices,dp),
+                                         rec(i+1,0,trans,prices,dp));
+        }
+    }
     int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
-        int trans=k;
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(trans+1,0)));
-        for(int i=0;i<n;i++)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                dp[i][buy][0]=0;
-            }
-        }
-        for(int buy=0;buy<=1;buy++)
-        {
-            for(int trans=0;trans<=k;trans++)
-            {
-                dp[n][buy][trans]=0;
-            }
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int buy=0;buy<=1;buy++)
-            {
-                for(int trans=1;trans<=k;trans++)
-                {
-                    if(buy)
-                    {
-                        dp[i][buy][trans]=max(-prices[i]+dp[i+1][0][trans],
-                                                     dp[i+1][1][trans]);
-                    }
-                    else {
-                         dp[i][buy][trans]=max(prices[i]+dp[i+1][1][trans-1],
-                                                     dp[i+1][0][trans]);
-                    }
-                }
-            }
-        }
-        return dp[0][1][trans];
+        vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return rec(0,1,k,prices,dp); 
+        
     }
 };
