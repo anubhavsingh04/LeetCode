3d5c4fd299354ecac14a1d6a7f4baa0f8c6c97f4
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-17 20:33:50 +0530

    Time: 272 ms (11.84%), Space: 92.2 MB (16.03%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,113 +1,54 @@
 class Solution {
 public:
-    // vector<int> NSL(vector<int>&heights)
-    // {
-    //     int n=heights.size();
-    //     stack<pair<int,int>>st;
-    //     vector<int>ans;
-    //     for(int i=0;i<n;i++)
-    //     {
-    //         if(st.empty()) ans.push_back(-1);
-    //         else if(st.top().first<heights[i]) ans.push_back(st.top().second);
-    //         else if(st.top().first>=heights[i]){
-    //             while(!st.empty() && st.top().first>=heights[i]) st.pop();
-    //             if(st.empty()) ans.push_back(-1);
-    //             else  ans.push_back(st.top().second);
-    //         }
-    //         st.push({heights[i],i});
-    //     }
-    //     return ans;
-    // }
-     vector<int>NSL(vector<int>h)
+    vector<int> NSL(vector<int>&heights)
     {
-        vector<int>left;
+        int n=heights.size();
         stack<pair<int,int>>st;
-        int pseudo=-1; 
-        for(int i=0;i<h.size();i++)
+        vector<int>ans;
+        for(int i=0;i<n;i++)
         {
-            if(st.empty())
-               left.push_back(pseudo);
-            
-            else if(st.size()>0 && st.top().first<h[i])
-                left.push_back(st.top().second);
-            
-            else if(st.size()>0 && st.top().first>=h[i])
-            {
-                while(st.size()>0 && st.top().first>=h[i])
-                    st.pop();
-                if(st.empty())
-                    left.push_back(pseudo);
-                else 
-                    left.push_back(st.top().second);
+            if(st.empty()) ans.push_back(-1);
+            else if(st.top().first<heights[i]) ans.push_back(st.top().second);
+            else if(st.top().first>=heights[i]){
+                while(!st.empty() && st.top().first>=heights[i]) st.pop();
+                if(st.empty()) ans.push_back(-1);
+                else  ans.push_back(st.top().second);
             }
-            st.push({h[i],i});
+            st.push({heights[i],i});
         }
-        return left;
+        return ans;
     }
     
-    // vector<int> NSR(vector<int>&heights)
-    // {
-    //     int n=heights.size();
-    //     stack<pair<int,int>>st;
-    //     vector<int>ans;
-    //     for(int i=n-1;i>=0;i--)
-    //     {
-    //         if(st.empty()) ans.push_back(n);
-    //         else if(st.top().first<heights[i]) ans.push_back(st.top().second);
-    //         else if(st.top().first>=heights[i]){
-    //             while(!st.empty() && st.top().first>=heights[i]) st.pop();
-    //             if(st.empty()) ans.push_back(n);
-    //             else  ans.push_back(st.top().second);
-    //         }
-    //         st.push({heights[i],i});
-    //     }
-    //     reverse(ans.begin(),ans.end());
-    //     return ans;
-    // }
-    vector<int>NSR(vector<int>h)
+    vector<int> NSR(vector<int>&heights)
     {
-        vector<int>right;
+        int n=heights.size();
         stack<pair<int,int>>st;
-        int pseudo=h.size(); 
-        for(int i=h.size()-1;i>=0;i--)
+        vector<int>ans;
+        for(int i=n-1;i>=0;i--)
         {
-            if(st.empty())
-               right.push_back(pseudo);
-            else if(st.size()>0 && st.top().first<h[i])
-                right.push_back(st.top().second);
-            else if(st.size()>0 && st.top().first>=h[i])
-            {
-                while(st.size()>0 && st.top().first>=h[i])
-                    st.pop();
-                if(st.empty())
-                    right.push_back(pseudo);
-                else 
-                    right.push_back(st.top().second);
+            if(st.empty()) ans.push_back(n);
+            else if(st.top().first<heights[i]) ans.push_back(st.top().second);
+            else if(st.top().first>=heights[i]){
+                while(!st.empty() && st.top().first>=heights[i]) st.pop();
+                if(st.empty()) ans.push_back(n);
+                else  ans.push_back(st.top().second);
             }
-            st.push({h[i],i});
+            st.push({heights[i],i});
         }
-        reverse(right.begin(),right.end());
-        return right;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
     int largestRectangleArea(vector<int>& heights) {
-        // vector<int>left=NSL(heights);
-        // vector<int>right=NSR(heights);
-        // int maxarea=-1;
-        // for(int i=0;i<heights.size();i++)
-        // {  
-        //     cout<<left[i]<<" "<<right[i]<<endl;
-        //     int width=right[i]-left[i]-1;
-        //     maxarea=max(maxarea,width*heights[i]);
-        // }
-        // return maxarea;
         vector<int>left=NSL(heights);
         vector<int>right=NSR(heights);
         int maxarea=-1;
         for(int i=0;i<heights.size();i++)
-        {
+        {  
+            // cout<<left[i]<<" "<<right[i]<<endl;
             int width=right[i]-left[i]-1;
             maxarea=max(maxarea,width*heights[i]);
         }
         return maxarea;
     }
 };
+
