4c9923ae70f673135ad1912ef58eca6179972801
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-17 12:00:41 +0530

    Time: 8 ms (52.04%), Space: 9.1 MB (6.54%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -6,18 +6,23 @@ public:
         int n=nums2.size();
         for(int i=n-1;i>=0;i--)
         {
-            if(st.empty()) mp[nums2[i]]=-1;
+            if(st.empty()){
+                mp[nums2[i]]=-1;
+            }
             else if(st.top()<nums2[i]){
                 while(!st.empty() && st.top()<nums2[i]) st.pop();
                 if(st.empty()) mp[nums2[i]]=-1;
                 else mp[nums2[i]]=st.top();
             }
-            else if(st.top()>nums2[i]) mp[nums2[i]]=st.top();
-            
+            else if(st.top()>nums2[i]) {
+                mp[nums2[i]]=st.top();
+            }
             st.push(nums2[i]);
         }
         vector<int>ans;
-        for(auto &i:nums1) ans.push_back(mp[i]);
+        for(auto &i:nums1){
+            ans.push_back(mp[i]);
+        }
         return ans;
     }
 };
