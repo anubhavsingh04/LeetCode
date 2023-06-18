63257e22c6a902b467158796fa4087747eada3b5
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-18 21:33:04 +0530

    Time: 119 ms (76.86%), Space: 13.1 MB (91.78%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    int longestStrChain(vector<string>& words) {
-        vector<unordered_set<string>> W(17);
-        for (auto word : words) 
-            W[word.size()].insert(word);
-        unordered_map<string, int> dp;
-        int best = 1;
-        for (int i = 16; i; i--) {
-            if (W[i-1].empty()) continue;
-            for (auto word : W[i]) {
-                int wVal = dp[word] ? dp[word] : 1;
-                for (int j = 0; j < word.size(); j++) {
-                    string pred = word.substr(0,j) + word.substr(j+1);
-                    int pVal = dp[pred] ? dp[pred] : 1;
-                    if (W[i-1].find(pred) != W[i-1].end() && wVal >= pVal) {
-                        dp[pred] = wVal + 1;
-                        best = max(best, wVal + 1);
-                    }
-                }
+    bool isvalid(string &s1,string &s2)
+    {
+        if(s1.size()!=s2.size()+1) return false;
+        int i=0,j=0;
+        while(i<s1.size() ){
+            if(j<s2.size() && s1[i]==s2[j]) {
+                i++;
+                j++;
+            }
+            else {
+                i++; // i points to s1 which is larger 
             }
         }
-        return best;
+        if(i==s1.size() && j==s2.size()) return true;
+        return false;
+        
+    }
+    static bool cmp(string& s1, string& s2){
+        return s1.size() < s2.size();
+    }
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),cmp);
+        int n=words.size();
+        vector<int>dp(n,1);
+        int maxi=-1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(isvalid(words[i],words[j]) && 1+dp[j]>dp[i]) {
+                    dp[i]=1+dp[j];
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        return maxi;
     }
 };
