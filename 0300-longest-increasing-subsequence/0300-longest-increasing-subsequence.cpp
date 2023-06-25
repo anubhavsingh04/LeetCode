facc1cbb203335b7a7e0b87ef8805bb3e6f6dc76
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-25 17:44:57 +0530

    Time: 593 ms (35.83%), Space: 10.5 MB (65.83%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         vector<int>dp(n,1);
         int ans=1;
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<i;j++)
             {
