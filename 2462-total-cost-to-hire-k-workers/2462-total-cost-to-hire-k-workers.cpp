49a2b0971bfd973bfbdbe59c67c53c045db7d7d1
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-06-26 20:01:35 +0530

    Time: 190 ms (90.24%), Space: 76 MB (21.45%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int c) {
-        long long n=costs.size(),i=0,j=n-1,ans=0;
-
-        priority_queue<long long,vector<long long>,greater<long long>>pq1,pq2;
-        while(k--){
-            while(pq1.size()<c && i<=j) pq1.push(costs[i++]);
-            while(pq2.size()<c && j>=i) pq2.push(costs[j--]);
-            int a=pq1.size()>0?pq1.top():INT_MAX;
-            int b=pq2.size()>0?pq2.top():INT_MAX;
-            // cout<<a<<" "<<b<<"\n";
-            if(a<=b){
+        long long n=costs.size(),start=0,end=n-1,ans=0;
+        priority_queue<long long,vector<long long>,greater<long long>>pq,pq1;
+        while(k--)
+        {
+            while(pq.size()<c && start<=end) pq.push(costs[start++]);
+            while(pq1.size()<c && end>=start) pq1.push(costs[end--]);
+            int a=1e9,b=1e9;
+            if(pq.size()>0) a=pq.top();
+            if(pq1.size()>0) b=pq1.top();
+            if(a<=b) {
                 ans+=a;
-                pq1.pop();
-            }else{
-                ans+=b;
-                pq2.pop();
+                pq.pop();
             }
-            // cnt++;
+            else {
+                ans+=b;
+                pq1.pop();
+            }
         }
         return ans;
     }
