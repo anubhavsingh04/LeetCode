586165bdbc85c47c756afd1df6474ab04df155a1
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-07-06 09:28:12 +0530

    Time: 381 ms (81.69%), Space: 10.5 MB (7.81%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int rec(int i,int j,vector<int>&nums,vector<vector<int>>&dp)
-    {
-        if(i>j) return 0;
-        long long ans=INT_MIN;
-        if(dp[i][j]!=-1) return dp[i][j];
-        for(int k=i;k<=j;k++)
-        {
-            long long cost=nums[k]*nums[i-1]*nums[j+1]+rec(i,k-1,nums,dp)+rec(k+1,j,nums,dp);
-            ans=max(ans,cost);
-        }
-        return dp[i][j]= ans;
-    }
     int maxCoins(vector<int>& nums) {
         int i=1,j=nums.size(),n=j;
         nums.insert(nums.begin(),1);
         nums.insert(nums.end(),1);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return rec(i,j,nums,dp);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i>j) continue;
+                long long ans=INT_MIN;
+                for(int k=i;k<=j;k++)
+                {
+                    long long cost=nums[k]*nums[i-1]*nums[j+1]+dp[i][k-1]+dp[k+1][j];
+                    ans=max(ans,cost);
+                }
+                dp[i][j]= ans;
+            }
+        }
+        return dp[1][n];
     }
 };
