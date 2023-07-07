328f3a0cfabb0f4a2fac0fd61606d100449d42a0
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-07-07 20:54:49 +0530

    Time: 83 ms (9.06%), Space: 42.4 MB (26.58%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -3,24 +3,19 @@ public:
     int maxScore(vector<int>& nums, int k) {
         int left=0,right=0,sum=0,ans=1e9;
         int total=accumulate(nums.begin(),nums.end(),0); 
-        
         int n=nums.size();
         int windowsz=n-k;
-        // if(k==n) return total;
+        if(k==n) return total;
         while(right<n)
         {
             sum+=nums[right++];
             if(right-left==windowsz)
             {
-                // cout<<'*'<<endl;
-                // cout<<sum<<endl;
                 ans=min(ans,sum);
-                // sum+=nums[right];
                 sum-=nums[left++];
-                // cout<<sum<<endl;
             }
         }
         
-        return ans==1e9?total:total-ans;
+        return total-ans;
     }
 };
