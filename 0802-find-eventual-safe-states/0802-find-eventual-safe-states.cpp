028c1281e64cb8902fbc97bc5598fec5d8e25d6a
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-07-12 10:35:43 +0530

    Time: 199 ms (56.23%), Space: 50.6 MB (56.74%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph,int vertex,vector<int>&vis,unordered_set<int>&safeNode)
+    {
+          vis[vertex]=1;
+         for(auto child:graph[vertex])
+         {
+              
+              if(safeNode.find(child)!=safeNode.end())continue;
+              if(vis[child]==1)return false;
+              vis[child]=1;
+              if(!dfs(graph,child,vis,safeNode))return false ;   
+         }
+        safeNode.insert(vertex);
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        int n=graph.size();
+        unordered_set<int>safeNode;
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]==0)
+            {
+                dfs(graph,i,vis,safeNode);
+            }
+        }
+        vector<int>ans;
+        for(auto it:safeNode)ans.push_back(it);
+         sort(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
