565ccddb15cf6a3ea284332a180bba0ff1c643ad
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-07-15 23:41:24 +0530

    Time: 1700 ms (45.77%), Space: 6.4 MB (98.57%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,23 +8,23 @@ public:
         }
         return true;
     }
-    int rec(int i,int n,string &s,vector<int>&dp)
-    {
-        if(i>=n-1 || ispalindrome(s,i,n-1)) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mini=INT_MAX;
-        for(int k=i;k<n-1;k++)
-        {
-            if(ispalindrome(s,i,k)){
-                int cost=1+rec(k+1,n,s,dp);
-                mini=min(mini,cost);
-            }
-        }
-        return dp[i]=mini;
-    }
+    
     int minCut(string s) {
         int n=s.size();
-        vector<int>dp(n+1,-1);
-        return rec(0,n,s,dp);
+        vector<int>dp(n+1,0);
+        dp[n-1]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            int mini=INT_MAX;
+            for(int k=i;k<n;k++)
+            {
+                if(ispalindrome(s,i,k)){
+                    int cost=1+dp[k+1];
+                    mini=min(mini,cost);
+                }
+            }
+             dp[i]=mini;
+        }
+        return dp[0]-1;
     }
 };
