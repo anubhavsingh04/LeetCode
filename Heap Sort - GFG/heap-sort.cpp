cab0c54e961b2b31414f8f2dc07e9c2b1d5c5c9b
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2023-08-11 20:22:54 +0530

    Added solution - LeetHub

--- Heap Sort - GFG/heap-sort.cpp
+++ Heap Sort - GFG/heap-sort.cpp
@@ -14,26 +14,27 @@ class Solution
     //Heapify function to maintain heap property.
     void heapify(int arr[], int n, int i)  
     {
-      int largest=i;
-      int l=2*i+1;
-      int r=2*i+2;
-      if(l<n && arr[l]>arr[largest]) largest=l;
-      if(r<n && arr[r]>arr[largest]) largest=r;
-      if(largest!=i)
-      {
-          swap(arr[i],arr[largest]);
-          heapify(arr,n,largest);
-      }
+        int largest=i;
+        int l=2*i+1;
+        int r=2*i+2;
+        if(l<n && arr[l]>arr[largest]) {
+            largest=l;
+        }
+        if(r<n && arr[r]>arr[largest]){
+            largest=r;
+        }
+        if(largest!=i)
+        {
+            swap(arr[i],arr[largest]);
+            heapify(arr,n,largest);
+        }
     }
 
     public:
     //Function to build a Heap from array.
     void buildHeap(int arr[], int n)  
     { 
-        for(int i=n/2-1;i>=0;i--)
-        {
-            heapify(arr,n,i);
-        }
+        for(int i=n/2-1;i>=0;i--) heapify(arr,n,i);
     }
 
     
