cc6b4f7b07049a2942ec74f82cc38469266c1cb1
Anubhav singh <85066847+anubhavsingh04@users.noreply.github.com>
2024-01-15 15:37:58 +0530

    Time: 706 ms (19.77%), Space: 181 MB (39.23%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        map<int,int>win,lose;
-        int n=matches.size();
-        for(int i=0;i<n;i++)
+        map<int,pair<int,int>>mp;
+        for(auto i:matches)
         {
-            int a=matches[i][0];
-            int b=matches[i][1];
-            lose[b]++;
-            win[a]++;
+            int a=i[0];
+            int b=i[1];
+            int win=1,lose=0;
+            if(mp.count(a)){
+                auto p=mp[a];
+                win+=p.first;
+                lose+=p.second;
+            }
+            mp[a]={win,lose};
+            win=0,lose=1;
+            if(mp.count(b)){
+                auto p=mp[b];
+                win+=p.first;
+                lose+=p.second;
+            }
+            mp[b]={win,lose};
         }
+        vector<int>ans1,ans2;
+        for(auto i:mp)
+        {
+            int a=i.first;
+            auto b=i.second;
+            if(b.second==0){
+                ans1.push_back(a);
+            }
+            if(b.second==1){
+                ans2.push_back(a);
+            }
+        }
+        sort(ans1.begin(),ans1.end());
+        sort(ans2.begin(),ans2.end());
         vector<vector<int>>ans;
-        vector<int>all_wins,one_lost;
-        for(auto i:win) {
-            if(!lose.count(i.first))
-            all_wins.push_back(i.first);
-        }
-        for(auto &i:lose)
-        {
-            if(lose[i.first]==1)
-                one_lost.push_back(i.first);
-        }
-        ans.push_back(all_wins);
-        ans.push_back(one_lost);
+        ans.push_back(ans1);
+        ans.push_back(ans2);
         return ans;
     }
 };
